c2553325985bd0224cf606597898db8035e3cb4e
mightyiam <mightyiampresence@gmail.com>
2021-07-23 18:07:27 +0700

    tmux bindings keep current path

--- home.nix
+++ home.nix
@@ -58,6 +58,10 @@ in {
         set-option -ga terminal-overrides ",alacritty:Tc"
         set-option -g status-right "tmux"
         set-option -g status-style "bg=black,fg=gray"
+
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
       '';
       shell = zshBin;
     };
