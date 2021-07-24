77a26b4e1a9c351a4f2bc75e9cefcce6400866a0
mightyiam <mightyiampresence@gmail.com>
2021-07-25 00:05:00 +0700

    configure starship

--- home.nix
+++ home.nix
@@ -130,6 +130,123 @@ in {
     starship = {
       enable = true;
       enableBashIntegration = true;
+      enableZshIntegration = true;
+      settings = let pink = "#FFC0CB"; in {
+        add_newline = false;
+        ## https://starship.rs/config/#prompt
+        format = builtins.concatStringsSep "" [
+          "$\{custom.line\}"
+          "\n "
+          "$username"
+          "$hostname"
+          "$kubernetes"
+          "$vcsh"
+          "$git_branch"
+          "$git_commit"
+          "$git_state"
+          "$git_metrics"
+          "$git_status"
+          "$hg_branch"
+          "$docker_context"
+          "$package"
+          "$cmake"
+          "$dart"
+          "$deno"
+          "$dotnet"
+          "$elixir"
+          "$elm"
+          "$erlang"
+          "$golang"
+          "$helm"
+          "$java"
+          "$julia"
+          "$kotlin"
+          "$nim"
+          "$nodejs"
+          "$ocaml"
+          "$perl"
+          "$php"
+          "$purescript"
+          "$python"
+          "$red"
+          "$ruby"
+          "$rust"
+          "$scala"
+          "$swift"
+          "$terraform"
+          "$vlang"
+          "$vagrant"
+          "$zig"
+          "$nix_shell"
+          "$conda"
+          "$memory_usage"
+          "$aws"
+          "$gcloud"
+          "$openstack"
+          "$env_var"
+          "$crystal"
+          "$cmd_duration"
+          "$lua"
+          "$jobs"
+          "$battery"
+          "\n "
+          "$directory"
+          "\n "
+          "$status"
+          "[ $shell](bold ${pink})"
+          "$shlvl"
+          "$character"
+        ];
+        custom.line = {
+          command = "printf '━%.0s' {6..$COLUMNS}";
+          when = "true";
+          format = " [  ($output)]($style)";
+          style = "bold ${pink}";
+        };
+        directory = {
+          truncation_length = 0;
+          format = "[ : $path]($style)[$read_only]($read_only_style) ";
+          home_symbol = " ";
+        };
+        shell.disabled = false;
+        shlvl = {
+          disabled = false;
+          threshold = 4;
+          style = "bold red";
+          symbol = "";
+        };
+        status = {
+          disabled = false;
+          symbol = " ";
+        };
+        username = { show_always = true; format = "[  $user]($style) "; };
+        git_branch = { format = "[$symbol$branch]($style) ";};
+
+        ## https://starship.rs/presets/#nerd-font-symbols
+        aws.symbol = "  ";
+        conda.symbol = " ";
+        dart.symbol = " ";
+        directory.read_only = " ";
+        docker_context.symbol = " ";
+        elixir.symbol = " ";
+        elm.symbol = " ";
+        git_branch.symbol = " ";
+        golang.symbol = " ";
+        hg_branch.symbol = " ";
+        java.symbol = " ";
+        julia.symbol = " ";
+        memory_usage.symbol = " ";
+        nim.symbol = " ";
+        nix_shell.symbol = " ";
+        package.symbol = " ";
+        perl.symbol = " ";
+        php.symbol = " ";
+        python.symbol = " ";
+        ruby.symbol = " ";
+        rust.symbol = " ";
+        scala.symbol = " ";
+        swift.symbol = "ﯣ ";
+      };
     };
     chromium = {
       enable = true;
