a22ecd37e3a16ec6107403f3eed0ed0e3d4e6762
mightyiam <mightyiampresence@gmail.com>
2021-07-25 22:09:29 +0700

    clean up ssh

--- ssh.nix
+++ ssh.nix
@@ -11,8 +11,6 @@ homeDirectory: {
       extraOptions = {
         PubkeyAuthentication = "yes";
       };
-      #identityFile = "/home/mightyiam/.ssh/id_github.com";
-      #identityFile = "${"/home/mightyiam"}/.ssh/id_github.com";
       identityFile = builtins.toString homeDirectory + "/.ssh/id_github.com";
     };
   };
