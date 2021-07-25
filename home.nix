17abb9725ac793a5e740c366013504808705e213
mightyiam <mightyiampresence@gmail.com>
2021-07-25 21:28:46 +0700

    nerdtree exit if last

--- home.nix
+++ home.nix
@@ -282,6 +282,9 @@ in {
             nnoremap <C-n> :NERDTree<CR>
             nnoremap <C-t> :NERDTreeToggle<CR>
             nnoremap <C-f> :NERDTreeFind<CR>
+
+            " Exit Vim if NERDTree is the only window remaining in the only tab.
+            autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
           '';
         }
       ];
