1c8621a2ddaefe3c35af4104365074c73aa835ed
mightyiam <mightyiampresence@gmail.com>
2021-07-25 22:37:11 +0700

    extract alacritty

--- home.nix
+++ home.nix
@@ -93,26 +93,7 @@ in {
     };
     home-manager.enable = true;
     neovim = import ./nvim/main.nix pkgs;
-    alacritty = {
-      enable = true;
-      settings = {
-        shell = {
-          program = zshBin;
-          args = ["--login"];
-        };
-        background_opacity = 0.8;
-        decorations = "none";
-        dynamic_title = true;
-        font = {
-          size = 10.0;
-        };
-        bell = {
-          color = "#000000";
-          duration = 200.0;
-        };
-        live_config_reload = true;
-      };
-    };
+    alacritty = import ./alacritty.nix zshBin;
     mako = {
       enable = true;
       anchor = "top-right";
