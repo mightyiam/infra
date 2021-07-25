c7cd221557c82cda4a1627827b3bef30643d6597
mightyiam <mightyiampresence@gmail.com>
2021-07-25 22:55:17 +0700

    no exit vim when nerdtree last

--- nvim/main.nix
+++ nvim/main.nix
@@ -13,9 +13,6 @@ pkgs: {
         nnoremap <leader>n :NERDTreeFocus<CR>
         nnoremap <C-n> :NERDTree<CR>
         nnoremap <C-t> :NERDTreeToggle<CR>
-
-        " Exit Vim if NERDTree is the only window remaining in the only tab.
-        autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
       '';
     }
     {
