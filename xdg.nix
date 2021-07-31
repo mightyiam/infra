ced52af94d2001d87206e268930df8ad72adcaec
mightyiam <mightyiampresence@gmail.com>
2021-07-31 22:08:14 +0700

    xdg mimeApps

--- xdg.nix
+++ xdg.nix
@@ -1,6 +1,22 @@
 configHome: {
     enable = true;
     inherit configHome;
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications =
+        let 
+          web_browser = [ "firefox.desktop" "chromium.desktop" ];
+          image = [ "imv.desktop" ];
+        in {
+          "text/html" = web_browser;
+          "x-scheme-handler/http" = web_browser;
+          "x-scheme-handler/https" = web_browser;
+          "x-scheme-handler/about" = web_browser;
+          "x-scheme-handler/unknown" = web_browser;
+          "image/png" = image;
+        };
+    };
     #userDirs = let tmp = "${homeDirectory}/tmp"; in {
       #enable = true;
       #createDirectories = false;
