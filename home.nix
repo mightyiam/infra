24bfb58fe459d977a0d6a53f635edb4bc088fee7
Shahar Dawn Or <mightyiampresence@gmail.com>
2021-09-28 19:44:40 +0700

    no programs.nix

--- home.nix
+++ home.nix
@@ -10,7 +10,22 @@ in {
     (import ./accounts.nix)
     (import ./systemd.nix)
     (import ./fonts.nix).module
-    (import ./programs.nix)
+    (import ./zsh)
+    (import ./tmux.nix)
+    (import ./alacritty.nix)
+    (import ./exa.nix)
+    (import ./firefox.nix)
+    (import ./chromium.nix)
+    (import ./command-not-found.nix)
+    (import ./info.nix)
+    (import ./bat.nix)
+    (import ./neovim.nix)
+    (import ./vscode.nix)
+    (import ./gh.nix)
+    (import ./ssh.nix)
+    (import ./starship)
+    (import ./git.nix)
+    (import ./bottom.nix)
     (import ./sway.nix)
   ];
 
