fb1fdefc995b0e827ccac3a204e616e7ed212005
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-02-02 17:02:20 +0700

    mimetypes rewrite

--- xdg.nix
+++ xdg.nix
@@ -6,14 +6,23 @@ homeDirectory: {
       enable = true;
       defaultApplications =
         let 
-          web_browser = [ "firefox.desktop" "chromium.desktop" ];
+          web_browser = [ "firefox.desktop" ];
+          browser_types = [
+            "x-scheme-handler/http"
+            "x-scheme-handler/https"
+            "x-scheme-handler/chrome"
+            "text/html"
+            "application/x-extension-htm"
+            "application/x-extension-html"
+            "application/x-extension-shtml"
+            "application/xhtml+xml"
+            "application/x-extension-xhtml"
+            "application/x-extension-xht"
+          ];
           image = [ "imv.desktop" ];
-        in {
-          "text/html" = web_browser;
-          "x-scheme-handler/http" = web_browser;
-          "x-scheme-handler/https" = web_browser;
-          "x-scheme-handler/about" = web_browser;
-          "x-scheme-handler/unknown" = web_browser;
+          type_to_browser_entry = (type: { name = type; value = web_browser; });
+          browser_entries = builtins.listToAttrs (map type_to_browser_entry browser_types);
+        in browser_entries // {
           "image/png" = image;
         };
     };
