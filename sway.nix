f72dc6c95b5600151a1a5be413529318cd467ade
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-02-09 22:57:59 +0700

    format sway

--- sway.nix
+++ sway.nix
@@ -1,50 +1,65 @@
 { pkgs, lib, config, ... }:
-  let
-    outputs = import ./outputs.nix;
-    secrets = (import ./secrets.nix);
-    lockCommand = builtins.concatStringsSep " " [
-      (pkgs.swaylock + /bin/swaylock)
-      "--daemonize"
-      "--show-keyboard-layout"
-      "--indicator-caps-lock"
-      "--color 000000"
-      "--indicator-radius 200"
-    ];
-  in {
-    nixpkgs.overlays = [
-      (self: super: {
-        sway-unwrapped = super.sway-unwrapped.overrideAttrs (oldAttrs: rec {
-          version = "1.6.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "swaywm";
-            repo = "sway";
-            rev = version;
-            sha256 = "0j4sdbsrlvky1agacc0pcz9bwmaxjmrapjnzscbd2i0cria2fc5j";
-          };
-          buildInputs = with pkgs; [
-            wayland libxkbcommon pcre json_c dbus libevdev
-            pango cairo libinput libcap pam gdk-pixbuf librsvg
-            wayland-protocols libdrm
-            (wlroots_0_14.override { enableXWayland = true; })
-          ];
-        });
-      })
-    ];
-    wayland.windowManager.sway = {
-      enable = true;
-      wrapperFeatures.gtk = true;
-      extraSessionCommands = ''
-        export WLR_DRM_NO_MODIFIERS=1
-        export I3RS_GITHUB_TOKEN=${secrets.I3RS_GITHUB_TOKEN}
-        export MOZ_ENABLE_WAYLAND=1
-      '';
-      config = let
+let
+  outputs = import ./outputs.nix;
+  secrets = (import ./secrets.nix);
+  lockCommand = builtins.concatStringsSep " " [
+    (pkgs.swaylock + /bin/swaylock)
+    "--daemonize"
+    "--show-keyboard-layout"
+    "--indicator-caps-lock"
+    "--color 000000"
+    "--indicator-radius 200"
+  ];
+in
+{
+  nixpkgs.overlays = [
+    (self: super: {
+      sway-unwrapped = super.sway-unwrapped.overrideAttrs (oldAttrs: rec {
+        version = "1.6.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "swaywm";
+          repo = "sway";
+          rev = version;
+          sha256 = "0j4sdbsrlvky1agacc0pcz9bwmaxjmrapjnzscbd2i0cria2fc5j";
+        };
+        buildInputs = with pkgs; [
+          wayland
+          libxkbcommon
+          pcre
+          json_c
+          dbus
+          libevdev
+          pango
+          cairo
+          libinput
+          libcap
+          pam
+          gdk-pixbuf
+          librsvg
+          wayland-protocols
+          libdrm
+          (wlroots_0_14.override { enableXWayland = true; })
+        ];
+      });
+    })
+  ];
+  wayland.windowManager.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraSessionCommands = ''
+      export WLR_DRM_NO_MODIFIERS=1
+      export I3RS_GITHUB_TOKEN=${secrets.I3RS_GITHUB_TOKEN}
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+    config =
+      let
         mod-key = "Mod4";
-      in {
+      in
+      {
         output = {
           "${outputs.right}" = {
             res = "1920x1080@144Hz";
-          } ;
+          };
           "${outputs.left}" = {
             res = "1920x1080@75Hz";
           };
@@ -61,7 +76,8 @@
           let
             step = 5;
             incVol = d: "exec pactl set-sink-volume @DEFAULT_SINK@ ${d}${builtins.toString step}%";
-          in lib.mkOptionDefault {
+          in
+          lib.mkOptionDefault {
             "${mod-key}+Shift+e" = "exec swaymsg exit";
             "${mod-key}+Ctrl+l" = "exec ${lockCommand}";
             "${mod-key}+minus" = incVol "-";
@@ -71,27 +87,27 @@
           { command = "mako"; }
         ];
       };
-    };
-    home.packages = [pkgs.pulseaudio];
-    programs.mako = {
-      enable = true;
-      anchor = "top-right";
-      defaultTimeout = 3000;
-      ignoreTimeout = true;
-      output = outputs.left;
-    };
-    services.swayidle = {
-      enable = true;
-      timeouts = [
-        {
-          timeout = 60 * 10;
-          command = lockCommand;
-        }
-        {
-          timeout = 60 * 11;
-          command = "${pkgs.sway + /bin/swaymsg} \"output * dpms off\"";
-          resumeCommand = "${pkgs.sway + /bin/swaymsg} \"output * dpms on\"";
-        }
-      ];
-    };
-  }
+  };
+  home.packages = [ pkgs.pulseaudio ];
+  programs.mako = {
+    enable = true;
+    anchor = "top-right";
+    defaultTimeout = 3000;
+    ignoreTimeout = true;
+    output = outputs.left;
+  };
+  services.swayidle = {
+    enable = true;
+    timeouts = [
+      {
+        timeout = 60 * 10;
+        command = lockCommand;
+      }
+      {
+        timeout = 60 * 11;
+        command = "${pkgs.sway + /bin/swaymsg} \"output * dpms off\"";
+        resumeCommand = "${pkgs.sway + /bin/swaymsg} \"output * dpms on\"";
+      }
+    ];
+  };
+}
