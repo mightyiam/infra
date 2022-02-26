ef312bc6bfe6b7613c71f2bea84ebe2832e72d95
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-02-26 22:05:08 +0700

    split up xdg

---
+++ xdg-mime.nix
@@ -0,0 +1,34 @@
+with builtins; { pkgs, ... }:
+let
+  pipe = pkgs.lib.trivial.pipe;
+  web_browser = [ "firefox.desktop" ];
+  browser_types = [
+    "x-scheme-handler/http"
+    "x-scheme-handler/https"
+    "x-scheme-handler/chrome"
+    "text/html"
+    "application/x-extension-htm"
+    "application/x-extension-html"
+    "application/x-extension-shtml"
+    "application/xhtml+xml"
+    "application/x-extension-xhtml"
+    "application/x-extension-xht"
+  ];
+  image = [ "imv.desktop" ];
+  type_to_browser_entry = (type: { name = type; value = web_browser; });
+  browser_entries = pipe browser_types [ (map type_to_browser_entry) listToAttrs ];
+in
+{
+  xdg = {
+    enable = true;
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications =
+        browser_entries // {
+          "image/png" = image;
+          "x-scheme-handler/magnet" = "transmission-gtk";
+        };
+    };
+  };
+}
---
+++ xdg-user-dirs.nix
@@ -0,0 +1,18 @@
+{ config, ... }: {
+  xdg = {
+    enable = true;
+    userDirs = let tmp = "${config.home.homeDirectory}/tmp"; in
+      {
+        enable = true;
+        createDirectories = true;
+        desktop = tmp;
+        documents = tmp;
+        download = tmp;
+        music = tmp;
+        pictures = tmp;
+        publicShare = "${config.home.homeDirectory}/public";
+        templates = "${config.home.homeDirectory}/templates";
+        videos = tmp;
+      };
+  };
+}
