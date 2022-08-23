7dfedce48bcfbb8acadcb2a9eb56706b87e39434
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-08-23 21:02:49 +0700

    fq style systems/desktop/configuration.nix

--- systems/desktop/configuration.nix
+++ systems/desktop/configuration.nix
@@ -4,92 +4,62 @@
   ...
 }: {
   imports = [../../hardware-configuration.nix];
-  hardware = {
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = true;
-    opengl.enable = true;
-    bluetooth.enable = true;
-  };
-  boot = {
-    loader = {
-      timeout = 2;
-      grub = {
-        enable = true;
-        efiSupport = true;
-        mirroredBoots = [
-          {
-            devices = ["nodev"];
-            path = "/boot0";
-          }
-          {
-            devices = ["nodev"];
-            path = "/boot1";
-          }
-        ];
-      };
-      efi.canTouchEfiVariables = true;
-    };
-    zfs = {
-      forceImportAll = false;
-      forceImportRoot = false;
-    };
-    kernelParams = ["nohibernate"];
-  };
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.opengl.enable = true;
+  hardware.bluetooth.enable = true;
+  boot.loader.timeout = 4;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.mirroredBoots = [
+    {
+      devices = ["nodev"];
+      path = "/boot0";
+    }
+    {
+      devices = ["nodev"];
+      path = "/boot1";
+    }
+  ];
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.zfs.forceImportAll = false;
+  boot.zfs.forceImportRoot = false;
+  boot.kernelParams = ["nohibernate"];
   security.rtkit.enable = true;
-  networking = {
-    hostName = "mightyiam-desktop";
-    hostId = "6b5dea2a";
-    networkmanager.enable = true;
-  };
+  networking.hostName = "mightyiam-desktop";
+  networking.hostId = "6b5dea2a";
+  networking.networkmanager.enable = true;
   nix.settings.auto-optimise-store = true;
-  users = {
-    mutableUsers = false;
-    defaultUserShell = pkgs.zsh;
-    users.mightyiam = {
-      isNormalUser = true;
-      extraGroups = ["wheel" "audio" "networkmanager" "docker"];
-      hashedPassword = import ./hashed-password.nix;
-    };
-  };
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    defaultEditor = true;
-  };
-  services = {
-    avahi = {
-      enable = true;
-      nssmdns = true;
-    };
-    zfs.autoScrub = {
-      enable = true;
-      interval = "monthly";
-    };
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
-    printing.enable = true;
-  };
+  users.mutableUsers = false;
+  users.defaultUserShell = pkgs.zsh;
+  users.users.mightyiam.isNormalUser = true;
+  users.users.mightyiam.extraGroups = ["wheel" "audio" "networkmanager" "docker"];
+  users.users.mightyiam.hashedPassword = import ./hashed-password.nix;
+  programs.neovim.enable = true;
+  programs.neovim.vimAlias = true;
+  programs.neovim.defaultEditor = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoScrub.interval = "monthly";
+  services.pipewire.enable = true;
+  services.pipewire.alsa.enable = true;
+  services.pipewire.alsa.support32Bit = true;
+  services.pipewire.pulse.enable = true;
+  services.printing.enable = true;
   ## Things I'd like configured by home-manager:
   #
-  xdg.portal = {
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-wlr
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [
+    pkgs.xdg-desktop-portal-wlr
+    pkgs.xdg-desktop-portal-gtk
+  ];
   ###
   security.pam.services.swaylock = {};
   fonts.enableDefaultFonts = false;
-  fonts.fontconfig.defaultFonts = {
-    serif = [];
-    emoji = [];
-    sansSerif = [];
-    monospace = [];
-  };
+  fonts.fontconfig.defaultFonts.serif = [];
+  fonts.fontconfig.defaultFonts.emoji = [];
+  fonts.fontconfig.defaultFonts.sansSerif = [];
+  fonts.fontconfig.defaultFonts.monospace = [];
   system.stateVersion = "21.11";
 }
