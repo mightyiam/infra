ff0a4707337d90949a78bcc507161dd020e33a32
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-09-17 10:47:40 +0700

    teeveera no services.xserver.videoDrivers

--- systems/teeveera/hardware.nix
+++ systems/teeveera/hardware.nix
@@ -1,6 +1,5 @@
 {
   boot.initrd.kernelModules = ["amdgpu"];
-  services.xserver.videoDrivers = ["amdgpu"];
   hardware = {
     enableRedistributableFirmware = true;
     cpu.amd.updateMicrocode = true;
