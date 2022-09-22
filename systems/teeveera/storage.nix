4e75427b80ef4548f96871b802da20dd2e383591
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-09-22 09:45:10 +0700

    teeveera ephemeral mounts

--- systems/teeveera/storage.nix
+++ systems/teeveera/storage.nix
@@ -1,29 +1,8 @@
-let
+{config, ...}: let
   media = import ./media.nix;
-  boot = n: {
-    "/boot${toString n}" = {
-      device = "/dev/disk/by-partlabel/boot${toString n}";
-      fsType = "vfat";
-    };
-  };
-  zfs = mountPath: let
-    zfsNameSuffix =
-      if mountPath == "/"
-      then ""
-      else mountPath;
-  in {
-    "${mountPath}" = {
-      device = "storage/root${zfsNameSuffix}";
-      fsType = "zfs";
-    };
-  };
 in {
-  fileSystems =
-    zfs "/"
-    // boot 0
-    // boot 1
-    // zfs media.dir;
   system.activationScripts.media.text = ''
+    mkdir --parents ${media.dir}
     chown root:${media.group} ${media.dir}
     chmod ${toString media.mode.mode} ${media.dir}
   '';
