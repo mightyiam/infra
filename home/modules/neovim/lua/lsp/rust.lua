50fc55b39ef3a6ebe034f2ac73efc5c587112fb9
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-10-01 13:45:27 +0700

    neovim clippy

--- home/modules/neovim/lua/lsp/rust.lua
+++ home/modules/neovim/lua/lsp/rust.lua
@@ -1,5 +1,14 @@
 local function setup(lsp_zero)
-	local options = lsp_zero.build_options("rust_analyzer", {})
-	require("rust-tools").setup({ server = options })
+  local options = lsp_zero.build_options("rust_analyzer", {
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy"
+        }
+      }
+    }
+  })
+  require("rust-tools").setup({ server = options })
 end
+
 return setup
