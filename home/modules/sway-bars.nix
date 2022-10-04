4265a4ae815f4192d3c4380d9c241630f8bd13e6
Shahar Dawn Or <mightyiampresence@gmail.com>
2022-10-04 11:31:15 +0700

    battery block

--- home/modules/sway-bars.nix
+++ home/modules/sway-bars.nix
@@ -35,6 +35,10 @@ in {
             };
           }
           {block = "sound";}
+          {
+            block = "battery";
+            hide_missing = true;
+          }
         ];
       };
     };
