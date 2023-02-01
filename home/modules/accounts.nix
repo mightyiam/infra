e7ccc85b5a9e851fabc3ba6218b4b416b06a3738
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-02-01 15:09:39 +0700

    change realName to have Dawn in quotes

--- home/modules/accounts.nix
+++ home/modules/accounts.nix
@@ -1,7 +1,7 @@
 instance: {
   accounts.email.accounts.default = {
     address = "mightyiampresence@gmail.com";
-    realName = "Shahar Dawn Or";
+    realName = "Shahar \"Dawn\" Or";
     flavor = "gmail.com";
     primary = true;
   };
