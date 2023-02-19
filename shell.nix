56ff393a6e44c7f5bb3e67d5ef9813ce28c053d9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-02-19 08:00:05 +0700

    rm .local.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,10 @@
-{pkgs ? import <nixpkgs> {}}: let
-  shared = {
-    buildInputs = with pkgs; [
-      alejandra
-      rnix-lsp
-      stylua
-      sumneko-lua-language-server
-    ];
-  };
-  local_path = ./local.nix;
-  local =
-    if builtins.pathExists local_path
-    then (import local_path) {inherit pkgs;}
-    else {};
-  merged = pkgs.lib.recursiveUpdate shared local;
-in
-  pkgs.mkShell merged
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    alejandra
+    rnix-lsp
+    stylua
+    sumneko-lua-language-server
+    mob
+  ];
+}
