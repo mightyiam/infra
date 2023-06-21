0239e83d08373a6b0b472f507bfc99ffd6e39965
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-06-21 21:11:54 +0700

    lualine

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -77,9 +77,70 @@ with builtins;
             }
           ''))
           editorconfig-nvim
-          (omitPluginInVSCode lightline-vim ''
-            set noshowmode
-          '')
+          (omitPluginInVSCode lualine-nvim (embedLua ''
+            local winbar = {
+              lualine_a = {
+                'encoding',
+                'fileformat',
+              },
+              lualine_b = {
+                'filetype',
+              },
+              lualine_c = {
+                {
+                  'filename',
+                  path = 1,
+                  shorting_target = 0,
+                },
+              },
+              lualine_x = {
+                'diff',
+              },
+              lualine_y = {
+                'location',
+              },
+              lualine_z = {
+                'progress',
+              }
+            }
+
+            require('lualine').setup({
+              options = {
+                globalstatus = true,
+                section_separators = "",
+                component_separators = "",
+              },
+
+              extensions = {
+                'nerdtree',
+                'trouble',
+              },
+
+              winbar = winbar,
+              inactive_winbar = winbar,
+
+              sections = {
+                lualine_a = {
+                  'mode',
+                },
+                lualine_b = {
+                  {
+                    'diagnostics',
+                    sources = { 'nvim_lsp' },
+                  },
+                },
+                lualine_c = {
+                },
+                lualine_x = {
+                },
+                lualine_y = {
+                },
+                lualine_z = {
+                  'branch',
+                },
+              },
+            })
+          ''))
           (omitPluginInVSCode nerdtree ''
             nnoremap ${fileTreeFocus} :NERDTreeFocus<CR>
             nnoremap ${fileTreeToggle} :NERDTreeToggle<CR>
