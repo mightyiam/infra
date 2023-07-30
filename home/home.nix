fea8e65ae3f5d67792ecd49867478dd3b83c252d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-07-30 18:46:42 +0700

    refactor

--- home/home.nix
+++ home/home.nix
@@ -15,19 +15,15 @@ with builtins;
     imports = map (module: import module instance) activeModules;
     home.packages = concatMap (feature: getAttr feature packages) instance.features;
 
-    programs.home-manager = {
-      enable = true;
-      path =
-        if instance ? homeManagerPath
-        then instance.homeManagerPath
-        else null;
-    };
+    programs.home-manager.enable = true;
+    programs.home-manager.path =
+      if instance ? homeManagerPath
+      then instance.homeManagerPath
+      else null;
 
-    home = {
-      inherit username;
-      homeDirectory = "/home/${username}";
-      stateVersion = "21.05";
-      sessionVariables.TZ = "\$(<~/.config/timezone)";
-      sessionVariables.CACHIX_AUTH_TOKEN = instance.secrets.CACHIX_AUTH_TOKEN;
-    };
+    home.username = username;
+    home.homeDirectory = "/home/${username}";
+    home.stateVersion = "21.05";
+    home.sessionVariables.TZ = "\$(<~/.config/timezone)";
+    home.sessionVariables.CACHIX_AUTH_TOKEN = instance.secrets.CACHIX_AUTH_TOKEN;
   }
