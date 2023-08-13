a7810b9df1b99b224a40b0dabb15fa406f38895a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-08-13 14:17:36 +0700

    rewrite sway

--- home/modules/gui/sway.nix
+++ home/modules/gui/sway.nix
@@ -28,87 +28,79 @@ with builtins;
       ...
     }: {
       name = path;
-      value = {
-        res = with resolution; "${toString width}x${toString height}@${toString refreshRate}Hz";
-        pos = with position; "${toString x} ${toString y}";
-        scale = toString scale;
-        background = "${(import ../../gruvbox.nix).colors.dark0} solid_color";
-      };
+      value.res = with resolution; "${toString width}x${toString height}@${toString refreshRate}Hz";
+      value.pos = with position; "${toString x} ${toString y}";
+      value.scale = toString scale;
+      value.background = "${(import ../../gruvbox.nix).colors.dark0} solid_color";
     };
     swayMsgPath = config.wayland.windowManager.sway.package + /bin/swaymsg;
-  in {
-    wayland.windowManager.sway = {
-      enable = true;
-      wrapperFeatures.gtk = true;
-      extraSessionCommands = ''
+  in
+    lib.attrsets.recursiveUpdate
+    {
+      wayland.windowManager.sway.enable = true;
+      wayland.windowManager.sway.wrapperFeatures.gtk = true;
+      wayland.windowManager.sway.extraSessionCommands = ''
         export MOZ_ENABLE_WAYLAND=1
       '';
-      config = {
-        output = listToAttrs (attrValues (mapAttrs toSwayOutput instance.outputs));
-        fonts.size = (import ../../fonts.nix).default.size;
-        input = {
-          "type:keyboard" = {
-            xkb_layout = "us,il";
-            xkb_options = "grp:lalt_lshift_toggle";
-            repeat_delay = "200";
-            repeat_rate = "50";
-          };
-        };
-        terminal = "false";
-        modifier = "Mod4";
-        keybindings = with keyboard.wm; {
-          "${terminal}" = "exec ${config.programs.alacritty.package + /bin/alacritty}";
-          "${kill}" = "kill";
-          "${menu}" = "exec ${config.wayland.windowManager.sway.config.menu}";
+      wayland.windowManager.sway.config.output = listToAttrs (attrValues (mapAttrs toSwayOutput instance.outputs));
+      wayland.windowManager.sway.config.fonts.size = (import ../../fonts.nix).default.size;
+      wayland.windowManager.sway.config.input."type:keyboard".xkb_layout = "us,il";
+      wayland.windowManager.sway.config.input."type:keyboard".xkb_options = "grp:lalt_lshift_toggle";
+      wayland.windowManager.sway.config.input."type:keyboard".repeat_delay = "200";
+      wayland.windowManager.sway.config.input."type:keyboard".repeat_rate = "50";
+      wayland.windowManager.sway.config.terminal = "false";
+      wayland.windowManager.sway.config.modifier = "Mod4";
+    }
+    (with keyboard.wm; {
+      wayland.windowManager.sway.config.keybindings.${terminal} = "exec ${config.programs.alacritty.package + /bin/alacritty}";
+      wayland.windowManager.sway.config.keybindings.${kill} = "kill";
+      wayland.windowManager.sway.config.keybindings.${menu} = "exec ${config.wayland.windowManager.sway.config.menu}";
 
-          "${focus.left}" = "focus left";
-          "${focus.down}" = "focus down";
-          "${focus.up}" = "focus up";
-          "${focus.right}" = "focus right";
-          "${focus.parent}" = "focus parent";
+      wayland.windowManager.sway.config.keybindings.${focus.left} = "focus left";
+      wayland.windowManager.sway.config.keybindings.${focus.down} = "focus down";
+      wayland.windowManager.sway.config.keybindings.${focus.up} = "focus up";
+      wayland.windowManager.sway.config.keybindings.${focus.right} = "focus right";
+      wayland.windowManager.sway.config.keybindings.${focus.parent} = "focus parent";
 
-          "${move.left}" = "move left";
-          "${move.down}" = "move down";
-          "${move.up}" = "move up";
-          "${move.right}" = "move right";
+      wayland.windowManager.sway.config.keybindings.${move.left} = "move left";
+      wayland.windowManager.sway.config.keybindings.${move.down} = "move down";
+      wayland.windowManager.sway.config.keybindings.${move.up} = "move up";
+      wayland.windowManager.sway.config.keybindings.${move.right} = "move right";
 
-          "${fullscreen}" = "fullscreen toggle";
+      wayland.windowManager.sway.config.keybindings.${fullscreen} = "fullscreen toggle";
 
-          "${layout.tabbed}" = "layout tabbed";
-          "${layout.toggleSplit}" = "layout toggle split";
+      wayland.windowManager.sway.config.keybindings.${layout.tabbed} = "layout tabbed";
+      wayland.windowManager.sway.config.keybindings.${layout.toggleSplit} = "layout toggle split";
 
-          "${floatingToggle}" = "floating toggle";
-          "${focusModeToggle}" = "focus mode_toggle";
+      wayland.windowManager.sway.config.keybindings.${floatingToggle} = "floating toggle";
+      wayland.windowManager.sway.config.keybindings.${focusModeToggle} = "focus mode_toggle";
 
-          "${workspace."1"}" = "workspace number 1";
-          "${workspace."2"}" = "workspace number 2";
-          "${workspace."3"}" = "workspace number 3";
-          "${workspace."4"}" = "workspace number 4";
-          "${workspace."5"}" = "workspace number 5";
-          "${workspace."6"}" = "workspace number 6";
-          "${workspace."7"}" = "workspace number 7";
-          "${workspace."8"}" = "workspace number 8";
-          "${workspace."9"}" = "workspace number 9";
+      wayland.windowManager.sway.config.keybindings.${workspace."1"} = "workspace number 1";
+      wayland.windowManager.sway.config.keybindings.${workspace."2"} = "workspace number 2";
+      wayland.windowManager.sway.config.keybindings.${workspace."3"} = "workspace number 3";
+      wayland.windowManager.sway.config.keybindings.${workspace."4"} = "workspace number 4";
+      wayland.windowManager.sway.config.keybindings.${workspace."5"} = "workspace number 5";
+      wayland.windowManager.sway.config.keybindings.${workspace."6"} = "workspace number 6";
+      wayland.windowManager.sway.config.keybindings.${workspace."7"} = "workspace number 7";
+      wayland.windowManager.sway.config.keybindings.${workspace."8"} = "workspace number 8";
+      wayland.windowManager.sway.config.keybindings.${workspace."9"} = "workspace number 9";
 
-          "${moveToWorkspace."1"}" = "move container to workspace number 1";
-          "${moveToWorkspace."2"}" = "move container to workspace number 2";
-          "${moveToWorkspace."3"}" = "move container to workspace number 3";
-          "${moveToWorkspace."4"}" = "move container to workspace number 4";
-          "${moveToWorkspace."5"}" = "move container to workspace number 5";
-          "${moveToWorkspace."6"}" = "move container to workspace number 6";
-          "${moveToWorkspace."7"}" = "move container to workspace number 7";
-          "${moveToWorkspace."8"}" = "move container to workspace number 8";
-          "${moveToWorkspace."9"}" = "move container to workspace number 9";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."1"} = "move container to workspace number 1";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."2"} = "move container to workspace number 2";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."3"} = "move container to workspace number 3";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."4"} = "move container to workspace number 4";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."5"} = "move container to workspace number 5";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."6"} = "move container to workspace number 6";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."7"} = "move container to workspace number 7";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."8"} = "move container to workspace number 8";
+      wayland.windowManager.sway.config.keybindings.${moveToWorkspace."9"} = "move container to workspace number 9";
 
-          "${reload}" = "reload";
-          "${exit}" = "exec ${swayMsgPath} exit";
+      wayland.windowManager.sway.config.keybindings.${reload} = "reload";
+      wayland.windowManager.sway.config.keybindings.${exit} = "exec ${swayMsgPath} exit";
 
-          "${resize}" = "mode resize";
+      wayland.windowManager.sway.config.keybindings.${resize} = "mode resize";
 
-          "${volume.decrement}" = incVol "-";
-          "${volume.increment}" = incVol "+";
-          "${volume.toggleMuteSources}" = toggleMuteSources;
-        };
-      };
-    };
-  }
+      wayland.windowManager.sway.config.keybindings.${volume.decrement} = incVol "-";
+      wayland.windowManager.sway.config.keybindings.${volume.increment} = incVol "+";
+      wayland.windowManager.sway.config.keybindings.${volume.toggleMuteSources} = toggleMuteSources;
+    })
