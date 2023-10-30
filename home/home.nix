e6c09b31d71ab9f39617bb6ed2249d9d05daaeea
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-10-30 13:01:58 +0700

    support nix-on-droid with username lunacy

--- home/home.nix
+++ home/home.nix
@@ -8,6 +8,13 @@ with builtins;
   }: let
     username = "mightyiam";
     getName = lib.getName;
+    userAndHome =
+      if (instance ? "nix-on-droid")
+      then {}
+      else {
+        home.username = username;
+        home.homeDirectory = "/home/${username}";
+      };
     packages = (import ./packages.nix) pkgs;
     activeModules = let
       featureToModulePaths = feature: let
@@ -17,19 +24,19 @@ with builtins;
         map (path: "${dir}/${path}") relativePaths;
     in
       concatMap featureToModulePaths instance.features;
-  in {
-    imports = map (module: import module instance) activeModules;
-    home.packages = concatMap (feature: getAttr feature packages) instance.features;
+    always = {
+      imports = map (module: import module instance) activeModules;
+      home.packages = concatMap (feature: getAttr feature packages) instance.features;
 
-    programs.home-manager.enable = true;
-    programs.home-manager.path =
-      if instance ? homeManagerPath
-      then instance.homeManagerPath
-      else null;
+      programs.home-manager.enable = true;
+      programs.home-manager.path =
+        if instance ? homeManagerPath
+        then instance.homeManagerPath
+        else null;
 
-    home.username = username;
-    home.homeDirectory = "/home/${username}";
-    home.stateVersion = "21.05";
-    home.sessionVariables.TZ = "\$(<~/.config/timezone)";
-    home.sessionVariables.CACHIX_AUTH_TOKEN = instance.secrets.CACHIX_AUTH_TOKEN;
-  }
+      home.stateVersion = "21.05";
+      home.sessionVariables.TZ = "\$(<~/.config/timezone)";
+      home.sessionVariables.CACHIX_AUTH_TOKEN = instance.secrets.CACHIX_AUTH_TOKEN;
+    };
+  in
+    lib.attrsets.recursiveUpdate always userAndHome
