cd24533619a82ad6c416a34e554ebb8fcddbabf1
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-11-03 20:05:52 +0700

    sink-rotate v1.0.2

--- home/modules/gui/sink-rotate.nix
+++ home/modules/gui/sink-rotate.nix
@@ -5,16 +5,16 @@ instance: {
 }: let
   sink-rotate = pkgs.rustPlatform.buildRustPackage {
     pname = "sink-rotate";
-    version = "1.0.0";
+    version = "1.0.2";
 
     src = pkgs.fetchFromGitHub {
       owner = "mightyiam";
       repo = "sink-rotate";
-      rev = "v1.0.0";
-      hash = "sha256-xqDnyKlw44HyVHw9P3fdoXs1swXr0DMNRrg4YG5WMXI=";
+      rev = "v1.0.2";
+      hash = "sha256-nNOhQ0l3skF3lbDfE73biT0naUicJemdDsDw464PpLc=";
     };
 
-    cargoHash = "sha256-TL9uY/vq+SG1jtvw/eeW4/SKt+TGndpWMhRKU07BB5s=";
+    cargoHash = "sha256-5BW8tfA1cXEdU8KQnA+xkKO0XXFfqwl9ytVighgf7ac=";
 
     buildInputs = with pkgs; [pipewire wireplumber];
 
