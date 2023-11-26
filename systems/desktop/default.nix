5abae52926d2f37d974ed8d208e4acb9fe434bdf
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-11-26 22:34:56 +0700

    xdg.portal change

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -14,7 +14,8 @@
   ## Things I'd like configured by home-manager:
   #
   xdg.portal.enable = true;
-  xdg.portal.extraPortals = [
+  xdg.portal.wlr.enable = true;
+  xdg.portal.configPackages = [
     (pkgs.xdg-desktop-portal-wlr.overrideAttrs (attrs: {
       postInstall = ''
         wrapProgram $out/libexec/xdg-desktop-portal-wlr --prefix PATH ":" ${lib.makeBinPath [pkgs.grim pkgs.slurp pkgs.bash]}
