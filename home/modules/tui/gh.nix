cd2e7fbe8691ff8213f3f8f1d104959b011d7543
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-07 15:25:04 +0700

    wrap gh to unset GITHUB_TOKEN

--- home/modules/tui/gh.nix
+++ home/modules/tui/gh.nix
@@ -1,5 +1,13 @@
-instance: {
+instance: {pkgs, ...}: {
   programs.gh = {
+    package = pkgs.symlinkJoin {
+      name = pkgs.gh.name;
+      paths = [pkgs.gh];
+      buildInputs = [pkgs.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/gh --unset GITHUB_TOKEN
+      '';
+    };
     enable = true;
     settings = {
       git_protocol = "ssh";
