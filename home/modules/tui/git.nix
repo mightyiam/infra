094c6b1e02f6290effbf5866c9eaee5fdcee9a00
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-19 20:51:18 +0700

    git-fixup

--- home/modules/tui/git.nix
+++ home/modules/tui/git.nix
@@ -1,4 +1,8 @@
-instance: {config, ...}: {
+instance: {
+  config,
+  pkgs,
+  ...
+}: {
   programs.git = {
     enable = true;
     userName = config.accounts.email.accounts.default.realName;
@@ -16,4 +20,24 @@ instance: {config, ...}: {
       merge.conflictstyle = "diff3";
     };
   };
+
+  home.packages = [
+    # https://github.com/NixOS/nixpkgs/pull/275402
+    (let
+      version = "0.1.8";
+      pname = "git-fixup";
+    in
+      pkgs.rustPlatform.buildRustPackage {
+        inherit pname version;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "quodlibetor";
+          repo = "git-fixup";
+          rev = "v${version}";
+          hash = "sha256-vuLkbreW1K2KSW659UpWfbIUK+Nmyp1buvcG50CXIDQ=";
+        };
+
+        cargoHash = "sha256-sD3CQvUL3vcORCC3yFZ41uG2HZzPx/51wN27V9b/FRs=";
+      })
+  ];
 }
