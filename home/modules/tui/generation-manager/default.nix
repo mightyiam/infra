7b6206a16b621db773fc0726f275ecf905ef5c1f
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-24 16:24:34 +0700

    generation-manager

---
+++ home/modules/tui/generation-manager/default.nix
@@ -0,0 +1,10 @@
+instance: {pkgs, ...}: let
+  generation-manager = pkgs.rustPlatform.buildRustPackage {
+    pname = "generation-manager";
+    version = "1.0.0";
+    src = ./.;
+    cargoHash = "sha256-IrgdeufcGE4lQxTLz0m1mgIOCCsHCJdU77WOkV6ExvA=";
+  };
+in {
+  home.packages = [generation-manager];
+}
