2ce8e349f4532783fd3b898a375acbc55c1b1b72
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-29 08:02:34 +0700

    nvim-spectre

--- home/modules/tui/neovim/default.nix
+++ home/modules/tui/neovim/default.nix
@@ -254,6 +254,29 @@ with builtins;
           (omitPluginInVSCode which-key-nvim (embedLua ''
             require("which-key").setup {}
           ''))
+          (omitPluginInVSCode nvim-spectre (embedLua ''
+            require('spectre').setup()
+            local wk = require('which-key')
+
+            wk.register({
+              ["<leader>"] = {
+                s = {
+                  s = {
+                    '<cmd>lua require("spectre").toggle()<CR>',
+                    "Toggle Spectre",
+                  },
+                  w = {
+                    '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+                    "Search current word",
+                  },
+                  p = {
+                    '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+                    "Search current file",
+                  },
+                },
+              },
+            })
+          ''))
         ];
         extraConfig = concatStringsSep "\n" [
           ''
