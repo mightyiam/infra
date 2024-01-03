4ab099b1c24679d5d56c9ba6fd51bd3b405e4ee9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-01-03 14:50:56 +0700

    nvim RustParentModule binding

--- home/modules/tui/neovim/lua/lsp/rust.lua
+++ home/modules/tui/neovim/lua/lsp/rust.lua
@@ -1,5 +1,18 @@
 local function setup(lsp_zero)
   local options = lsp_zero.build_options("rust_analyzer", {
+    on_attach = function(client, bufnr)
+      require("which-key").register({
+        ["<leader>"] = {
+          r = {
+            name = "Rust",
+            p = {
+              "<CMD>RustParentModule<CR>",
+              "go to parent module",
+            },
+          },
+        },
+      })
+    end,
     settings = {
       ["rust-analyzer"] = {
         checkOnSave = {
