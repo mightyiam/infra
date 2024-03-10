2a8889ae63603b30258dd93a42247cc8306a2b13
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-03-10 09:46:09 +0700

    sink-rotate from nixpkgs

--- home/modules/gui/sink-rotate.nix
+++ home/modules/gui/sink-rotate.nix
@@ -3,30 +3,7 @@ instance: {
   config,
   ...
 }: let
-  sink-rotate = pkgs.rustPlatform.buildRustPackage {
-    pname = "sink-rotate";
-    version = "1.0.4";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "mightyiam";
-      repo = "sink-rotate";
-      rev = "v1.0.4";
-      hash = "sha256-q20uUr+7yLJlZc5YgEkY125YrZ2cuJrPv5IgWXaYRlo=";
-    };
-
-    cargoHash = "sha256-MPeyPTkxpi6iw/BT5m4S7jVBD0c2zG2rsv+UZWQxpUU=";
-
-    buildInputs = with pkgs; [pipewire wireplumber];
-
-    meta = with pkgs.lib; {
-      description = ''Command that sets "next" PipeWire audio sink as default'';
-      homepage = "https://github.com/mightyiam/sink-rotate";
-      license = licenses.mit;
-      maintainers = [];
-    };
-  };
-
   keyboard = import ../../keyboard.nix;
 in {
-  wayland.windowManager.sway.config.keybindings."${keyboard.wm.volume.sinkRotate}" = "exec ${sink-rotate}/bin/sink-rotate";
+  wayland.windowManager.sway.config.keybindings."${keyboard.wm.volume.sinkRotate}" = "exec ${pkgs.sink-rotate}/bin/sink-rotate";
 }
