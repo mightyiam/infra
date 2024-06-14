bd744eb5a6301c6b90f5fcc3c3d7ed4a0e3715e2
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-06-14 12:57:07 +0700

    mightyiam-desktop via flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  outputs = {...}: {
+    # Usage:
+    # ```nix
+    # {
+    #   inputs.nixconfigs.url = "path:/home/mightyiam/src/nixconfigs";
+    #   outputs = {
+    #     nixpkgs,
+    #     nixconfigs,
+    #     ...
+    #   }: let
+    #     inherit (nixpkgs.lib) nixosSystem;
+    #     inherit (nixconfigs.nixosModules) mightyiam-desktop;
+    #   in {
+    #     nixosConfigurations.mightyiam-desktop = nixosSystem mightyiam-desktop;
+    #   };
+    # }
+    # ```
+    nixosModules.mightyiam-desktop.modules = [./nixos-configurations/mightyiam-desktop.nix];
+  };
+}
--- nixos-configurations/mightyiam-desktop.nix
+++ nixos-configurations/mightyiam-desktop.nix
@@ -3,6 +3,8 @@
     ./common.nix
   ];
 
+  nixpkgs.hostPlatform = "x86_64-linux";
+
   # https://www.reddit.com/r/buildapc/comments/xypn1m/network_card_intel_ethernet_controller_i225v_igc/
   boot.kernelParams = ["pcie_port_pm=off" "pcie_aspm.policy=performance"];
 
