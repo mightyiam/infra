939e193e0d5c2824ec0012ae175bef89d4d84639
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-06-16 00:04:08 +0700

    reorder some attrs

--- nixos-modules/hosts/mightyiam-desktop/mobo.nix
+++ nixos-modules/hosts/mightyiam-desktop/mobo.nix
@@ -1,6 +1,6 @@
 {
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "sd_mod"];
+
   # https://www.reddit.com/r/buildapc/comments/xypn1m/network_card_intel_ethernet_controller_i225v_igc/
   boot.kernelParams = ["pcie_port_pm=off" "pcie_aspm.policy=performance"];
-
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "sd_mod"];
 }
