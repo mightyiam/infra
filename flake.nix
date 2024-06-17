6348a34b8c7aa7b189f9f5d9281ff995236438a8
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-06-17 12:59:38 +0700

    home-manager config flake output

--- flake.nix
+++ flake.nix
@@ -18,5 +18,37 @@
     # }
     # ```
     nixosModules.mightyiam-desktop.imports = [./nixos-modules/hosts/mightyiam-desktop];
+    # Usage:
+    # ```nix
+    # {
+    #   inputs.catppuccin.url = "github:catppuccin/nix";
+    #   inputs.nixconfigs.url = "path:/home/mightyiam/src/nixconfigs";
+    #   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #   inputs.home-manager.url = "github:nix-community/home-manager";
+    #   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    #
+    #   outputs = {
+    #     nixconfigs,
+    #     nixpkgs,
+    #     home-manager,
+    #     catppuccin,
+    #     ...
+    #   }: {
+    #     homeConfigurations.mightyiam = home-manager.lib.homeManagerConfiguration {
+    #       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    #
+    #       modules = [
+    #         catppuccin.homeManagerModules.catppuccin
+    #         nixconfigs.homeManagerModules.mightyiam
+    #         {
+    #           location.latitude = 18.746;
+    #           location.longitude = 99.075;
+    #         }
+    #       ];
+    #     };
+    #   };
+    # }
+    # ```
+    homeManagerModules.mightyiam.imports = [./home/home.nix];
   };
 }
--- home/modules/catppuccin.nix
+++ home/modules/catppuccin.nix
@@ -1,5 +1,4 @@
 {
-  imports = [<catppuccin/modules/home-manager>];
   catppuccin.flavor = "mocha";
   catppuccin.enable = true;
   programs.chromium.extensions = [{id = "bkkmolkhemgaeaeggcmfbghljjjoofoh";}];
