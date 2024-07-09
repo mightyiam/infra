6164aac29ef261f627d0d6a7e8bca832e1bb8056
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-09 11:13:18 +0700

    comment out nvim-spectre

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -384,29 +384,29 @@ in {
         autocmd VimEnter * nested WatchForChangesAllFile!
       '')
       (omitPluginInVSCode vimPlugins.unicode-vim "")
-      (omitPluginInVSCode vimPlugins.nvim-spectre (embedLua ''
-        require('spectre').setup()
-        local wk = require('which-key')
-
-        wk.register({
-          ["<leader>"] = {
-            s = {
-              s = {
-                '<cmd>lua require("spectre").toggle()<CR>',
-                "Toggle Spectre",
-              },
-              w = {
-                '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
-                "Search current word",
-              },
-              p = {
-                '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
-                "Search current file",
-              },
-            },
-          },
-        })
-      ''))
+      # (omitPluginInVSCode vimPlugins.nvim-spectre (embedLua ''
+      #   require('spectre').setup()
+      #   local wk = require('which-key')
+      #
+      #   wk.register({
+      #     ["<leader>"] = {
+      #       s = {
+      #         s = {
+      #           '<cmd>lua require("spectre").toggle()<CR>',
+      #           "Toggle Spectre",
+      #         },
+      #         w = {
+      #           '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+      #           "Search current word",
+      #         },
+      #         p = {
+      #           '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+      #           "Search current file",
+      #         },
+      #       },
+      #     },
+      #   })
+      # ''))
     ];
     extraConfig = concatStringsSep "\n" [
       ''
