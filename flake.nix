2bfe81a5d89fc1bdfce25b400a3f8b2a3e9a3ef2
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-12 13:23:52 +0700

    slight syntax refactor

--- flake.nix
+++ flake.nix
@@ -20,38 +20,32 @@
       ];
 
       flake = {
-        nixosModules.termitomyces = {
-          imports = [
-            ./nixos-modules/hosts/termitomyces
-            inputs.catppuccin.nixosModules.catppuccin
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.users.mightyiam.imports = [
-                self.homeManagerModules.mightyiam
-              ];
-            }
-          ];
-        };
+        nixosModules.termitomyces.imports = [
+          ./nixos-modules/hosts/termitomyces
+          inputs.catppuccin.nixosModules.catppuccin
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.users.mightyiam.imports = [
+              self.homeManagerModules.mightyiam
+            ];
+          }
+        ];
 
-        nixosModules.ganoderma = {
-          imports = [
-            ./nixos-modules/hosts/ganoderma
-            inputs.catppuccin.nixosModules.catppuccin
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.users.mightyiam.imports = [
-                self.homeManagerModules.mightyiam
-              ];
-            }
-          ];
-        };
+        nixosModules.ganoderma.imports = [
+          ./nixos-modules/hosts/ganoderma
+          inputs.catppuccin.nixosModules.catppuccin
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.users.mightyiam.imports = [
+              self.homeManagerModules.mightyiam
+            ];
+          }
+        ];
 
-        homeManagerModules.mightyiam = {
-          imports = [
-            inputs.catppuccin.homeManagerModules.catppuccin
-            ./home/home.nix
-          ];
-        };
+        homeManagerModules.mightyiam.imports = [
+          inputs.catppuccin.homeManagerModules.catppuccin
+          ./home/home.nix
+        ];
       };
 
       systems = [
