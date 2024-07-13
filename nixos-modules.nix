2391c99bef35a06b06326db74ea4c0f167819eb4
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-13 09:00:39 +0700

    refactor nixosModules mapping

--- nixos-modules.nix
+++ nixos-modules.nix
@@ -3,19 +3,23 @@
   inputs,
   ...
 }: {
-  flake.nixosModules = lib.pipe ./nixos-modules/hosts [
+  imports = lib.pipe ./nixos-modules/hosts [
     builtins.readDir
-    (lib.mapAttrs (hostname: _: {
-      imports = [
-        inputs.catppuccin.nixosModules.catppuccin
-        inputs.home-manager.nixosModules.home-manager
-        {
-          home-manager.users.mightyiam.imports = [
-            inputs.self.homeManagerModules.mightyiam
-          ];
-        }
-        (./nixos-modules/hosts + "/${hostname}")
-      ];
+    lib.attrNames
+
+    (map (hostname: {
+      flake.nixosModules.${hostname} = {
+        imports = [
+          inputs.catppuccin.nixosModules.catppuccin
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.users.mightyiam.imports = [
+              inputs.self.homeManagerModules.mightyiam
+            ];
+          }
+          (./nixos-modules/hosts + "/${hostname}")
+        ];
+      };
     }))
   ];
 }
