71b7235dc60819c06cd4117c8ced2decbb09734f
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-13 20:44:48 +0700

    no default nix package in home

--- home/modules/nix.nix
+++ home/modules/nix.nix
@@ -1,10 +1,4 @@
 {
-  pkgs,
-  lib,
-  ...
-}: {
-  # mkDeafult to avoid conflict with usage as NixOS module
-  nix.package = lib.mkDefault pkgs.nix;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.keep-outputs = true;
 }
