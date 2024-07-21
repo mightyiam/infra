5d3019d2b305cd2f54def9ade3ee88692f97089b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-21 23:07:07 +0700

    termitomyces single fs

--- nixos-configurations/termitomyces/filesystems.nix
+++ nixos-configurations/termitomyces/filesystems.nix
@@ -1,43 +1,6 @@
 {
-  fileSystems = {
-    "/" = {
-      device = "nixos/root";
-      fsType = "zfs";
-    };
-
-    "/etc" = {
-      device = "nixos/root/etc";
-      fsType = "zfs";
-    };
-
-    "/nix" = {
-      device = "nixos/root/nix";
-      fsType = "zfs";
-    };
-
-    "/var" = {
-      device = "nixos/root/var";
-      fsType = "zfs";
-    };
-
-    "/bin" = {
-      device = "nixos/root/bin";
-      fsType = "zfs";
-    };
-
-    "/home" = {
-      device = "nixos/root/home";
-      fsType = "zfs";
-    };
-
-    "/root" = {
-      device = "nixos/root/root";
-      fsType = "zfs";
-    };
-
-    "/sbin" = {
-      device = "nixos/root/sbin";
-      fsType = "zfs";
-    };
+  fileSystems."/" = {
+    device = "storage/root";
+    fsType = "zfs";
   };
 }
