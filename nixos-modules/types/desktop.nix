631885bbfeaf693e3b6eff888b9a077e4c1e9074
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-22 08:52:24 +0700

    sort an imports list

--- nixos-modules/types/desktop.nix
+++ nixos-modules/types/desktop.nix
@@ -1,32 +1,32 @@
 {
   imports = [
+    ../modules/1bowapinya.nix
+    ../modules/allow-unfree-packages.nix
     ../modules/bluetooth.nix
     ../modules/boot-message.nix
+    ../modules/catppuccin.nix
+    ../modules/dconf.nix
     ../modules/editor.nix
     ../modules/firmware.nix
     ../modules/flakes.nix
     ../modules/flipper-zero.nix
     ../modules/fonts.nix
+    ../modules/graphics.nix
     ../modules/grub.nix
     ../modules/home-related-hacks.nix
     ../modules/home.nix
+    ../modules/mightyiam.nix
     ../modules/networking.nix
     ../modules/nix.nix
-    ../modules/graphics.nix
     ../modules/pipewire.nix
     ../modules/printing.nix
     ../modules/service-discovery.nix
     ../modules/shell.nix
     ../modules/steam.nix
-    ../modules/sudo.nix
     ../modules/storage.nix
+    ../modules/sudo.nix
     ../modules/tmp.nix
-    ../modules/mightyiam.nix
     ../modules/virtualbox.nix
     ../modules/zfs.nix
-    ../modules/dconf.nix
-    ../modules/catppuccin.nix
-    ../modules/1bowapinya.nix
-    ../modules/allow-unfree-packages.nix
   ];
 }
