8cbdca8e83881954581a3c9b532474a2326e5dc5
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-08-05 07:28:39 +0000

    home nix.package is mkDefault

--- home-manager-modules/users/mightyiam/modules/nix.nix
+++ home-manager-modules/users/mightyiam/modules/nix.nix
@@ -1,5 +1,9 @@
-{pkgs, ...}: {
-  nix.package = pkgs.nixVersions.latest;
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  nix.package = lib.mkDefault pkgs.nixVersions.latest;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.keep-outputs = true;
 }
