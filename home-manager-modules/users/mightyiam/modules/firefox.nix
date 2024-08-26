a6d1ac0f6d29249909df5afbadf32952cc1a71ad
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-08-26 14:28:59 +0700

    refmt some file

--- home-manager-modules/users/mightyiam/modules/firefox.nix
+++ home-manager-modules/users/mightyiam/modules/firefox.nix
@@ -3,12 +3,17 @@ let
   inherit (lib) mkIf;
 in
 mkIf config.gui.enable {
-  programs.firefox.enable = true;
-  programs.firefox.profiles.primary.id = 0;
-  programs.firefox.profiles.primary.settings."toolkit.legacyUserProfileCustomizations.stylesheets" =
-    true;
-  programs.firefox.profiles.primary.settings."browser.ctrlTab.sortByRecentlyUsed" = true;
-  programs.firefox.profiles.primary.settings."browser.tabs.closeWindowWithLastTab" = false;
-  programs.firefox.profiles.primary.userChrome = '''';
-  programs.firefox.profiles.primary.userContent = '''';
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      primary = {
+        id = 0;
+        settings."toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        settings."browser.ctrlTab.sortByRecentlyUsed" = true;
+        settings."browser.tabs.closeWindowWithLastTab" = false;
+        userChrome = '''';
+        userContent = '''';
+      };
+    };
+  };
 }
