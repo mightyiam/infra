588e50428935acab7e1bf9d8e6225cb4d482c5e4
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-09-01 20:22:49 +0700

    nix.nixPath nixpkgs from inputs

--- nixos-modules/modules/nix.nix
+++ nixos-modules/modules/nix.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, self, ... }:
 {
   nix.package = pkgs.nixVersions.latest;
   nix.settings.auto-optimise-store = true;
+  nix.nixPath = [
+    "nixpkgs=${self.inputs.nixpkgs}"
+  ];
 }
