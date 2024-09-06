decb228230472dc23bd1936f12b084a5d51ae187
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-09-06 11:30:45 +0700

    workaround https://github.com/NixOS/nix/issues/11358

--- nixos-modules/modules/nix.nix
+++ nixos-modules/modules/nix.nix
@@ -1,6 +1,30 @@
-{ pkgs, self, ... }:
 {
-  nix.package = pkgs.nixVersions.latest;
+  pkgs,
+  self,
+  lib,
+  ...
+}:
+let
+  package = pkgs.nixVersions.latest;
+in
+{
+  # workaround https://github.com/NixOS/nix/issues/11358
+  nix.package = pkgs.stdenv.mkDerivation {
+    name = "nix-with-workaround";
+    version = package.version;
+    runtimeInputs = [ package ];
+    unpackPhase = "true";
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    buildPhase = ''
+      cp -r ${package} $out
+      chmod -R +w $out
+      wrapProgram $out/bin/nix \
+        --set GIT_CONFIG_COUNT 1 \
+        --set GIT_CONFIG_KEY_0 safe.bareRepository \
+        --set GIT_CONFIG_VALUE_0 all
+    '';
+    meta.mainProgram = "nix";
+  };
   nix.settings.auto-optimise-store = true;
   nix.nixPath = [
     "nixpkgs=${self.inputs.nixpkgs}"
