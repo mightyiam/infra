ff99b96fe0f6b7ad7be20995030c945597fb8c81
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-09-20 15:44:28 +0700

    sshd after tailscaled

--- nixos-modules/modules/tailscale.nix
+++ nixos-modules/modules/tailscale.nix
@@ -1,5 +1,29 @@
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   services.tailscale = {
     enable = true;
+  };
+  systemd.services.sshd = {
+    after = [ "tailscaled.service" ];
+    serviceConfig.ExecStartPre = lib.getExe (
+      pkgs.writeShellApplication {
+        name = "wait-for-tailscale";
+        runtimeInputs = [
+          pkgs.gnugrep
+          pkgs.tailscale
+        ];
+        text = ''
+          until tailscale status | grep --quiet ${config.networking.hostName}
+          do
+            sleep 0.3
+          done
+        '';
+      }
+    );
   };
 }
