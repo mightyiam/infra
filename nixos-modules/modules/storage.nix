7fd5e8fb207d2c1b7b987aff538d6874074cdb3b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-09-23 15:51:20 +0700

    default kernelPackages

--- nixos-modules/modules/storage.nix
+++ nixos-modules/modules/storage.nix
@@ -1,6 +1,5 @@
 { config, ... }:
 {
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   boot.zfs.forceImportRoot = false;
   services.zfs.autoScrub.enable = true;
   services.zfs.autoScrub.interval = "monthly";
