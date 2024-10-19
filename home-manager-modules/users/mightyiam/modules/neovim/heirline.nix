6303d4ce9217e73d9b1eefbb1b818d171353c214
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-19 20:02:40 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:home-manager-modules/users/mightyiam/modules/neovim/heirline.nix

--- home-manager-modules/users/mightyiam/modules/neovim/heirline.nix
+++ home-manager-modules/users/mightyiam/modules/neovim/heirline.nix
@@ -1,5 +1,52 @@
 {
-	programs.nixvim.plugins.lualine = {
-		enable = true;
-	};
+  programs.nixvim.plugins.lualine = {
+    enable = true;
+    settings = {
+      options = {
+        globalstatus = true;
+        section_separators = "";
+        component_separators = "";
+
+        disabled_filetypes = {
+          winbar = [
+            "Trouble"
+          ];
+        };
+
+        ignore_focus = ["Trouble"];
+      };
+
+      extensions = ["trouble"];
+
+      winbar = {
+           lualine_a = [
+             "encoding"
+             "fileformat"
+           ];
+           lualine_b = [
+             "filetype"
+           ];
+           lualine_c = [
+             {
+            "filename" = null;
+               path = 1;
+               shorting_target = 0;
+             }
+           ];
+           lualine_x = [
+             "diff"
+           ];
+           lualine_y = [
+             "%o"
+             "location"
+           ];
+           lualine_z = [
+             "progress"
+           ];
+         };
+
+      sections = {
+      };
+    };
+  };
 }
