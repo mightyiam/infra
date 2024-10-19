8ac0e036715b7f13d55b365cee6008b64a0340d6
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-19 16:17:25 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:home-manager-modules/users/mightyiam/modules/neovim/flash.nix

--- home-manager-modules/users/mightyiam/modules/neovim/flash.nix
+++ home-manager-modules/users/mightyiam/modules/neovim/flash.nix
@@ -1,13 +1,49 @@
-{config, ...}: {
+{ config, ... }:
+{
   programs.nixvim = {
-	  plugins.flash.enable = true;
-	  keymaps = [
-	  {
-	    key = "s";
-	    mode = ["n" "x" "o"];
-	    action = config.lib.nixvim.mkRaw ''function() require("flash").jump() end'';
-	  options.desc = "Flash";
-	  }
-	  ];
+    plugins.flash.enable = true;
+    keymaps = [
+      {
+        key = "s";
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
+        action = config.lib.nixvim.mkRaw ''function() require("flash").jump() end'';
+        options.desc = "Flash";
+      }
+      {
+        key = "S";
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
+        action = config.lib.nixvim.mkRaw ''function() require("flash").treesitter() end'';
+        options.desc = "Flash Treesitter";
+      }
+      {
+        key = "r";
+        mode = "o";
+        action = config.lib.nixvim.mkRaw ''function() require("flash").remote() end'';
+        options.desc = "Remote Flash";
+      }
+      {
+        key = "R";
+        mode = [
+          "o"
+          "x"
+        ];
+        action = config.lib.nixvim.mkRaw ''function() require("flash").treesitter_search() end'';
+        options.desc = "Treesitter search";
+      }
+      {
+        key = "<c-s>";
+        mode = "c";
+        action = config.lib.nixvim.mkRaw ''function() require("flash").toggle() end'';
+        options.desc = "Toggle Flash search";
+      }
+    ];
   };
 }
