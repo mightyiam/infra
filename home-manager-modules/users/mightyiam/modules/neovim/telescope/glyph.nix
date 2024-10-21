340806118538a66d0390881d8c1c58109b96ee3f
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-21 18:58:38 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:home-manager-modules/users/mightyiam/modules/neovim/telescope/glyph.nix

--- home-manager-modules/users/mightyiam/modules/neovim/telescope/glyph.nix
+++ home-manager-modules/users/mightyiam/modules/neovim/telescope/glyph.nix
@@ -1,16 +1,24 @@
-{
+{pkgs, ...}: {
   programs.nixvim = {
-		extraPackages = [
-
-		];
-		plugins.telescope = {
-enabledExtensions = ["glyph"];
-			keymaps = {
-				"<leader>fu" = {
-					action = "glyph";
-				};
-			};
-		};
-	};
+    extraPackages = [
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "telescope-glyph-nvim";
+        version = "unstable-2022-08-22";
+        src = pkgs.fetchFromGitHub {
+          owner = "alduraibi";
+          repo = "telescope-glyph.nvim";
+          rev = "f63f01e129e71cc25b79637610674bbf0be5ce9d";
+          hash = "";
+        };
+      })
+    ];
+    plugins.telescope = {
+      enabledExtensions = [ "glyph" ];
+      keymaps = {
+        "<leader>fu" = {
+          action = "glyph";
+        };
+      };
+    };
+  };
 }
-
