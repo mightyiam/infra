184c5c87721f52ea25d7d6581fec481b1ed678c9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-25 16:04:44 +0700

    format inputs

--- flake.nix
+++ flake.nix
@@ -1,35 +1,68 @@
 {
   nixConfig.extra-experimental-features = [ "pipe-operators" ];
+
   inputs = {
     # https://github.com/catppuccin/nix/pull/358
     catppuccin.url = "github:/ryand56/catppuccin-nix/home-manager-kvantum-fix"; # "github:catppuccin/nix"
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
-    devshell.url = "github:numtide/devshell";
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-on-droid.inputs.home-manager.follows = "home-manager";
-    nix-on-droid.inputs.nixpkgs-docs.follows = "nixpkgs";
-    nix-on-droid.inputs.nixpkgs-for-bootstrap.follows = "nixpkgs";
-    nix-on-droid.inputs.nixpkgs.follows = "nixpkgs";
-    nix-on-droid.url = "github:nix-community/nix-on-droid";
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid";
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs-docs.follows = "nixpkgs";
+        nixpkgs-for-bootstrap.follows = "nixpkgs";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-    nixvim.inputs.devshell.follows = "devshell";
-    nixvim.inputs.flake-parts.follows = "flake-parts";
-    nixvim.inputs.home-manager.follows = "home-manager";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.inputs.treefmt-nix.follows = "treefmt-nix";
-    nixvim.url = "github:nix-community/nixvim";
-    sink-rotate.inputs.flake-parts.follows = "flake-parts";
-    sink-rotate.inputs.nixpkgs.follows = "nixpkgs";
-    sink-rotate.inputs.treefmt-nix.follows = "treefmt-nix";
-    sink-rotate.url = "github:mightyiam/sink-rotate";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs = {
+        devshell.follows = "devshell";
+        flake-parts.follows = "flake-parts";
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    sink-rotate = {
+      url = "github:mightyiam/sink-rotate";
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inputs = inputs; } {
