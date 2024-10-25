780a7ad3dc9cd71521c8dba546d75c35fd74dd59
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-25 18:54:59 +0700

    set version of vim-autoread to unstable

--- nixvim/autoread.nix
+++ nixvim/autoread.nix
@@ -4,7 +4,7 @@
     {
       plugin = pkgs.vimUtils.buildVimPlugin {
         pname = "vim-autoread";
-        version = "24061f84652d768bfb85d222c88580b3af138dab";
+        version = "unstable";
         src = self.inputs.vim-autoread;
       };
       config = ''
