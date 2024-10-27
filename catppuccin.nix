ab05128479cc452a6192d6fd082aeaf2143c6dbb
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-27 15:30:43 +0700

    support multiple common modules

--- catppuccin.nix
+++ catppuccin.nix
@@ -4,32 +4,36 @@ let
 in
 {
   flake = {
-    nixosModules.common = {
-      imports = [ self.inputs.catppuccin.nixosModules.catppuccin ];
-      catppuccin = {
-        enable = true;
-        inherit flavor;
-      };
-    };
-    homeManagerModules.common = {
-      imports = [ self.inputs.catppuccin.homeManagerModules.catppuccin ];
-      catppuccin = {
-        enable = true;
-        inherit flavor;
-        pointerCursor.enable = true;
-      };
+    nixosModules.common.imports = [
+      {
+        imports = [ self.inputs.catppuccin.nixosModules.catppuccin ];
+        catppuccin = {
+          enable = true;
+          inherit flavor;
+        };
+      }
+    ];
+    homeManagerModules.common.imports = [
+      {
+        imports = [ self.inputs.catppuccin.homeManagerModules.catppuccin ];
+        catppuccin = {
+          enable = true;
+          inherit flavor;
+          pointerCursor.enable = true;
+        };
 
-      # IFD
-      programs.swaylock.catppuccin.enable = false;
-      services.mako.catppuccin.enable = false;
+        # IFD
+        programs.swaylock.catppuccin.enable = false;
+        services.mako.catppuccin.enable = false;
 
-      programs.chromium.extensions = [ { id = "bkkmolkhemgaeaeggcmfbghljjjoofoh"; } ];
-      gtk.gtk2.extraConfig = ''
-        gtk-theme-name="Adwaita-dark"
-      '';
-      gtk.gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-      gtk.gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
-    };
+        programs.chromium.extensions = [ { id = "bkkmolkhemgaeaeggcmfbghljjjoofoh"; } ];
+        gtk.gtk2.extraConfig = ''
+          gtk-theme-name="Adwaita-dark"
+        '';
+        gtk.gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+        gtk.gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+      }
+    ];
   };
 
   nixvimModules.default.colorschemes.catppuccin = {
