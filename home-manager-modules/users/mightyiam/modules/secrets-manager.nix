dd975343bf21e5b51dd0d4eff360cce5a0c1a0ba
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-21 00:42:22 +0700

    rofi-rbw no keybindings for copying

--- home-manager-modules/users/mightyiam/modules/secrets-manager.nix
+++ home-manager-modules/users/mightyiam/modules/secrets-manager.nix
@@ -14,6 +14,25 @@
       };
     };
     home.packages = [ pkgs.rofi-rbw-wayland ];
+    xdg.configFile."rofi-rbw.rc".text = lib.generators.toINIWithGlobalSection { } {
+      globalSection = {
+        keybindings = lib.concatStringsSep "," [
+          "Alt+1:type:username:tab:password"
+          "Alt+2:type:username"
+          "Alt+3:type:password"
+          "Alt+4:type:totp"
+          # "Alt+c:copy:password"
+          "Alt+u:copy:username"
+          # "Alt+t:copy:totp"
+          "Alt+m::menu"
+          "Alt+s:sync"
+        ];
+        menu-keybindings = lib.concatStringsSep "," [
+          "Alt+t:type"
+          # "Alt+c:copy"
+        ];
+      };
+    };
   };
   options.secretsMenu = lib.mkOption {
     type = lib.types.path;
