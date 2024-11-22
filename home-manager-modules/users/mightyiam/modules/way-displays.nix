2b4f94853b97d785157a8c184a994627bde0de2a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-23 03:14:35 +0700

    way-displays command not always

--- home-manager-modules/users/mightyiam/modules/way-displays.nix
+++ home-manager-modules/users/mightyiam/modules/way-displays.nix
@@ -10,19 +10,10 @@ let
   exe = "${lib.getBin pkgs.way-displays}/bin/way-displays";
 in
 lib.mkIf config.gui.enable {
-  wayland.windowManager.sway.config.startup = [
-    {
-      command = "${lib.getBin pkgs.coreutils}/bin/mkdir -p ${dir}";
-      always = true;
-    }
-    {
-      command = "${lib.getBin pkgs.coreutils}/bin/touch ${file}";
-      always = true;
-    }
-    {
-      command = "${exe} -c ${file} > /tmp/way-displays.$XDG_VTNR.$USER.log 2>&1";
-      always = true;
-    }
+  wayland.windowManager.sway.config.startup = map (command: { inherit command; }) [
+    "${lib.getBin pkgs.coreutils}/bin/mkdir -p ${dir}"
+    "${lib.getBin pkgs.coreutils}/bin/touch ${file}"
+    "${exe} -c ${file} > /tmp/way-displays.$XDG_VTNR.$USER.log 2>&1"
   ];
 
   home.packages = [ pkgs.way-displays ];
