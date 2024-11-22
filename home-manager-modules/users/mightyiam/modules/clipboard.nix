91ad3752605a663f379f79964d961ec8e2732204
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-23 03:01:18 +0700

    cliphist

---
+++ home-manager-modules/users/mightyiam/modules/clipboard.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  mod = config.wayland.windowManager.sway.config.modifier;
+
+  rofi-cliphist = pkgs.writeShellApplication {
+    name = "rofi-cliphist";
+    runtimeInputs = [
+      config.services.cliphist.package
+      config.programs.rofi.package
+    ];
+    text = ''
+      prompt=$(if [ "$1" == "copy" ]; then echo ' 󰆏 '; else echo '   '; fi)
+      content=$(cliphist list | rofi -dmenu -p "$prompt")
+      decoded=$(cliphist decode <<<"$content")
+
+      if [ "$1" == "copy" ]; then
+        echo "$decoded" | wl-copy
+      else
+        echo "type $decoded" | ${config.dotoolc}
+      fi
+    '';
+  };
+in
+{
+  services.cliphist.enable = true;
+
+  wayland.windowManager.sway.config.keybindings = {
+    "--no-repeat ${mod}+p" = "exec ${lib.getExe rofi-cliphist} copy";
+    "--no-repeat ${mod}+Shift+p" = "exec ${lib.getExe rofi-cliphist} type";
+  };
+}
