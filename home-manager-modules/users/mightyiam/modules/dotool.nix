834b36cd8d59b04141e6680c97b0035decf5cb8a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-22 21:21:02 +0700

    rofi-type

---
+++ home-manager-modules/users/mightyiam/modules/dotool.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  package = pkgs.dotool;
+in
+{
+  options.dotoolc = lib.mkOption {
+    type = lib.types.pathInStore;
+    default = lib.getExe' package "dotoolc";
+  };
+  config = {
+    home.packages = [ package ];
+    wayland.windowManager.sway.extraConfig = "exec ${lib.getExe' package "dotoold"}";
+  };
+}
