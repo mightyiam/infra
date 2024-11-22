94249677932594b67207b34c2c03115270bb42d6
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-22 21:19:47 +0700

    rofi-type

---
+++ home-manager-modules/users/mightyiam/modules/dotool.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  package = pkgs.dotool;
+in
+{
+  options.dotoolc = lib.mkOption {
+    type = lib.types.pathInStore;
+    default = lib.getExe' package "dotoolc";
+  };
+  config = {
+    home.packages = [ package ];
+    wayland.windowManager.sway.extraConfig = "exec ${lib.getExe' package "dotoold"}";
+  };
+}
---
+++ home-manager-modules/users/mightyiam/modules/rofi-type.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  rofi-type = pkgs.writeShellScript "rofi-type" ''
+    CMD=$(${lib.getExe config.programs.rofi.package} -dmenu -p '   ')
+    echo "type $($CMD)" | ${config.dotoolc}
+  '';
+
+  mod = config.wayland.windowManager.sway.config.modifier;
+in
+{
+  wayland.windowManager.sway.config.keybindings."--no-repeat ${mod}+t" = "exec ${rofi-type}";
+}
