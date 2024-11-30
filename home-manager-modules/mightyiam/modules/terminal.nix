d4e277e69950d46e66db5e263c8ef91888d95a0d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-30 19:09:36 +0700

    style:

--- home-manager-modules/mightyiam/modules/terminal.nix
+++ home-manager-modules/mightyiam/modules/terminal.nix
@@ -21,10 +21,12 @@
       enable = true;
       settings = {
         general.live_config_reload = true;
-        window.decorations = "none";
-        window.dynamic_title = true;
-        window.opacity = config.style.windowOpacity;
-        window.title = "Terminal";
+        window = {
+          decorations = "none";
+          dynamic_title = true;
+          opacity = config.style.windowOpacity;
+          title = "Terminal";
+        };
         bell = {
           duration = builtins.ceil config.style.bellDuration;
           color = "#000000";
