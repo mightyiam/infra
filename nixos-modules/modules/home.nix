98d14e9d7a9b91e3054174c31edc328b107184e7
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-01 19:00:12 +0700

    style:

--- nixos-modules/modules/home.nix
+++ nixos-modules/modules/home.nix
@@ -1,18 +1,20 @@
 { self, ... }:
 {
   imports = [ self.inputs.home-manager.nixosModules.home-manager ];
-  home-manager.useGlobalPkgs = true;
-  home-manager.users.mightyiam.imports = [
-    self.homeManagerModules.common
-    ../../home-manager-modules/mightyiam
-    (
-      { osConfig, ... }:
-      {
-        home.stateVersion = osConfig.system.stateVersion;
-      }
-    )
-  ];
-  home-manager.extraSpecialArgs = {
-    inherit self;
+  home-manager = {
+    useGlobalPkgs = true;
+    users.mightyiam.imports = [
+      self.homeManagerModules.common
+      ../../home-manager-modules/mightyiam
+      (
+        { osConfig, ... }:
+        {
+          home.stateVersion = osConfig.system.stateVersion;
+        }
+      )
+    ];
+    extraSpecialArgs = {
+      inherit self;
+    };
   };
 }
