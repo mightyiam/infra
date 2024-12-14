553ee16e1bd928ab4c9398e43f1aa1398889c7fb
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-14 15:58:22 +0700

    swayr switch-window workspace focused

--- home-manager-modules/mightyiam/modules/window-switcher.nix
+++ home-manager-modules/mightyiam/modules/window-switcher.nix
@@ -1,7 +1,29 @@
 # icons don't work because arbitrary codepoints cannot be expressed in Nix string literals
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   programs.swayr = {
+    package =
+      let
+        src = pkgs.fetchFromSourcehut {
+          owner = "~tsdh";
+          repo = "swayr";
+          rev = "0512a9fadc43c64f25afbc9db39c1286b4ac1a8c";
+          sha256 = "sha256-tuqSt85wthhe7ywTjRvXz1vi1/eo9rqm7idecQlycWs=";
+        };
+      in
+      pkgs.swayr.overrideAttrs (orig: {
+        version = "unstable-2024-12-04";
+        inherit src;
+        cargoDeps = orig.cargoDeps.overrideAttrs {
+          inherit src;
+          outputHash = "sha256-eK9EJjTnjkIeFaWDO2bB3q+1IDu7hzUB44e5ZCmCj8c=";
+        };
+      });
     enable = true;
     systemd.enable = true;
     # https://sr.ht/~tsdh/swayr/#swayr-configuration
@@ -39,7 +61,7 @@
       swayr = lib.getExe config.programs.swayr.package;
     in
     {
-      "--no-repeat ${mod}+semicolon" = "exec ${swayr} switch-to";
+      "--no-repeat ${mod}+semicolon" = "exec ${swayr} switch-window '[workspace=__focused__]'";
       "--no-repeat ${mod}+tab" = "exec ${swayr} switch-to-urgent-or-lru-window";
     };
 }
