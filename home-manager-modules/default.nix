4754089c499bcef8279ad3933d1bcb7ee8e7801d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-23 04:00:12 +0700

    move some location options around

--- home-manager-modules/default.nix
+++ home-manager-modules/default.nix
@@ -10,17 +10,6 @@
       default = true;
     };
 
-    location = {
-      latitude = lib.mkOption {
-        type = lib.types.numbers.between (-90) 90;
-        default = 18.746;
-      };
-      longitude = lib.mkOption {
-        type = lib.types.numbers.between (-180) 180;
-        default = 99.075;
-      };
-    };
-
     style = {
       windowOpacity = lib.mkOption {
         type = lib.types.numbers.between 0 1.0;
