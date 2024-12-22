6f604c1ecc274b6b92b4e805c03c10ff6235bf91
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-23 03:56:00 +0700

    more some options around

--- home-manager-modules/fontconfig.nix
+++ home-manager-modules/fontconfig.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   expandPrefer = family: "<family>${family}</family>";
   expandAlias =
@@ -22,9 +27,76 @@ let
       ''</fontconfig>''
     ];
 in
-lib.mkIf config.gui.enable {
-  fonts.fontconfig.enable = true;
-  home.file."${config.xdg.configHome}/fontconfig/fonts.conf" = {
-    text = fontsConf { inherit (config.gui.fonts) aliases; };
+{
+  options.gui.fonts = {
+    packages = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = with pkgs; [
+        open-dyslexic
+        nerd-fonts.open-dyslexic
+        font-awesome
+        noto-fonts
+        noto-fonts-emoji
+      ];
+    };
+
+    default = {
+      family = lib.mkOption {
+        type = lib.types.str;
+        default = "sans-serif";
+      };
+
+      size = lib.mkOption {
+        type = lib.types.float;
+        default = 10.0;
+      };
+    };
+
+    monospace = {
+      family = lib.mkOption {
+        type = lib.types.str;
+        default = "monospace";
+      };
+
+      size = lib.mkOption {
+        type = lib.types.float;
+        default = 10.0;
+      };
+    };
+
+    aliases = lib.mkOption {
+      type = lib.types.listOf (
+        lib.types.submodule {
+          options.family = lib.mkOption { type = lib.types.str; };
+
+          options.prefer = lib.mkOption { type = lib.types.listOf lib.types.str; };
+        }
+      );
+
+      default = [
+        {
+          family = "monospace";
+          prefer = [
+            "OpenDyslexicM Nerd Font"
+            "Noto Color Emoji"
+          ];
+        }
+        {
+          family = "sans-serif";
+          prefer = [ "OpenDyslexic" ];
+        }
+        {
+          family = "serif";
+          prefer = [ "OpenDyslexic" ];
+        }
+      ];
+    };
+  };
+
+  config = lib.mkIf config.gui.enable {
+    fonts.fontconfig.enable = true;
+    home.file."${config.xdg.configHome}/fontconfig/fonts.conf" = {
+      text = fontsConf { inherit (config.gui.fonts) aliases; };
+    };
   };
 }
