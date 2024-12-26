46881d5f0a9c22de3af5601c159961876c940f84
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-26 14:16:03 +0700

    correct some incorrect getBin usages

--- home-manager-modules/way-displays.nix
+++ home-manager-modules/way-displays.nix
@@ -7,13 +7,12 @@
 let
   dir = "$XDG_CONFIG_HOME/way-displays";
   file = "${dir}/cfg.yaml";
-  exe = "${lib.getBin pkgs.way-displays}/bin/way-displays";
 in
 lib.mkIf config.gui.enable {
   wayland.windowManager.sway.config.startup = map (command: { inherit command; }) [
-    "${lib.getBin pkgs.coreutils}/bin/mkdir -p ${dir}"
-    "${lib.getBin pkgs.coreutils}/bin/touch ${file}"
-    "${exe} -c ${file} > /tmp/way-displays.$XDG_VTNR.$USER.log 2>&1"
+    "${lib.getExe' pkgs.coreutils "mkdir"} -p ${dir}"
+    "${lib.getExe' pkgs.coreutils "touch"} ${file}"
+    "${lib.getExe pkgs.way-displays} -c ${file} > /tmp/way-displays.$XDG_VTNR.$USER.log 2>&1"
   ];
 
   home.packages = [ pkgs.way-displays ];
