5d6d0172de739d8402fea40d4e90e9929c128209
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-12-26 14:02:02 +0700

    use mkMerge instead of imports in nixos-configurations

--- nixos-configurations/default.nix
+++ nixos-configurations/default.nix
@@ -5,28 +5,26 @@
   util,
   ...
 }:
-{
-  imports =
-    util.readModulesDir ./.
-    |> lib.mapAttrsToList (
-      hostName: path: {
-        flake =
-          let
-            nixosConfiguration = inputs.nixpkgs.lib.nixosSystem {
-              specialArgs = { inherit self; };
-              modules = [
-                path
-                { networking = { inherit hostName; }; }
-              ];
-            };
+util.readModulesDir ./.
+|> lib.mapAttrsToList (
+  hostName: path: {
+    flake =
+      let
+        nixosConfiguration = inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit self; };
+          modules = [
+            path
+            { networking = { inherit hostName; }; }
+          ];
+        };
 
-            system = nixosConfiguration.config.nixpkgs.hostPlatform.system;
-          in
-          {
-            nixosConfigurations.${hostName} = nixosConfiguration;
-            checks.${system}."nixosConfigurations/${hostName}" =
-              nixosConfiguration.config.system.build.toplevel;
-          };
-      }
-    );
-}
+        system = nixosConfiguration.config.nixpkgs.hostPlatform.system;
+      in
+      {
+        nixosConfigurations.${hostName} = nixosConfiguration;
+        checks.${system}."nixosConfigurations/${hostName}" =
+          nixosConfiguration.config.system.build.toplevel;
+      };
+  }
+)
+|> lib.mkMerge
