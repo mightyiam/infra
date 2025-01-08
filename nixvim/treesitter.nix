7f8dc22189434e388ba9d4701ff91d2abd3d01ac
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-01-08 17:21:04 +0700

    treesitter incremental selection

--- nixvim/treesitter.nix
+++ nixvim/treesitter.nix
@@ -4,6 +4,14 @@
     settings = {
       highlight.enable = true;
       indent.enable = true;
+      incremental_selection = {
+        enable = true;
+        keymaps = {
+          init_selection = "<CR>";
+          node_incremental = "<CR>";
+          node_decremental = "<BS>";
+        };
+      };
     };
   };
 }
