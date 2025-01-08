60fb1d1e64e7a097608bad9df3e75949a9d9d8cb
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-01-08 20:17:28 +0700

    zsh module style

--- home-manager-modules/zsh/default.nix
+++ home-manager-modules/zsh/default.nix
@@ -1,35 +1,37 @@
 {
-  programs.zsh.enable = true;
-  programs.zsh.autosuggestion.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.syntaxHighlighting = {
+  programs.zsh = {
     enable = true;
-    highlighters = [
-      "main"
-      "brackets"
-      "pattern"
-      "regexp"
-      "cursor"
-      "line"
-    ];
-  };
-  programs.zsh.autocd = true;
-  programs.zsh.defaultKeymap = "viins";
-  programs.zsh.history.ignorePatterns = [ "rm *" ];
-  programs.zsh.initExtra = ''
-    bindkey '^[;' up-line-or-search
-    bindkey '^w' backward-kill-word
-    bindkey '^r' history-incremental-search-backward
-    bindkey '^[^M' autosuggest-execute
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting = {
+      enable = true;
+      highlighters = [
+        "main"
+        "brackets"
+        "pattern"
+        "regexp"
+        "cursor"
+        "line"
+      ];
+    };
+    autocd = true;
+    defaultKeymap = "viins";
+    history.ignorePatterns = [ "rm *" ];
+    initExtra = ''
+      bindkey '^[;' up-line-or-search
+      bindkey '^w' backward-kill-word
+      bindkey '^r' history-incremental-search-backward
+      bindkey '^[^M' autosuggest-execute
 
-    precmd() {
-      local cwd
-      cwd=''${PWD/#$HOME/\~}
-      print -Pn "\e]0;zsh ''${cwd}\a"
-    }
-  '';
-  programs.zsh.initExtraBeforeCompInit = ''
-    zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-  '';
-  programs.zsh.shellAliases.nix-shell = "nix-shell --run zsh";
+      precmd() {
+        local cwd
+        cwd=''${PWD/#$HOME/\~}
+        print -Pn "\e]0;zsh ''${cwd}\a"
+      }
+    '';
+    initExtraBeforeCompInit = ''
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+    '';
+    shellAliases.nix-shell = "nix-shell --run zsh";
+  };
 }
