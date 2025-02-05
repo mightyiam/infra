957acff6b9b8a51f6e7e5d4d0f9101d07e56db7c
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-02-05 13:52:17 +0700

    keybinding to launch qutebrowser window

--- home-manager-modules/qutebrowser.nix
+++ home-manager-modules/qutebrowser.nix
@@ -4,5 +4,8 @@
     programs.qutebrowser = {
       enable = true;
     };
+
+    wayland.windowManager.sway.config.keybindings."Mod4+q" =
+      "exec ${lib.getExe config.programs.qutebrowser.package}";
   };
 }
