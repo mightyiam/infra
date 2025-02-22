43a1d9773bb0d25f52852f653dc313e2a57685b2
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-02-22 21:37:16 +0700

    default browser qutebrowser

--- home-manager-modules/qutebrowser.nix
+++ home-manager-modules/qutebrowser.nix
@@ -14,5 +14,21 @@
 
     wayland.windowManager.sway.config.keybindings."Mod4+q" =
       "exec ${lib.getExe config.programs.qutebrowser.package}";
+
+    xdg.mimeApps.defaultApplications =
+      [
+        "x-scheme-handler/http"
+        "x-scheme-handler/https"
+        "x-scheme-handler/chrome"
+        "text/html"
+        "application/x-extension-htm"
+        "application/x-extension-html"
+        "application/x-extension-shtml"
+        "application/xhtml+xml"
+        "application/x-extension-xhtml"
+        "application/x-extension-xht"
+      ]
+      |> map (type: lib.nameValuePair type [ "org.qutebrowser.qutebrowser.desktop" ])
+      |> lib.listToAttrs;
   };
 }
--- home-manager-modules/xdg-mime.nix
+++ home-manager-modules/xdg-mime.nix
@@ -1,24 +1,6 @@
 { config, lib, ... }:
 let
-  web_browser = [ "firefox.desktop" ];
-  browser_types = [
-    "x-scheme-handler/http"
-    "x-scheme-handler/https"
-    "x-scheme-handler/chrome"
-    "text/html"
-    "application/x-extension-htm"
-    "application/x-extension-html"
-    "application/x-extension-shtml"
-    "application/xhtml+xml"
-    "application/x-extension-xhtml"
-    "application/x-extension-xht"
-  ];
   image = [ "imv.desktop" ];
-  type_to_browser_entry = type: {
-    name = type;
-    value = web_browser;
-  };
-  browser_entries = map type_to_browser_entry browser_types |> lib.listToAttrs;
 in
 lib.mkIf config.gui.enable {
   xdg = {
@@ -26,7 +8,7 @@ lib.mkIf config.gui.enable {
     mime.enable = true;
     mimeApps = {
       enable = true;
-      defaultApplications = browser_entries // {
+      defaultApplications = {
         "image/png" = image;
       };
     };
