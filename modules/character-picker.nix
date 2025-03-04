b0702c75aaf29414478bddbb9a96144975e59e79
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-03-04 21:13:05 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/character-picker.nix

---
+++ modules/character-picker.nix
@@ -0,0 +1,25 @@
+{ lib, ... }:
+{
+  flake.modules.homeManager.home =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      rofimoji = pkgs.rofimoji.override {
+        rofi = pkgs.rofi-wayland;
+      };
+    in
+    {
+      home.packages = lib.mkIf config.gui.enable [ rofimoji ];
+      wayland.windowManager.sway.config.keybindings =
+        let
+          mod = config.wayland.windowManager.sway.config.modifier;
+        in
+        {
+          "--no-repeat ${mod}+u" = "exec ${lib.getExe rofimoji}";
+          "--no-repeat ${mod}+Shift+u" = "exec ${lib.getExe rofimoji} --files all";
+        };
+    };
+}
