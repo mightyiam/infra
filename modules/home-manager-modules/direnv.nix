185494aefa9f0892e66a95f974e3b1d1208198db
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-03-04 21:19:35 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:modules/direnv.nix

--- modules/home-manager-modules/direnv.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
-  programs.git.ignores = [
-    ".envrc"
-    ".direnv"
-  ];
-  programs.direnv.config.global.warn_timeout = 0;
-}
