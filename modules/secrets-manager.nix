3ca972b0b03e7eb54180e3ae9d4947e00b621dda
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-03-10 07:16:27 +0700

    rofi-rbw default config

--- modules/secrets-manager.nix
+++ modules/secrets-manager.nix
@@ -17,23 +17,8 @@
         };
         home.packages = lib.mkIf config.gui.enable [ pkgs.rofi-rbw-wayland ];
         xdg.configFile."rofi-rbw.rc".text = lib.generators.toINIWithGlobalSection { } {
-          globalSection = {
-            keybindings = lib.concatStringsSep "," [
-              "Alt+1:type:username:tab:password"
-              "Alt+2:type:username"
-              "Alt+3:type:password"
-              "Alt+4:type:totp"
-              # "Alt+c:copy:password"
-              "Alt+u:copy:username"
-              # "Alt+t:copy:totp"
-              "Alt+m::menu"
-              "Alt+s:sync"
-            ];
-            menu-keybindings = lib.concatStringsSep "," [
-              "Alt+t:type"
-              # "Alt+c:copy"
-            ];
-          };
+          # https://github.com/fdw/rofi-rbw#configuration
+          globalSection = { };
         };
       };
       options.secretsMenu = lib.mkOption {
