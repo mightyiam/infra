ce73f3071b3c82a4dde152e99900e6f40039e753
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-03-10 07:39:31 +0700

    iamb notifications

--- modules/messaging-apps/iamb.nix
+++ modules/messaging-apps/iamb.nix
@@ -6,6 +6,7 @@
       home.packages = with pkgs; [ iamb ];
       xdg.configFile."iamb/config.toml".source = pkgs.writers.writeTOML "iamb-config.toml" {
         profiles.default.user_id = config.flake.meta.owner.matrix;
+        settings.notifications.enabled = true;
       };
     };
 }
