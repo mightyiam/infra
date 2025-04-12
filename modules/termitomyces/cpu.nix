e493f121eaa2c0cc06c0938759eb29c3cc1b7ef9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-04-12 13:08:13 +0700

    remove relic comment

--- modules/termitomyces/cpu.nix
+++ modules/termitomyces/cpu.nix
@@ -1,6 +1,5 @@
 {
   flake.modules.nixos."nixosConfigurations/termitomyces" = {
-    # https://github.com/e-tho/ucodenix/issues/32
     services.ucodenix = {
       enable = true;
       cpuModelId = "00A60F12";
