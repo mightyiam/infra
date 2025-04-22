d2efa390367eb3c2e20661973c0cc4803ea7d5dd
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-04-22 16:49:35 +0700

    rm explicit rustaceanvim check command clippy

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -4,10 +4,7 @@
       rustaceanvim = {
         enable = true;
         settings.server.default_settings.rust-analyzer = {
-          check = {
-            command = "clippy";
-            allTargets = true;
-          };
+          check.allTargets = true;
           inlayHints = {
             maxLength = 99;
             lifetimeElisionHints.enable = "always";
