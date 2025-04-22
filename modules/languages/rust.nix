60a91bc4bee16d06531c811cdac317975f59ebe6
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-04-22 15:32:54 +0700

    rustaceanvim.settings.server nesting correction

--- modules/languages/rust.nix
+++ modules/languages/rust.nix
@@ -3,16 +3,14 @@
     nixvim.astrea.plugins = {
       rustaceanvim = {
         enable = true;
-        settings = {
-          default_settings.rust-analyzer = {
-            check = {
-              command = "clippy";
-              allTargets = true;
-            };
-            inlayHints = {
-              maxLength = 99;
-              lifetimeElisionHints.enable = "always";
-            };
+        settings.server.default_settings.rust-analyzer = {
+          check = {
+            command = "clippy";
+            allTargets = true;
+          };
+          inlayHints = {
+            maxLength = 99;
+            lifetimeElisionHints.enable = "always";
           };
         };
       };
