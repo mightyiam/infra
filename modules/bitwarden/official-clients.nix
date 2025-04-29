faba63d647f01e972989ab3247432f08634243c1
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-04-29 22:12:04 +0700

    bitwarden official clients

---
+++ modules/bitwarden/official-clients.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.homeManager = {
+    base =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.bitwarden-cli ];
+      };
+
+    gui =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.bitwarden ];
+      };
+  };
+}
