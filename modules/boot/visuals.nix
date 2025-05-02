f97cf135c6b6999dc4b86e739636033f00d906ab
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-02 19:02:56 +0700

    rm redundant loglevel kernel param

--- modules/boot/visuals.nix
+++ modules/boot/visuals.nix
@@ -1,9 +1,6 @@
 {
   flake.modules.nixos.pc.boot = {
-    kernelParams = [
-      "loglevel=4"
-      "systemd.show_status=error"
-    ];
+    kernelParams = [ "systemd.show_status=error" ];
     plymouth.enable = true;
   };
 }
