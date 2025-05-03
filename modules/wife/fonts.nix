7c180f5b200720601736a89fecdb81a46bdabaa2
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-03 21:48:07 +0700

    expose fredoka-font package

--- modules/wife/fonts.nix
+++ modules/wife/fonts.nix
@@ -1,25 +1,12 @@
+{ withSystem, ... }:
 let
   polyModule =
     polyArgs@{ pkgs, ... }:
-    let
-      fontPath = "ofl/fredoka/Fredoka[wdth,wght].ttf";
-
-      google-fonts = pkgs.fetchgit {
-        url = "https://github.com/google/fonts.git";
-        rev = "54bbd6880add9f874368d5c266790d7af9c94b66";
-        sparseCheckout = [ fontPath ];
-        hash = "sha256-gEB7BoGGvNtAn/Pk1h4xUQId2DL4XAqK3FptzsrPbGg=";
-      };
-    in
     {
       stylix.fonts = {
         sansSerif = {
           name = "Fredoka";
-
-          package = pkgs.runCommand "fredoka-font" { } ''
-            mkdir -p $out/share/fonts/truetype
-            cp "${google-fonts}/${fontPath}" $out/share/fonts/truetype
-          '';
+          package = withSystem pkgs.system (psArgs: psArgs.config.packages.fredoka-font);
         };
 
         serif = polyArgs.config.stylix.fonts.sansSerif;
@@ -27,6 +14,25 @@ let
     };
 in
 {
+  perSystem =
+    { pkgs, ... }:
+    let
+      fontPath = "ofl/fredoka/Fredoka[wdth,wght].ttf";
+
+      google-fonts-sparse = pkgs.fetchgit {
+        url = "https://github.com/google/fonts.git";
+        rev = "54bbd6880add9f874368d5c266790d7af9c94b66";
+        sparseCheckout = [ fontPath ];
+        hash = "sha256-gEB7BoGGvNtAn/Pk1h4xUQId2DL4XAqK3FptzsrPbGg=";
+      };
+    in
+    {
+      packages.fredoka-font = pkgs.runCommand "fredoka-font" { } ''
+        mkdir -p $out/share/fonts/truetype
+        cp "${google-fonts-sparse}/${fontPath}" $out/share/fonts/truetype
+      '';
+    };
+
   flake.modules = {
     homeManager.wife = polyModule;
     nixos.wife = polyModule;
