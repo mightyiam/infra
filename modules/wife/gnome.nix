f125cab21506e8cf400638e345c0756e4a76b325
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-05 20:59:37 +0700

    wife enable home-manager gnome-shell

--- modules/wife/gnome.nix
+++ modules/wife/gnome.nix
@@ -1,26 +1,30 @@
 {
-  flake.modules.nixos.wife =
-    { pkgs, ... }:
-    {
-      services.xserver.desktopManager.gnome.enable = true;
+  flake.modules = {
+    nixos.wife =
+      { pkgs, ... }:
+      {
+        services.xserver.desktopManager.gnome.enable = true;
 
-      environment.gnome.excludePackages = with pkgs; [
-        epiphany
-        geary
-        gnome-calendar
-        gnome-connections
-        gnome-contacts
-        gnome-logs
-        gnome-maps
-        gnome-music
-        gnome-text-editor
-        gnome-tour
-        gnome-user-docs
-        gnome-weather
-        orca
-        simple-scan
-        snapshot
-        yelp
-      ];
-    };
+        environment.gnome.excludePackages = with pkgs; [
+          epiphany
+          geary
+          gnome-calendar
+          gnome-connections
+          gnome-contacts
+          gnome-logs
+          gnome-maps
+          gnome-music
+          gnome-text-editor
+          gnome-tour
+          gnome-user-docs
+          gnome-weather
+          orca
+          simple-scan
+          snapshot
+          yelp
+        ];
+      };
+
+    homeManager.wife.programs.gnome-shell.enable = true;
+  };
 }
