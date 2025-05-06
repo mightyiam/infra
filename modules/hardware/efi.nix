16beba9e5337d2123a24444030dcb74ecafccf9b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-06 16:57:59 +0700

    some efi cli tools

--- modules/hardware/efi.nix
+++ modules/hardware/efi.nix
@@ -1,6 +1,17 @@
 {
-  flake.modules.nixos.efi.boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub.efiSupport = true;
+  flake.modules = {
+    nixos.efi.boot.loader = {
+      efi.canTouchEfiVariables = true;
+      grub.efiSupport = true;
+    };
+
+    home-manager.base =
+      { pkgs, ... }:
+      {
+        home.packages = [
+          pkgs.efivar
+          pkgs.efibootmgr
+        ];
+      };
   };
 }
