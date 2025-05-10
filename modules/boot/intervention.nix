0c4ab0054a13aadc2cca05db896c7e106e604257
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-10 22:28:51 +0700

    dobby no skip grub menu

--- modules/boot/intervention.nix
+++ modules/boot/intervention.nix
@@ -1,8 +1,15 @@
+{ lib, ... }:
 {
-  flake.modules.nixos.pc.boot.loader = {
-    timeout = 2;
-    grub = {
-      extraConfig = ''
+  flake.modules.nixos.pc = nixosArgs: {
+    options.boot.loader.grub.skipMenuUnlessShift = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+
+    config.boot.loader = {
+      timeout = 2;
+
+      grub.extraConfig = lib.mkIf nixosArgs.config.boot.loader.grub.skipMenuUnlessShift ''
         if keystatus; then
           if keystatus --shift; then
             set timeout=-1
---
+++ modules/wife/dobby/boot-intervention.nix
@@ -0,0 +1,3 @@
+{
+  flake.modules.nixos."nixosConfigurations/dobby".boot.loader.grub.skipMenuUnlessShift = false;
+}
