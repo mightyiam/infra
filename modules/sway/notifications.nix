45075eac1887e386881a5dd6b76658e7f9e24248
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-13 21:05:32 +0700

    fix wrong mako settings

--- modules/sway/notifications.nix
+++ modules/sway/notifications.nix
@@ -9,8 +9,8 @@
         enable = true;
         settings = {
           anchor = "top-right";
-          defaultTimeout = 3000;
-          ignoreTimeout = true;
+          default-timeout = 3000;
+          ignore-timeout = 1;
         };
         criteria."mode=${mode}".invisible = 1;
       };
