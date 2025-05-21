1f1b0efd492930aefaf647979e361d289c3a1742
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-21 22:06:25 +0700

    style fix

--- modules/sway/sway.nix
+++ modules/sway/sway.nix
@@ -17,43 +17,41 @@
             (options.wayland.windowManager.sway.config.type.getSubOptions { }).keybindings.default;
         }
       ];
-      config = {
-        wayland.windowManager.sway = {
-          enable = true;
-          wrapperFeatures.gtk = true;
-          extraSessionCommands = ''
-            export MOZ_ENABLE_WAYLAND=1
-          '';
+      wayland.windowManager.sway = {
+        enable = true;
+        wrapperFeatures.gtk = true;
+        extraSessionCommands = ''
+          export MOZ_ENABLE_WAYLAND=1
+        '';
 
-          config = {
-            input = {
-              "type:keyboard" = {
-                xkb_layout = "us,il";
-                xkb_options = "grp:lalt_lshift_toggle";
-                repeat_delay = "200";
-                repeat_rate = "50";
-              };
-              "type:touchpad".tap = "enabled";
+        config = {
+          input = {
+            "type:keyboard" = {
+              xkb_layout = "us,il";
+              xkb_options = "grp:lalt_lshift_toggle";
+              repeat_delay = "200";
+              repeat_rate = "50";
             };
+            "type:touchpad".tap = "enabled";
+          };
 
-            workspaceLayout = "tabbed";
+          workspaceLayout = "tabbed";
 
-            modifier = "Mod4";
+          modifier = "Mod4";
 
-            keybindings = {
-              "${mod}+Shift+e" = null;
-              "--no-repeat ${mod}+Shift+e" =
-                "exec ${lib.getExe' config.wayland.windowManager.sway.package "swaymsg"} exit";
-              "${mod}+Shift+a" = "focus child";
-            };
+          keybindings = {
+            "${mod}+Shift+e" = null;
+            "--no-repeat ${mod}+Shift+e" =
+              "exec ${lib.getExe' config.wayland.windowManager.sway.package "swaymsg"} exit";
+            "${mod}+Shift+a" = "focus child";
           };
         };
-        xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
-
-        home.packages = with pkgs; [
-          wl-color-picker
-          xdg-utils
-        ];
       };
+      xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+
+      home.packages = with pkgs; [
+        wl-color-picker
+        xdg-utils
+      ];
     };
 }
