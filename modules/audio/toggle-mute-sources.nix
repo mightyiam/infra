e7027c71016ef63e4a4e9f3437a612b79c71b3c3
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-22 19:00:31 +0700

    rewrite toggle-mute-sources

--- modules/audio/toggle-mute-sources.nix
+++ modules/audio/toggle-mute-sources.nix
@@ -1,19 +1,32 @@
-{ lib, ... }:
+{ lib, withSystem, ... }:
 {
-  flake.modules.homeManager.gui =
-    { pkgs, config, ... }:
-    let
-      pactl = lib.getExe' pkgs.pulseaudio "pactl";
-      mod = config.wayland.windowManager.sway.config.modifier;
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.toggle-mute-sources = pkgs.writeShellApplication {
+        name = "toggle-mute-sources";
+        runtimeInputs = with pkgs; [
+          pulseaudio
+          gawk
+        ];
+        text = ''
+          for source in $(pactl list short sources | awk "{print \$2}");
+          do pactl set-source-mute "$source" toggle;
+          done
+        '';
+      };
+    };
 
-      toggleMuteSources = lib.concatStringsSep " " [
-        "exec ${lib.getExe pkgs.zsh} -c '"
-        "for source in $(${pactl} list short sources | ${lib.getExe' pkgs.gawk "awk"} \"{print \\$2}\");"
-        "do ${pactl} set-source-mute \"$source\" toggle;"
-        "done'"
-      ];
+  flake.modules.homeManager.gui =
+    { config, pkgs, ... }:
+    let
+      mod = config.wayland.windowManager.sway.config.modifier;
+      toggle-mute-sources = withSystem pkgs.system (psArgs: psArgs.config.packages.toggle-mute-sources);
     in
     {
-      wayland.windowManager.sway.config.keybindings."--no-repeat ${mod}+z" = toggleMuteSources;
+      wayland.windowManager.sway.config.keybindings."--no-repeat ${mod}+z" =
+        "exec ${lib.getExe toggle-mute-sources}";
+
+      home.packages = [ toggle-mute-sources ];
     };
 }
