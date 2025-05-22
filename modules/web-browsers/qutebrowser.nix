c4da27da07a00378b5240e0fcd79cdfef3368f89
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-22 20:05:49 +0700

    code style

--- modules/web-browsers/qutebrowser.nix
+++ modules/web-browsers/qutebrowser.nix
@@ -1,50 +1,48 @@
 { lib, ... }:
 {
-  flake.modules.homeManager.gui =
-    { config, ... }:
-    {
-      config = {
-        programs.qutebrowser = {
-          enable = true;
+  flake.modules.homeManager.gui = hmArgs: {
+    config = {
+      programs.qutebrowser = {
+        enable = true;
 
-          loadAutoconfig = true;
+        loadAutoconfig = true;
 
-          keyBindings.normal.e = "edit-url";
+        keyBindings.normal.e = "edit-url";
 
-          settings = {
-            editor.command = [
-              config.guiEditorCommand
-              "{file}"
-              "+{line}"
-            ];
-            tabs.tabs_are_windows = true;
+        settings = {
+          editor.command = [
+            hmArgs.config.guiEditorCommand
+            "{file}"
+            "+{line}"
+          ];
+          tabs.tabs_are_windows = true;
 
-            # https://github.com/qutebrowser/qutebrowser/issues/8535
-            qt.force_software_rendering = "chromium";
+          # https://github.com/qutebrowser/qutebrowser/issues/8535
+          qt.force_software_rendering = "chromium";
 
-            auto_save.session = true;
-          };
-
+          auto_save.session = true;
         };
 
-        wayland.windowManager.sway.config.keybindings."--no-repeat Mod4+q" =
-          "exec ${lib.getExe config.programs.qutebrowser.package}";
-
-        xdg.mimeApps.defaultApplications =
-          [
-            "x-scheme-handler/http"
-            "x-scheme-handler/https"
-            "x-scheme-handler/chrome"
-            "text/html"
-            "application/x-extension-htm"
-            "application/x-extension-html"
-            "application/x-extension-shtml"
-            "application/xhtml+xml"
-            "application/x-extension-xhtml"
-            "application/x-extension-xht"
-          ]
-          |> map (type: lib.nameValuePair type [ "org.qutebrowser.qutebrowser.desktop" ])
-          |> lib.listToAttrs;
       };
+
+      wayland.windowManager.sway.config.keybindings."--no-repeat Mod4+q" =
+        "exec ${lib.getExe hmArgs.config.programs.qutebrowser.package}";
+
+      xdg.mimeApps.defaultApplications =
+        [
+          "x-scheme-handler/http"
+          "x-scheme-handler/https"
+          "x-scheme-handler/chrome"
+          "text/html"
+          "application/x-extension-htm"
+          "application/x-extension-html"
+          "application/x-extension-shtml"
+          "application/xhtml+xml"
+          "application/x-extension-xhtml"
+          "application/x-extension-xht"
+        ]
+        |> map (type: lib.nameValuePair type [ "org.qutebrowser.qutebrowser.desktop" ])
+        |> lib.listToAttrs;
     };
+  };
 }
