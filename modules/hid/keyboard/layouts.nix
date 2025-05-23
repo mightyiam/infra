bbb0b4215c66659869812166945cf156e6ee8fb9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-23 21:15:09 +0700

    sway keyboard_layout bar block only in sway

--- modules/hid/keyboard/layouts.nix
+++ modules/hid/keyboard/layouts.nix
@@ -1,23 +1,28 @@
 { lib, ... }:
 {
-  flake.modules.homeManager.gui = {
-    wayland.windowManager.sway.config.input."type:keyboard" = {
-      xkb_layout = lib.concatStringsSep "," [
-        "us"
-        "il"
-      ];
-      xkb_options = "grp:lalt_lshift_toggle";
-    };
+  flake.modules.homeManager.gui =
+    { pkgs, ... }:
+    {
+      wayland.windowManager.sway.config.input."type:keyboard" = {
+        xkb_layout = lib.concatStringsSep "," [
+          "us"
+          "il"
+        ];
+        xkb_options = "grp:lalt_lshift_toggle";
+      };
 
-    programs.i3status-rust.bars.bottom.blocks = lib.mkOrder 1200 [
-      {
-        block = "keyboard_layout";
-        driver = "sway";
-        mappings = {
-          "English (US)" = "EN";
-          "Hebrew (N/A)" = "HE";
-        };
-      }
-    ];
-  };
+      programs.i3status-rust.bars.bottom.blocks = lib.mkOrder 1200 [
+        {
+          block = "keyboard_layout";
+          if_command = pkgs.writeShellScript "in-sway" ''
+            [ "$XDG_CURRENT_DESKTOP" = "sway" ]
+          '';
+          driver = "sway";
+          mappings = {
+            "English (US)" = "EN";
+            "Hebrew (N/A)" = "HE";
+          };
+        }
+      ];
+    };
 }
