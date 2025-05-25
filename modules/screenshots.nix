7882dfc7f46e2ac04b75eb13c59f83781d680c76
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-25 16:56:28 +0700

    home.packages shotman

--- modules/screenshots.nix
+++ modules/screenshots.nix
@@ -6,6 +6,8 @@
       shotman = "${lib.getExe' pkgs.shotman "shotman"} --capture";
     in
     {
+      home.packages = [ pkgs.shotman ];
+
       wayland.windowManager.sway.config.keybindings =
         let
           mod = hmArgs.config.wayland.windowManager.sway.config.modifier;
