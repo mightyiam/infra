e8d6d6b4dda18ad786b9beabca54ddb559a231dc
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-26 16:53:19 +0700

    tiny refactor

--- modules/screenshots.nix
+++ modules/screenshots.nix
@@ -2,9 +2,6 @@
 {
   flake.modules.homeManager.gui =
     hmArgs@{ pkgs, ... }:
-    let
-      shotman = "${lib.getExe' pkgs.shotman "shotman"} --capture";
-    in
     {
       home.packages = [ pkgs.shotman ];
 
@@ -13,9 +10,9 @@
           mod = hmArgs.config.wayland.windowManager.sway.config.modifier;
         in
         {
-          "${mod}+Shift+w" = "exec ${shotman} window";
-          "${mod}+Shift+o" = "exec ${shotman} output";
-          "${mod}+Shift+r" = "exec ${shotman} region";
+          "${mod}+Shift+w" = "exec ${lib.getExe' pkgs.shotman "shotman"} --capture window";
+          "${mod}+Shift+o" = "exec ${lib.getExe' pkgs.shotman "shotman"} --capture output";
+          "${mod}+Shift+r" = "exec ${lib.getExe' pkgs.shotman "shotman"} --capture region";
         };
     };
 }
