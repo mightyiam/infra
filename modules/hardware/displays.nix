3a9c1dc76086a77df49df38c44d65f7c0b884bb3
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-27 22:52:16 +0700

    update home-manager fork

--- modules/hardware/displays.nix
+++ modules/hardware/displays.nix
@@ -1,20 +1,8 @@
-{ lib, ... }:
 {
-  flake.modules.homeManager.gui =
-    { pkgs, ... }:
-    let
-      command = ''${lib.getExe' pkgs.systemd "systemctl"} --user start "way-displays@$XDG_VTNR".service'';
-    in
-    {
-      services.way-displays = {
-        enable = true;
-        statefulConfiguration = true;
-        settings = null;
-      };
-
-      wayland.windowManager = {
-        sway.config.startup = [ { inherit command; } ];
-        hyprland.settings.exec-once = [ command ];
-      };
+  flake.modules.homeManager.gui = {
+    services.way-displays = {
+      enable = true;
+      settings = null;
     };
+  };
 }
