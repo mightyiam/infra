cece66611b6f06f0877bf53217b39a5db467628d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-30 18:59:27 +0700

    rewrite git worktree tools

--- modules/git/worktrees.nix
+++ modules/git/worktrees.nix
@@ -1,22 +1,38 @@
-{ lib, ... }:
+{ lib, withSystem, ... }:
 {
-  flake.modules.homeManager.base.programs.zsh.initContent = lib.mkMerge [
-    # cd from worktree into clone
-    ''
-      git-cd () {
-        git_dir="$(git rev-parse --git-dir)"
-        cd "''${git_dir%/*/*}"
-      }
-    ''
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages = {
+        git-dir = pkgs.writeShellApplication {
+          name = "git-dir";
+          runtimeInputs = [ pkgs.git ];
+          text = ''
+            git_dir=$(git rev-parse --git-dir)
+            echo "''${git_dir%/*/*}"
+          '';
+        };
+        git-worktree-add = pkgs.writeShellApplication {
+          name = "git-worktree-add";
+          runtimeInputs = [ pkgs.git ];
+          text = ''
+            cd "$1"
+            worktree_path="$HOME/worktrees/$2"
+            git worktree add --detach "$worktree_path"
+            echo "$worktree_path"
+          '';
+        };
+      };
+    };
 
-    # create a new worktree by name and cd into it
-    ''
-      worktree-add () {
-        cd "$1" && \
-        worktree_path="$HOME/worktrees/$2" && \
-        git --bare worktree add --detach "$worktree_path" && \
-        cd "$worktree_path"
-      }
-    ''
-  ];
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      home.packages = withSystem pkgs.system (
+        psArgs: with psArgs.config.packages; [
+          git-dir
+          git-worktree-add
+        ]
+      );
+    };
 }
