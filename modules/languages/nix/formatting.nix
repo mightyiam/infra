6db92a678400db47b50223baa4c2b63205cba9dd
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-05-30 12:25:50 +0700

    nix nixvim formatter nixfmt-rfc-style

--- modules/languages/nix/formatting.nix
+++ modules/languages/nix/formatting.nix
@@ -1,38 +1,41 @@
 { inputs, ... }:
 {
-  flake.modules.nixvim.astrea = {
-    extraConfigLua = ''
-      vim.g.nixfmt_enabled = false
-    '';
+  flake.modules.nixvim.astrea =
+    { pkgs, ... }:
+    {
+      extraConfigLua = ''
+        vim.g.nixfmt_enabled = false
+      '';
 
-    keymaps = [
-      {
-        key = "<leader>n";
-        options.desc = "Toggle nixfmt";
-        action = inputs.nixvim.lib.nixvim.mkRaw ''
-          function()
-            vim.g.nixfmt_enabled = not vim.g.nixfmt_enabled
+      keymaps = [
+        {
+          key = "<leader>n";
+          options.desc = "Toggle nixfmt";
+          action = inputs.nixvim.lib.nixvim.mkRaw ''
+            function()
+              vim.g.nixfmt_enabled = not vim.g.nixfmt_enabled
 
-            local message
-            if vim.g.nixfmt_enabled then
-              message = "Nixfmt is on"
-            else
-              message = "Nixfmt is off"
+              local message
+              if vim.g.nixfmt_enabled then
+                message = "Nixfmt is on"
+              else
+                message = "Nixfmt is off"
+              end
+              vim.notify(message, vim.log.levels.INFO)
             end
-            vim.notify(message, vim.log.levels.INFO)
-          end
-        '';
-      }
-    ];
+          '';
+        }
+      ];
 
-    plugins.none-ls = {
-      enable = true;
-      sources.formatting.nixfmt = {
+      plugins.none-ls = {
         enable = true;
-        settings.runtime_condition = inputs.nixvim.lib.nixvim.mkRaw ''
-          function() return vim.g.nixfmt_enabled end
-        '';
+        sources.formatting.nixfmt = {
+          enable = true;
+          package = pkgs.nixfmt-rfc-style;
+          settings.runtime_condition = inputs.nixvim.lib.nixvim.mkRaw ''
+            function() return vim.g.nixfmt_enabled end
+          '';
+        };
       };
     };
-  };
 }
