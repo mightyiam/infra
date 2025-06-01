df3fc266d37ee0351423c7b79ddcd407a3c12ccd
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-01 14:03:59 +0700

    Revert "shorthand path input syntax"
    This reverts commit 6cc4d5943b871cbc5a91e66ea3144d4cf090efe0.

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     };
 
     home-manager = {
-      url = "./forks/home-manager";
+      url = "path:./forks/home-manager";
       flake = true;
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -88,7 +88,7 @@
     };
 
     stylix = {
-      url = "./forks/stylix";
+      url = "path:./forks/stylix";
       flake = true;
       inputs = {
         flake-compat.follows = "dedupe_flake-compat";
