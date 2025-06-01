fab6e65427b60cd32d64dcebe2a1e0c006fded7c
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-01 14:37:23 +0700

    input-forks-rebase package

--- modules/meta/input-forks.nix
+++ modules/meta/input-forks.nix
@@ -1,3 +1,15 @@
 {
-  perSystem.treefmt.settings.global.excludes = [ "forks/*" ];
+  perSystem =
+    { pkgs, ... }:
+    {
+      treefmt.settings.global.excludes = [ "forks/*" ];
+
+      packages.input-forks-rebase = pkgs.writeShellApplication {
+        name = "input-forks-rebase";
+        runtimeInputs = [ pkgs.git ];
+        text = ''
+          git submodule foreach "git switch \$name && git fetch upstream && git rebase upstream/HEAD && git push -f"
+        '';
+      };
+    };
 }
