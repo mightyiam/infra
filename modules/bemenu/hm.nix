82323751bcd45579c8d3a5dd05531c3c2a78e347
awwpotato <awwpotato@voidq.com>
2025-06-17 17:13:57 -0700

    bemenu: add defaultText to fontSize (#1513)
    Link: https://github.com/nix-community/stylix/pull/1513
    
    Reviewed-by: Matt Sturgeon <matt@sturgeon.me.uk>
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/bemenu/hm.nix
+++ modules/bemenu/hm.nix
@@ -15,6 +15,7 @@ mkTarget {
       '';
       type = with lib.types; nullOr int;
       default = config.stylix.fonts.sizes.popups;
+      defaultText = lib.literalExpression "config.stylix.fonts.sizes.popups";
     }; # optional argument
 
     alternate = lib.mkOption {
