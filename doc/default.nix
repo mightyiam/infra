danth/stylix

doc: use minimal module evals
We don't actually need fully blown NixOS or home-manager configurations
just to read the declared `options`.

Instead, we can directly call `lib.evalModules` to build a minimal
configuration containing only stylix modules.

docs/default.nix
@@ -6,40 +6,31 @@
 }@args:
 
 let
-  nixosConfiguration = lib.nixosSystem {
-    inherit (pkgs) system;
-    modules = [
-      inputs.home-manager.nixosModules.home-manager
-      inputs.self.nixosModules.stylix
-      ./settings.nix
-    ];
-  };
+  evalDocs =
+    module:
+    lib.evalModules {
+      modules = lib.toList module ++ [
+        ./eval_compat.nix
+        # TODO: enforce pkgs is not used in option docs
+        # E.g. throw when a package is evaluated
+        { _module.args = { inherit pkgs; }; }
+      ];
 
-  homeManagerConfiguration = inputs.home-manager.lib.homeManagerConfiguration {
-    inherit pkgs;
-    modules = [
-      inputs.self.homeManagerModules.stylix
-      ./settings.nix
-      {
-        home = {
-          homeDirectory = "/home/book";
-          stateVersion = "22.11";
-          username = "book";
-        };
-      }
-    ];
-  };
+    };
 
   # TODO: Include Nix Darwin options
 
   platforms = {
     home_manager = {
       name = "Home Manager";
-      configuration = homeManagerConfiguration;
+      configuration = evalDocs [
+        inputs.self.homeManagerModules.stylix
+        ./hm_compat.nix
+      ];
     };
     nixos = {
       name = "NixOS";
-      configuration = nixosConfiguration;
+      configuration = evalDocs inputs.self.nixosModules.stylix;
     };
   };
 

mightyiam/infra

doc/default.nix
@@ -2,9 +2,6 @@
   lib,
   pkgs,
   inputs,
-  nixosSystem,
-  homeManagerConfiguration,
-  system,
   callPackage,
   writeText,
   stdenvNoCC,
@@ -17,38 +14,26 @@ let
   # Prefix to remove from option declaration file paths.
   rootPrefix = toString ../. + "/";
 
-  nixosConfiguration = nixosSystem {
-    inherit system;
-    modules = [
-      inputs.home-manager.nixosModules.home-manager
-      inputs.self.nixosModules.stylix
-    ];
-  };
-
-  homeConfiguration = homeManagerConfiguration {
-    inherit pkgs;
-    modules = [
-      inputs.self.homeModules.stylix
-      {
-        home = {
-          homeDirectory = "/home/book";
-          stateVersion = "22.11";
-          username = "book";
-        };
-      }
-    ];
-  };
+  evalDocs =
+    module:
+    lib.evalModules {
+      modules = [ ./eval_compat.nix ] ++ lib.toList module;
+      specialArgs = { inherit pkgs; };
+    };
 
   # TODO: Include Nix Darwin options
 
   platforms = {
     home_manager = {
       name = "Home Manager";
-      configuration = homeConfiguration;
+      configuration = evalDocs [
+        inputs.self.homeModules.stylix
+        ./hm_compat.nix
+      ];
     };
     nixos = {
       name = "NixOS";
-      configuration = nixosConfiguration;
+      configuration = evalDocs inputs.self.nixosModules.stylix;
     };
   };
 