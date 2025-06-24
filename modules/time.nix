6653ef07382baa41fb3a6df8c12481f725db5131
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-24 22:46:07 +0700

    workaround geoclue being disabled by DE

--- modules/time.nix
+++ modules/time.nix
@@ -1,9 +1,13 @@
+{ lib, ... }:
 {
   flake.modules = {
     nixos.pc = {
       services = {
         ntpd-rs.enable = true;
         automatic-timezoned.enable = true;
+
+        # https://github.com/NixOS/nixpkgs/issues/68489#issuecomment-1484030107
+        geoclue2.enableDemoAgent = lib.mkForce true;
       };
     };
     nixOnDroid.base = {
