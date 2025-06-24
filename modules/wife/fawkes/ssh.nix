e5285d66357b1938885801e972c0baa251741157
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-24 08:02:49 +0700

    correct fawkes publicKey

--- modules/wife/fawkes/ssh.nix
+++ modules/wife/fawkes/ssh.nix
@@ -1,4 +1,4 @@
 {
   flake.modules.nixos."nixosConfigurations/fawkes".services.openssh.publicKey =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINH9fDYNQ6RTHc2eGvUOjp6HZm5vXr4ofTr+b9vRpc+T";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN9nypXKeLfgwbnAlQHvuvudkI1jrpcuggYIsEwunuSC";
 }
