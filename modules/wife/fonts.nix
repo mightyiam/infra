9f39d2bf58d26b804420a82e16c5efad286ca3d1
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-26 16:53:38 +0700

    wife monospace and emoji fonts

--- modules/wife/fonts.nix
+++ modules/wife/fonts.nix
@@ -1,14 +1,25 @@
+{ lib, ... }:
 let
   polyModule =
     polyArgs@{ pkgs, ... }:
     {
-      stylix.fonts = {
+      stylix.fonts = lib.mkForce {
         sansSerif = {
           name = "Fredoka";
           package = pkgs.google-fonts;
         };
 
         serif = polyArgs.config.stylix.fonts.sansSerif;
+
+        monospace = {
+          name = "Noto Sans Mono";
+          package = pkgs.google-fonts;
+        };
+
+        emoji = {
+          name = "Noto Color Emoji";
+          package = pkgs.google-fonts;
+        };
       };
     };
 in
