0f93e58628596297711954ba5ba6d3a3ef9cf3dd
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-06-26 03:49:46 +0200

    flake: infer default.nix import path (#1544)
    Fixes: c765b15fc394 ("stylix: move testbed to a dedicated directory")
    
    Reviewed-by: awwpotato <awwpotato@voidq.com>

--- flake/packages.nix
+++ flake/packages.nix
@@ -15,7 +15,7 @@
         # Testbeds are virtual machines based on NixOS, therefore they are
         # only available for Linux systems.
         (lib.mkIf pkgs.stdenv.hostPlatform.isLinux (
-          import ../stylix/testbed/default.nix {
+          import ../stylix/testbed {
             inherit pkgs inputs lib;
           }
         ))
