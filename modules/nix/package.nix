f329d762ab63e0122dfb1bd49bc702379e97a4ec
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-06-26 17:30:48 +0700

    rm nix bareRepository workaround

--- modules/nix/package.nix
+++ modules/nix/package.nix
@@ -1,47 +1,25 @@
 { lib, ... }:
 let
-  nixVersion = "nix_2_29";
-
-  # https://github.com/NixOS/nix/issues/11358
-  mkWorkaround =
-    pkgs: original:
-    pkgs.stdenv.mkDerivation {
-      name = "nix-with-workaround";
-      version = original.version;
-      runtimeInputs = [ original ];
-      unpackPhase = "true";
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      buildPhase = ''
-        cp -r ${original} $out
-        chmod -R +w $out
-        wrapProgram $out/bin/nix \
-          --set GIT_CONFIG_COUNT 1 \
-          --set GIT_CONFIG_KEY_0 safe.bareRepository \
-          --set GIT_CONFIG_VALUE_0 all
-      '';
-      meta.mainProgram = "nix";
-    };
-
-  mkNix = pkgs: pkgs.nixVersions.${nixVersion} |> mkWorkaround pkgs;
+  getNix = pkgs: pkgs.nixVersions.nix_2_29;
 in
 {
   flake.modules = {
     nixos.pc =
       { pkgs, ... }:
       {
-        nix.package = mkNix pkgs;
+        nix.package = getNix pkgs;
       };
 
     homeManager.base =
       { pkgs, ... }:
       {
-        nix.package = pkgs |> mkNix |> lib.mkDefault;
+        nix.package = pkgs |> getNix |> lib.mkDefault;
       };
 
     nixOnDroid.base =
       { pkgs, ... }:
       {
-        nix.package = mkNix pkgs;
+        nix.package = getNix pkgs;
       };
   };
 }
