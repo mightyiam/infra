52401c32651ff1cd94c33fc8aba5f50253a64bc5
Flameopathic <64027365+Flameopathic@users.noreply.github.com>
2025-07-03 14:22:21 -0400

    ncspot: add testbed (#1573)
    Reviewed-by: awwpotato <awwpotato@voidq.com>

---
+++ modules/ncspot/testbeds/ncspot.nix
@@ -0,0 +1,10 @@
+{ pkgs, lib, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = lib.getExe pkgs.ncspot;
+    useTerminal = true;
+  };
+  home-manager.sharedModules = lib.singleton {
+    programs.ncspot.enable = true;
+  };
+}
