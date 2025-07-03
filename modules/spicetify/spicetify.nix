ad13dd4eb2747b0ba10bc0f1f23bbe2f52f9c60e
awwpotato <awwpotato@voidq.com>
2025-07-03 11:36:34 -0700

    spicetify: use mkTarget

--- modules/spicetify/spicetify.nix
+++ modules/spicetify/spicetify.nix
@@ -1,56 +1,50 @@
 {
-  config,
-  options,
+  mkTarget,
   lib,
+  options,
   pkgs,
   ...
 }:
+mkTarget {
+  name = "spicetify";
+  humanName = "Spicetify";
 
-{
-  options.stylix.targets.spicetify.enable =
-    config.lib.stylix.mkEnableTarget "Spicetify" true;
-
-  config =
-    lib.mkIf
-      (
-        config.stylix.enable
-        && config.stylix.targets.spicetify.enable
-        && options.programs ? spicetify
-      )
-      {
-        programs.spicetify = {
-          theme = {
-            name = "stylix";
-            src = pkgs.writeTextFile {
-              name = "color.ini";
-              destination = "/color.ini";
-              text = with config.lib.stylix.colors; ''
-                [base]
-                text               = ${base05}
-                subtext            = ${base05}
-                main               = ${base00}
-                main-elevated      = ${base02}
-                highlight          = ${base02}
-                highlight-elevated = ${base03}
-                sidebar            = ${base01}
-                player             = ${base05}
-                card               = ${base04}
-                shadow             = ${base00}
-                selected-row       = ${base05}
-                button             = ${base05}
-                button-active      = ${base05}
-                button-disabled    = ${base04}
-                tab-active         = ${base02}
-                notification       = ${base02}
-                notification-error = ${base08}
-                equalizer          = ${base0B}
-                misc               = ${base02}
-              '';
-            };
-            # Sidebar configuration is incompatible with the default navigation bar
-            sidebarConfig = false;
+  configElements =
+    { colors }:
+    lib.optionalAttrs (options.programs ? spicetify) {
+      programs.spicetify = {
+        theme = {
+          name = "stylix";
+          src = pkgs.writeTextFile {
+            name = "color.ini";
+            destination = "/color.ini";
+            text = with colors; ''
+              [base]
+              text               = ${base05}
+              subtext            = ${base05}
+              main               = ${base00}
+              main-elevated      = ${base02}
+              highlight          = ${base02}
+              highlight-elevated = ${base03}
+              sidebar            = ${base01}
+              player             = ${base05}
+              card               = ${base04}
+              shadow             = ${base00}
+              selected-row       = ${base05}
+              button             = ${base05}
+              button-active      = ${base05}
+              button-disabled    = ${base04}
+              tab-active         = ${base02}
+              notification       = ${base02}
+              notification-error = ${base08}
+              equalizer          = ${base0B}
+              misc               = ${base02}
+            '';
           };
-          colorScheme = "base";
+          # Sidebar configuration is incompatible with the default navigation bar
+          sidebarConfig = false;
         };
+        colorScheme = "base";
       };
+    };
 }
