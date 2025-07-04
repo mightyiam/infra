e97d9f5eb51bf226d3b6bc38863f8eaf56c7e3b9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-04 23:09:57 +0700

    hyprland stateful monitors config file

--- modules/hardware/displays.nix
+++ modules/hardware/displays.nix
@@ -1,8 +1,21 @@
 {
-  flake.modules.homeManager.gui = {
-    services.way-displays = {
-      enable = true;
-      settings = null;
+  flake.modules.homeManager.gui =
+    hmArgs:
+    let
+      # https://wiki.hypr.land/Configuring/Monitors, example:
+      #
+      # ```
+      # monitor = desc:Microstep Optix G24C 0000000000001, 1920x1080@144, auto-center, 1
+      # monitor = desc:AOC 24V2W1G5 0x000006C2           , 1920x1080@75 , auto-left  , 1
+      # ```
+      hyprlandConfPath = "${hmArgs.config.xdg.configHome}/hypr/monitors.conf";
+    in
+    {
+      wayland.windowManager.hyprland.extraConfig = ''
+        source = ${hyprlandConfPath}
+      '';
+      home.activation.hyprlandStatefulMonitorsFile = hmArgs.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        run touch ${hyprlandConfPath}
+      '';
     };
-  };
 }
