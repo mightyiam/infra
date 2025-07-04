d0ef1caf4e1e3b832bb04342b9e4bbeb504154a3
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-04 23:19:53 +0700

    termitomyces static display config

---
+++ modules/termitomyces/displays.nix
@@ -0,0 +1,8 @@
+{
+  flake.modules.homeManager.gui = {
+    wayland.windowManager.hyprland.settings.monitor = [
+      "desc:Microstep Optix G24C 0000000000001, 1920x1080@144, auto-center, 1"
+      "desc:AOC 24V2W1G5 0x000006C2           , 1920x1080@75 , auto-left  , 1"
+    ];
+  };
+}
