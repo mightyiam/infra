50ed5ddd1072a6b10e6368cc338d759ffa02df9b
Flameopathic <64027365+Flameopathic@users.noreply.github.com>
2025-07-05 22:32:43 -0400

    {neovim, vim}: add testbeds (#1571)
    Link: https://github.com/nix-community/stylix/pull/1571
    
    Reviewed-by: "Shahar \"Dawn\" Or" <mightyiampresence@gmail.com>
    Reviewed-by: awwpotato <awwpotato@voidq.com>

---
+++ modules/neovim/testbeds/vim.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = "${lib.getExe pkgs.vim} example.md";
+    useTerminal = true;
+  };
+
+  home-manager.sharedModules = lib.singleton {
+    programs.vim.enable = true;
+    home.file."example.md" = {
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/sharkdp/bat/e2aa4bc33cca785cab8bdadffc58a4a30b245854/tests/syntax-tests/source/Markdown/example.md";
+        hash = "sha256-VYYwgRFY1c2DPY7yGM8oF3zG4rtEpBWyqfPwmGZIkcA=";
+      };
+    };
+  };
+}
