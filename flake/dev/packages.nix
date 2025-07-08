1baa44cf8c3a4699d0beda91f39ba7942b46269d
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-07-08 22:56:08 +0200

    flake: infer default.nix import path (#1636)
    Re-apply commit 0f93e5862859 ("flake: infer default.nix import path
    (#1544)") that was accidentally reverted in commit a5c1532ab8bf ("flake:
    partition dev inputs (#1289)").
    
    Link: https://github.com/nix-community/stylix/pull/1636
    
    Reviewed-by: awwpotato <awwpotato@voidq.com>

--- flake/dev/packages.nix
+++ flake/dev/packages.nix
@@ -10,7 +10,7 @@
       # Testbeds are virtual machines based on NixOS, therefore they are
       # only available for Linux systems.
       packages = lib.mkIf pkgs.stdenv.hostPlatform.isLinux (
-        import ../../stylix/testbed/default.nix {
+        import ../../stylix/testbed {
           inherit pkgs inputs lib;
         }
       );
