2947a83755d206553fb88116058f5322f3bc2d0e
Anton Mosich <anton@mosich.at>
2025-07-13 16:05:22 +0000

    gtksourceview: don't overlay all of gnome2 (#1680)
    Link: https://github.com/nix-community/stylix/pull/1680
    
    Fixes: 05752c77ac ("gtksourceview: init module (#958)")
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    Reviewed-by: awwpotato <awwpotato@voidq.com>

--- modules/gtksourceview/overlay.nix
+++ modules/gtksourceview/overlay.nix
@@ -27,9 +27,9 @@ in
         && config.stylix.targets.gtksourceview.enable
       )
       {
-        gnome2.gtksourceview = prev.gnome2.gtksourceview.overrideAttrs (
-          attrsOverride "2.0"
-        );
+        gnome2 = prev.gnome2 // {
+          gtksourceview = prev.gnome2.gtksourceview.overrideAttrs (attrsOverride "2.0");
+        };
         gtksourceview = prev.gtksourceview.overrideAttrs (attrsOverride "3.0");
         gtksourceview4 = prev.gtksourceview4.overrideAttrs (attrsOverride "4");
         gtksourceview5 = prev.gtksourceview5.overrideAttrs (attrsOverride "5");
