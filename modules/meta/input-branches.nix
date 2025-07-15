2fe8e2235339877f037bf025157065bcdf87c0b1
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-15 14:51:16 +0700

    nixpkgs input branch shallow

--- modules/meta/input-branches.nix
+++ modules/meta/input-branches.nix
@@ -25,9 +25,12 @@
   imports = [ inputs.input-branches.flakeModules.default ];
 
   input-branches.inputs = {
-    nixpkgs.upstream = {
-      url = "https://github.com/NixOS/nixpkgs.git";
-      ref = "nixpkgs-unstable";
+    nixpkgs = {
+      upstream = {
+        url = "https://github.com/NixOS/nixpkgs.git";
+        ref = "nixpkgs-unstable";
+      };
+      shallow = true;
     };
     home-manager.upstream = {
       url = "https://github.com/nix-community/home-manager.git";
