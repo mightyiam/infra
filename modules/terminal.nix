db55decb8db9aa38efaa96b65c648cf4764bbd59
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-16 21:39:05 +0700

    extract modules/terminal/alacritty.nix

--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,4 +1,3 @@
-{ lib, ... }:
 {
   flake.modules.homeManager = {
     base =
@@ -6,28 +5,5 @@
       {
         home.packages = with pkgs; [ ansifilter ];
       };
-    gui = hmArgs: {
-      terminal = {
-        path = lib.getExe hmArgs.config.programs.alacritty.package;
-      };
-
-      programs.alacritty = {
-        enable = true;
-        settings = {
-          general.live_config_reload = true;
-          window = {
-            decorations = "none";
-            dynamic_title = true;
-            title = "Terminal";
-          };
-          bell = {
-            # https://github.com/danth/stylix/discussions/1207
-            # ideally this would be some stylix color theme color
-            color = "#000000";
-            duration = 200;
-          };
-        };
-      };
-    };
   };
 }
