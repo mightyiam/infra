mightyiam/infra

commit 8c854fe383fda20e8befefc31ecf248988a40bcc

stylix: allow choosing testbed desktop (#1222)
Allow choosing the testbed desktop, ideally as a temporary solution
until migrating to the cage environment.

Link: https://github.com/nix-community/stylix/pull/1222

Reviewed-by: Flameopathic <64027365+Flameopathic@users.noreply.github.com>
Reviewed-by: awwpotato <awwpotato@voidq.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/testbed/graphical-environments/gnome.nix
@@ -1,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config =
+    lib.mkIf (config.stylix.testbed.ui.graphicalEnvironment or null == "gnome")
+      {
+        services.xserver = {
+          enable = true;
+          displayManager.gdm.enable = true;
+          desktopManager.gnome.enable = true;
+        };
+
+        # Disable the GNOME tutorial which pops up on first login.
+        environment.gnome.excludePackages = [ pkgs.gnome-tour ];
+      };
+}

modules/gnome/testbeds/gnome.nix
@@ -1,6 +1,3 @@
 {
-  services = {
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
-  };
+  stylix.testbed.ui.graphicalEnvironment = "gnome";
 }

modules/bspwm/testbeds/bspwm.nix
@@ -1,17 +1,13 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  services.xserver = {
-    enable = true;
-    windowManager.bspwm.enable = true;
-  };
+  stylix.testbed.ui = {
+    graphicalEnvironment = "bspwm";
 
-  home-manager.sharedModules = lib.singleton {
-    xsession.windowManager.bspwm = {
-      enable = true;
-
-      # We need something to open a window so that we can check the window borders
-      startupPrograms = [ "${lib.getExe pkgs.kitty}" ];
+    # We need something to open a window so that we can check the window borders
+    application = {
+      name = "kitty";
+      package = pkgs.kitty;
     };
   };
 }

modules/hyprland/testbeds/hyprland.nix
@@ -1,15 +1,13 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  environment.loginShellInit = lib.getExe pkgs.hyprland;
-  programs.hyprland.enable = true;
+  stylix.testbed.ui = {
+    graphicalEnvironment = "hyprland";
 
-  home-manager.sharedModules = lib.singleton {
-    wayland.windowManager.hyprland = {
-      enable = true;
-
-      # We need something to open a window so that we can check the window borders
-      settings.bind = [ "ALT, RETURN, exec, ${lib.getExe pkgs.foot}" ];
+    # We need something to open a window so that we can check the window borders
+    application = {
+      name = "kitty";
+      package = pkgs.kitty;
     };
   };
 }

modules/kde/testbeds/kde.nix
@@ -1,7 +1,10 @@
 {
-  services = {
-    displayManager.sddm.enable = true;
-
-    desktopManager.plasma6.enable = true;
+  lib,
+  ...
+}:
+{
+  config = {
+    stylix.testbed.ui.graphicalEnvironment = "kde";
+    services.displayManager.autoLogin.enable = lib.mkForce false;
   };
 }

modules/mako/testbeds/mako.nix
@@ -1,25 +1,23 @@
 { lib, pkgs, ... }:
 
 {
-  # We use Hyprland because Gnome has its own notification daemon
-  environment.loginShellInit = lib.getExe pkgs.hyprland;
-  programs.hyprland.enable = true;
+  stylix.testbed.ui = {
+    # We use Hyprland because Gnome has its own notification daemon
+    graphicalEnvironment = "hyprland";
+    command.text =
+      # Run as a single command to ensure the same order between executions
+      lib.concatMapStringsSep " && "
+        (
+          urgency: "${lib.getExe pkgs.libnotify} --urgency ${urgency} ${urgency} urgency"
+        )
+        [
+          "low"
+          "normal"
+          "critical"
+        ];
+  };
 
   home-manager.sharedModules = lib.singleton {
     services.mako.enable = true;
-    wayland.windowManager.hyprland = {
-      enable = true;
-      settings.exec-once =
-        # Run as a single command to ensure the same order between executions
-        lib.concatMapStringsSep " && "
-          (
-            urgency: "${lib.getExe pkgs.libnotify} --urgency ${urgency} ${urgency} urgency"
-          )
-          [
-            "low"
-            "normal"
-            "critical"
-          ];
-    };
   };
 }

stylix/testbed/available-graphical-environments.nix
@@ -1,0 +1,5 @@
+{ lib }:
+lib.pipe ./graphical-environments [
+  builtins.readDir
+  (lib.mapAttrsToList (name: _: lib.removeSuffix ".nix" name))
+]

stylix/testbed/default.nix
@@ -13,19 +13,23 @@ let
       system = lib.nixosSystem {
         inherit (pkgs) system;
 
-        modules = [
-          ./modules/common.nix
-          ./modules/enable.nix
-          ./modules/application.nix
-          inputs.self.nixosModules.stylix
-          inputs.home-manager.nixosModules.home-manager
-          testbed
+        modules =
+          [
+            ./modules/common.nix
+            ./modules/enable.nix
+            ./modules/application.nix
+            inputs.self.nixosModules.stylix
+            inputs.home-manager.nixosModules.home-manager
+            testbed
 
-          # modules for external targets
-          inputs.nvf.nixosModules.default
-          inputs.nixvim.nixosModules.nixvim
-          inputs.spicetify-nix.nixosModules.spicetify
-        ];
+            # modules for external targets
+            inputs.nvf.nixosModules.default
+            inputs.nixvim.nixosModules.nixvim
+            inputs.spicetify-nix.nixosModules.spicetify
+          ]
+          ++ map (name: import ./graphical-environments/${name}.nix) (
+            import ./available-graphical-environments.nix { inherit lib; }
+          );
       };
     in
     pkgs.writeShellApplication {

commit 03699ed214f6e8195bc7199d6ae3aeccf9732b08

stylix/testbed: respect xserver rename for gnome (#1714)
Fixes: 8c854fe383 ("stylix: allow choosing testbed desktop (#1222)")
Link: https://github.com/nix-community/stylix/pull/1714

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/testbed/graphical-environments/gnome.nix
@@ -8,8 +8,7 @@
   config =
     lib.mkIf (config.stylix.testbed.ui.graphicalEnvironment or null == "gnome")
       {
-        services.xserver = {
-          enable = true;
+        services = {
           displayManager.gdm.enable = true;
           desktopManager.gnome.enable = true;
         };