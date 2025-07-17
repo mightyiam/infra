52804c7c81fbe34e435113e253c002f788c096bf
awwpotato <awwpotato@voidq.com>
2025-07-17 03:56:53 -0700

    waybar: add testbed (#1712)
    Link: https://github.com/nix-community/stylix/pull/1712
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

---
+++ modules/waybar/testbeds/waybar.nix
@@ -0,0 +1,41 @@
+{ lib, ... }:
+{
+  stylix.testbed.ui = {
+    graphicalEnvironment = "hyprland";
+    command.text = "waybar";
+  };
+
+  home-manager.sharedModules = lib.singleton {
+    programs.waybar = {
+      enable = true;
+      settings = lib.singleton {
+        modules-left = [
+          "hyprland/workspaces"
+          "hyprland/submap"
+          "custom/media"
+        ];
+        modules-center = [
+          "hyprland/window"
+        ];
+        modules-right = [
+          "mpd"
+          "idle_inhibitor"
+          "pulseaudio"
+          "network"
+          "power-profiles-daemon"
+          "cpu"
+          "memory"
+          "temperature"
+          "backlight"
+          "keyboard-state"
+          "hyprland/language"
+          "battery"
+          "battery#bat2"
+          "clock"
+          "tray"
+          "custom/power"
+        ];
+      };
+    };
+  };
+}
