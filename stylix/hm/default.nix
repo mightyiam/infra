danth/stylix

commit f3c2f5a1796cbba1e1685270cf0f5a66f118ea96

stylix: set GTK icon theme (#603)
Closes: https://github.com/danth/stylix/issues/458
Link: https://github.com/danth/stylix/pull/603

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Tested-by: Kamron Bhavnagri <kamwithk@tuta.io>

stylix/hm/default.nix
@@ -11,6 +11,7 @@ in {
     ../opacity.nix
     ./cursor.nix
     ./fonts.nix
+	./icon.nix
     (import ./palette.nix { inherit palette-generator base16; })
     (import ../templates.nix inputs)
   ] ++ autoload;

stylix/hm/icon.nix
@@ -1,0 +1,22 @@
+{ config, lib, ... }:
+
+let
+	cfg = config.stylix.iconTheme;
+	inherit (config.stylix) polarity;
+in {
+	imports = [ ../icon.nix ];
+	config = lib.mkIf (config.stylix.enable && cfg.enable) {
+		gtk = {
+			iconTheme = {
+				inherit (cfg) package;
+				name = builtins.head (lib.filter (x: null != x) [
+				  ({
+					inherit (cfg) dark light;
+				  }."${polarity}" or null)
+				  cfg.dark
+				  cfg.light
+				]);
+			};
+		};
+	};
+}

stylix/icon.nix
@@ -1,0 +1,26 @@
+{ lib, ... }:
+
+{
+    options.stylix.iconTheme = {
+        enable = lib.mkOption {
+            description = "enable/disable icon theming.";
+            type = lib.types.bool;
+            default = false;
+        };
+        package = lib.mkOption {
+            description = "Package providing the icon theme.";
+            type = lib.types.nullOr lib.types.package;
+            default = null;
+        };
+        light = lib.mkOption {
+            description = "Light icon theme name.";
+            type = lib.types.nullOr lib.types.str;
+            default = null;
+        };
+        dark = lib.mkOption {
+            description = "Dark icon theme name.";
+            type = lib.types.nullOr lib.types.str;
+            default = null;
+        };
+    };
+}

mightyiam/infra

commit 699262a0f8cd42ea1e786351cf91b1403fa7598e

stylix: rename icon.nix to icon-theme.nix
Rename the icon.nix files to icon-theme.nix to better reflect its
declared iconTheme option.

Fixes: f3c2f5a1796c ("stylix: set GTK icon theme (#603)")

stylix/hm/default.nix
@@ -9,11 +9,11 @@ in
 {
   imports = [
     ./cursor.nix
-    ./icon.nix
+    ./icon-theme.nix
     ./palette.nix
     ../cursor.nix
     ../fonts.nix
-    ../icon.nix
+    ../icon-theme.nix
     ../opacity.nix
     ../palette.nix
     ../pixel.nix