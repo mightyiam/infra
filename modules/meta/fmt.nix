7aed7c4e24a7b929697b816c875cf1eeb0cd8ca8
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-22 14:02:52 +0700

    use pre-commit treefmt integration

--- modules/meta/fmt.nix
+++ modules/meta/fmt.nix
@@ -1,32 +1,27 @@
-{ lib, inputs, ... }:
+{ inputs, ... }:
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
-  perSystem =
-    { self', ... }:
-    {
-      treefmt = {
-        projectRootFile = "flake.nix";
-        programs = {
-          nixfmt.enable = true;
-          prettier.enable = true;
-          rustfmt.enable = true;
-          shfmt.enable = true;
-          yamlfmt.enable = true;
-        };
-        settings = {
-          on-unmatched = "fatal";
-          global.excludes = [
-            "*.jpg"
-            "*.png"
-            "*.toml"
-            "*/.gitignore"
-            "LICENSE"
-          ];
-        };
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs = {
+        nixfmt.enable = true;
+        prettier.enable = true;
+        rustfmt.enable = true;
+        shfmt.enable = true;
+        yamlfmt.enable = true;
       };
-      pre-commit.settings.hooks.nix-fmt = {
-        enable = true;
-        entry = lib.getExe self'.formatter;
+      settings = {
+        on-unmatched = "fatal";
+        global.excludes = [
+          "*.jpg"
+          "*.png"
+          "*.toml"
+          "*/.gitignore"
+          "LICENSE"
+        ];
       };
     };
+    pre-commit.settings.hooks.treefmt.enable = true;
+  };
 }
