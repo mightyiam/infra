0190c12941f7f0b051ae74ded11e9b8ffef6469a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-27 16:23:47 +0700

    pre-push input-branches hook

--- modules/meta/input-branches.nix
+++ modules/meta/input-branches.nix
@@ -47,8 +47,59 @@
     nixpkgs.flake.source = lib.mkForce (rootPath + "/inputs/nixpkgs");
   };
 
-  perSystem = psArgs: {
-    make-shells.default.packages = psArgs.config.input-branches.commands.all;
-    treefmt.settings.global.excludes = [ "${config.input-branches.baseDir}/*" ];
-  };
+  perSystem =
+    psArgs@{ pkgs, ... }:
+    {
+      make-shells.default.packages = psArgs.config.input-branches.commands.all;
+      treefmt.settings.global.excludes = [ "${config.input-branches.baseDir}/*" ];
+      pre-commit.settings.hooks.check-submodules-pushed = {
+        enable = true;
+        stages = [ "pre-push" ];
+        always_run = true;
+        verbose = true;
+        entry =
+          pkgs.writeShellApplication {
+            name = "check-submodules-pushed";
+            runtimeInputs = [
+              pkgs.git
+              pkgs.gnugrep
+            ];
+            text =
+              config.input-branches.inputs
+              |> lib.attrValues
+              |> map (
+                { path_, ... }:
+                # bash
+                ''
+                  (
+                    unset GIT_DIR
+                    cd ${path_}
+                    current_commit=$(git rev-parse --quiet HEAD)
+                    [ -z "$current_commit" ] && {
+                      echo "Error: could not find HEAD of submodule ${path_}"
+                      exit 1
+                    }
+                    status=$(git status --porcelain)
+                    echo "$status" | grep -q . && {
+                      echo "Error: submodule ${path_} not clean"
+                      exit 1
+                    }
+                    git fetch
+                    git ls-remote --heads | grep -q "$current_commit" || {
+                      echo "Error: submodule ${path_} commit $current_commit is not pushed"
+                      exit 1
+                    }
+                  )
+                ''
+              )
+              |> lib.concat [
+                ''
+                  set -o xtrace
+                ''
+              ]
+              |> lib.concatLines;
+          }
+          |> lib.getExe;
+      };
+    };
 }
