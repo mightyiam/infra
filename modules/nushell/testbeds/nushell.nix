4faa7be903dc4c4c2b09e977e21d71fb83216ece
awwpotato <awwpotato@voidq.com>
2025-07-28 03:25:18 -0700

    nushell: add testbed (#1785)
    Link: https://github.com/nix-community/stylix/pull/1785
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

---
+++ modules/nushell/testbeds/nushell.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = "nu";
+    useTerminal = true;
+  };
+
+  home-manager.sharedModules = lib.singleton {
+    programs.nushell.enable = true;
+  };
+}
