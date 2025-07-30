e0db8fc17af557765885a514c3752f332586a6af
awwpotato <awwpotato@voidq.com>
2025-07-30 05:55:42 -0700

    gnome/hm: use mkTarget (#1650)
    Link: https://github.com/nix-community/stylix/pull/1650
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/gnome/hm.nix
+++ modules/gnome/hm.nix
@@ -1,65 +1,33 @@
 {
+  mkTarget,
   pkgs,
   config,
   lib,
   ...
 }:
-let
-  inherit (config.stylix.fonts) sansSerif serif monospace;
-  fontSize = toString config.stylix.fonts.sizes.applications;
-  documentFontSize = toString (config.stylix.fonts.sizes.applications - 1);
-  cfg = config.stylix.targets.gnome;
+mkTarget {
+  name = "gnome";
+  humanName = "GNOME";
 
-  activator = pkgs.writeShellApplication {
-    name = "stylix-activate-gnome";
-    text = ''
-      get_exe() {
-        for directory in /run/current-system/sw/bin /usr/bin /bin; do
-          if [[ -f "$directory/$1" ]]; then
-            printf '%s\n' "$directory/$1"
-            return 0
-          fi
-        done
-        echo "Skipping '$1': command not found"
-        return 1
+  autoEnable = pkgs.stdenv.hostPlatform.isLinux;
+  autoEnableExpr = "pkgs.stdenv.hostPlatform.isLinux";
+
+  extraOptions.useWallpaper = config.lib.stylix.mkEnableWallpaper "GNOME" true;
+
+  configElements = [
+    (
+      { cfg, image }:
+      {
+        dconf.settings."org/gnome/desktop/background" = {
+          picture-uri = lib.mkIf cfg.useWallpaper "file://${image}";
+          picture-uri-dark = lib.mkIf cfg.useWallpaper "file://${image}";
+        };
       }
-
-      if gnome_extensions="$(get_exe gnome-extensions)"; then
-        extension='user-theme@gnome-shell-extensions.gcampax.github.com'
-
-        case "$1" in
-          reload)
-            "$gnome_extensions" disable "$extension"
-            "$gnome_extensions" enable "$extension"
-            ;;
-          enable)
-            "$gnome_extensions" enable "$extension"
-            ;;
-        esac
-      fi
-    '';
-  };
-in
-{
-  options.stylix.targets.gnome = {
-    enable = config.lib.stylix.mkEnableTargetWith {
-      name = "GNOME";
-      autoEnable = pkgs.stdenv.hostPlatform.isLinux;
-      autoEnableExpr = "pkgs.stdenv.hostPlatform.isLinux";
-    };
-    useWallpaper = config.lib.stylix.mkEnableWallpaper "GNOME" true;
-  };
-
-  config = lib.mkIf (config.stylix.enable && cfg.enable) {
-    home.packages = [ pkgs.gnomeExtensions.user-themes ];
-
-    dconf.settings = {
-      "org/gnome/desktop/background" = {
-        color-shading-type = "solid";
-        picture-options =
-          let
-            inherit (config.stylix) imageScalingMode;
-          in
+    )
+    (
+      { imageScalingMode }:
+      {
+        dconf.settings."org/gnome/desktop/background".picture-options =
           if imageScalingMode == "fit" then
             "scaled"
           else if imageScalingMode == "fill" then
@@ -71,50 +39,99 @@ in
           # Seemingly no tile support... :(
           else
             "zoom";
-        picture-uri = lib.mkIf cfg.useWallpaper "file://${config.stylix.image}";
-        picture-uri-dark = lib.mkIf cfg.useWallpaper "file://${config.stylix.image}";
-      };
-
-      "org/gnome/desktop/interface" = {
+      }
+    )
+    (
+      { polarity }:
+      {
         # We show the same colours regardless of this setting, and the quick
         # settings tile is removed. The value is still used by Epiphany to
         # request dark mode for websites which support it.
-        color-scheme =
-          if config.stylix.polarity == "dark" then "prefer-dark" else "default";
+        dconf.settings."org/gnome/desktop/interface".color-scheme =
+          if polarity == "dark" then "prefer-dark" else "default";
+      }
+    )
+    (
+      { fonts }:
+      let
+        fontSize = toString fonts.sizes.applications;
+        documentFontSize = toString (fonts.sizes.applications - 1);
+      in
+      {
+        dconf.settings."org/gnome/desktop/interface" = {
+          # Some GTK apps will use these font settings if they exist.
+          # i.e emacs-pgtk.
+          font-name = "${fonts.sansSerif.name} ${fontSize}";
+          document-font-name = "${fonts.serif.name}  ${documentFontSize}";
+          monospace-font-name = "${fonts.monospace.name} ${fontSize}";
+        };
+      }
+    )
+    (
+      { inputs, colors }:
+      let
+        activator = pkgs.writeShellApplication {
+          name = "stylix-activate-gnome";
+          text = ''
+            get_exe() {
+              for directory in /run/current-system/sw/bin /usr/bin /bin; do
+                if [[ -f "$directory/$1" ]]; then
+                  printf '%s\n' "$directory/$1"
+                  return 0
+                fi
+              done
+              echo "Skipping '$1': command not found"
+              return 1
+            }
 
-        # Some GTK apps will use these font settings if they exist.
-        # i.e emacs-pgtk.
-        font-name = "${sansSerif.name} ${fontSize}";
-        document-font-name = "${serif.name}  ${documentFontSize}";
-        monospace-font-name = "${monospace.name} ${fontSize}";
-      };
+            if gnome_extensions="$(get_exe gnome-extensions)"; then
+              extension='user-theme@gnome-shell-extensions.gcampax.github.com'
 
-      "org/gnome/shell/extensions/user-theme".name = "Stylix";
-    };
+              case "$1" in
+                reload)
+                  "$gnome_extensions" disable "$extension"
+                  "$gnome_extensions" enable "$extension"
+                  ;;
+                enable)
+                  "$gnome_extensions" enable "$extension"
+                  ;;
+              esac
+            fi
+          '';
+        };
+      in
+      {
+        home.packages = [ pkgs.gnomeExtensions.user-themes ];
 
-    xdg = {
-      dataFile."themes/Stylix/gnome-shell/gnome-shell.css" = {
-        source =
-          let
-            theme = pkgs.callPackage ./theme.nix {
-              inherit (config.lib.stylix) colors;
-              inherit (config.stylix) inputs;
-            };
-          in
-          "${theme}/share/gnome-shell/gnome-shell.css";
+        dconf.settings = {
+          "org/gnome/shell/extensions/user-theme".name = "Stylix";
+          "org/gnome/desktop/background".color-shading-type = "solid";
+        };
 
-        # Reload the extension so the new theme is applied immediately.
-        # (The extension doesn't watch the file for changes.)
-        onChange = "${lib.getExe activator} reload";
-      };
+        xdg = {
+          dataFile."themes/Stylix/gnome-shell/gnome-shell.css" = {
+            source =
+              let
+                theme = pkgs.callPackage ./theme.nix {
+                  inherit inputs colors;
+                };
+              in
+              "${theme}/share/gnome-shell/gnome-shell.css";
 
-      # Enable the extension after logging in.
-      configFile."autostart/stylix-activate-gnome.desktop".text = ''
-        [Desktop Entry]
-        Type=Application
-        Exec=${lib.getExe activator} enable
-        Name=Stylix: enable User Themes extension for GNOME Shell
-      '';
-    };
-  };
+            # Reload the extension so the new theme is applied immediately.
+            # (The extension doesn't watch the file for changes.)
+            onChange = "${lib.getExe activator} reload";
+          };
+
+          # Enable the extension after logging in.
+          configFile."autostart/stylix-activate-gnome.desktop".text = ''
+            [Desktop Entry]
+            Type=Application
+            Exec=${lib.getExe activator} enable
+            Name=Stylix: enable User Themes extension for GNOME Shell
+          '';
+        };
+      }
+    )
+  ];
 }
