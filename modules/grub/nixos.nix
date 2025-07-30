mightyiam/infra

commit 218d4424b0634f8e9e3af92df7ca807bda529255

treewide: remove redundant stylix.image Nix store copies (#1659)
Link: https://github.com/nix-community/stylix/pull/1659

Tested-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: awwpotato <awwpotato@voidq.com>

stylix/palette.nix
@@ -84,7 +84,7 @@ in
         default = pkgs.runCommand "palette.json" { } ''
           ${lib.getExe cfg.paletteGenerator} \
             "${cfg.polarity}" \
-            ${lib.escapeShellArg "${cfg.image}"} \
+            ${cfg.image} \
             "$out"
         '';
       };

modules/swaylock/hm.nix
@@ -71,7 +71,7 @@ mkTarget {
     (
       { cfg, image }:
       {
-        programs.swaylock.settings.image = lib.mkIf cfg.useWallpaper "${image}";
+        programs.swaylock.settings.image = lib.mkIf cfg.useWallpaper image;
       }
     )
     (

modules/wpaperd/hm.nix
@@ -25,7 +25,7 @@ mkTarget {
       in
       {
         services.wpaperd.settings.any = {
-          path = "${image}";
+          path = image;
         } // modeAttrs;
       }
     );

commit 3499e3ec704b00d2155e1be72bb8f34081e471e4

treewide: properly quote stylix.image when used as a shell argument
Closes: https://github.com/nix-community/stylix/issues/1720
Fixes: 218d4424b063 ("treewide: remove redundant stylix.image Nix store copies (#1659)")

modules/grub/nixos.nix
@@ -134,7 +134,11 @@ in
                 cfg.useWallpaper
               # Make sure the background image is .png by asking to convert it
               then
-                "${lib.getExe' pkgs.imagemagick "convert"} ${config.stylix.image} png32:$out/background.png"
+                ''
+                  ${lib.getExe' pkgs.imagemagick "convert"} \
+                    ${lib.escapeShellArg config.stylix.image} \
+                    "png32:$out/background.png"
+                ''
               else
                 "cp ${pixel "base00"} $out/background.png"
             }

stylix/palette.nix
@@ -83,7 +83,7 @@ in
         default = pkgs.runCommand "palette.json" { } ''
           ${lib.getExe cfg.paletteGenerator} \
             "${cfg.polarity}" \
-            ${cfg.image} \
+            ${lib.escapeShellArg cfg.image} \
             "$out"
         '';
       };

stylix/testbed/images.nix
@@ -9,7 +9,9 @@
   };
 
   light = fetchurl {
-    name = "three-bicycles.jpg";
+    # Includes a space to make sure the path is properly quoted via
+    # `lib.escapeShellArg` when used as a shell argument.
+    name = "three bicycles.jpg";
     url = "https://unsplash.com/photos/hwLAI5lRhdM/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE2MzYxNDcwfA&force=true";
     hash = "sha256-S0MumuBGJulUekoGI2oZfUa/50Jw0ZzkqDDu1nRkFUA=";
   };