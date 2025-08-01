7b07d4c4eb1a8616a6d401674b623cba03b32ad7
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-08-01 14:41:24 +0700

    bump inputs

--- modules/wife/auto-login.nix
+++ modules/wife/auto-login.nix
@@ -5,11 +5,5 @@
       enable = true;
       user = config.flake.meta.wife.username;
     };
-
-    # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-    systemd.services = {
-      "getty@tty1".enable = false;
-      "autovt@tty1".enable = false;
-    };
   };
 }
