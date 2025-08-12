344767c5aa3ffbe4792cefc668d4b386f072f1a5
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-08-12 13:12:59 +0700

    systemd-resolved

---
+++ modules/networking/dns.nix
@@ -0,0 +1,22 @@
+{
+  flake.modules.nixos.base = {
+    services.resolved = {
+      enable = true;
+      dnsovertls = "opportunistic";
+
+      fallbackDns = [
+        # https://developers.cloudflare.com/1.1.1.1
+        "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
+
+        # https://developers.google.com/speed/public-dns/docs/using
+        "8.8.8.8"
+        "8.8.4.4"
+        "2001:4860:4860::8888"
+        "2001:4860:4860::8844"
+      ];
+    };
+  };
+}
