0d452e3dd32ed168d5840ee33ce4ebcb236c9533
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-08-16 23:27:29 +0700

    move some stuff around

--- modules/formatting.nix
+++ modules/formatting.nix
@@ -1,5 +1,27 @@
 { inputs, ... }:
 {
+  imports = [ inputs.treefmt-nix.flakeModule ];
+
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs = {
+        prettier.enable = true;
+        shfmt.enable = true;
+      };
+      settings = {
+        on-unmatched = "fatal";
+        global.excludes = [
+          "*.jpg"
+          "*.png"
+          "*/.gitignore"
+          "LICENSE"
+        ];
+      };
+    };
+    pre-commit.settings.hooks.treefmt.enable = true;
+  };
+
   flake.modules.nixvim.astrea = {
     plugins = {
       lsp.keymaps.lspBuf."<space>f" = "format";
