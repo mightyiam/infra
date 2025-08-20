28312c563c0f8c79c34a3b64bc5c62a0929f850b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-08-20 14:16:57 +0700

    resolved: set DNS instead of FallbackDNS

--- modules/networking/dns.nix
+++ modules/networking/dns.nix
@@ -1,22 +1,31 @@
+{ lib, ... }:
 {
-  flake.modules.nixos.base = {
-    services.resolved = {
-      enable = true;
-      dnsovertls = "opportunistic";
+  flake.modules.nixos.base =
+    let
+      nameservers =
+        [
+          # https://developers.cloudflare.com/1.1.1.1
+          "1.1.1.1"
+          "1.0.0.1"
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
 
-      fallbackDns = [
-        # https://developers.cloudflare.com/1.1.1.1
-        "1.1.1.1"
-        "1.0.0.1"
-        "2606:4700:4700::1111"
-        "2606:4700:4700::1001"
-
-        # https://developers.google.com/speed/public-dns/docs/using
-        "8.8.8.8"
-        "8.8.4.4"
-        "2001:4860:4860::8888"
-        "2001:4860:4860::8844"
-      ];
+          # https://developers.google.com/speed/public-dns/docs/using
+          "8.8.8.8"
+          "8.8.4.4"
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
+        ]
+        |> lib.concatStringsSep " ";
+    in
+    {
+      services.resolved = {
+        enable = true;
+        dnsovertls = "opportunistic";
+        extraConfig = ''
+          [Resolve]
+          DNS=${nameservers}
+        '';
+      };
     };
-  };
 }
