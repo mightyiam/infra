af4e8c98c3502fa5ae483102154702218aefa2d8
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-08-21 22:52:48 +0700

    grimblast to satty scripts

--- modules/screenshots.nix
+++ modules/screenshots.nix
@@ -1,17 +1,63 @@
 { lib, ... }:
 {
   flake.modules.homeManager.gui =
-    { pkgs, ... }:
+    hmArgs@{ pkgs, ... }:
+    let
+      mkScript =
+        target:
+        pkgs.writeShellApplication {
+          name = "take-screenshot-and-edit-it-${target}";
+          runtimeInputs = [
+            pkgs.grimblast
+            hmArgs.config.programs.satty.package
+          ];
+          text = ''
+            grimblast save ${target} - | satty --filename -
+          '';
+        };
+    in
     {
       home.packages = [
-        pkgs.shotman
         pkgs.grimblast
+        hmArgs.config.programs.satty.package
       ];
 
-      wayland.windowManager.hyprland.settings.bind = [
-        "SUPER+SHIFT, w, exec, ${lib.getExe pkgs.grimblast} copy active"
-        "SUPER+SHIFT, o, exec, ${lib.getExe pkgs.grimblast} copy output"
-        "SUPER+SHIFT, r, exec, ${lib.getExe pkgs.grimblast} copy area"
-      ];
+      programs.satty = {
+        enable = true;
+        settings.general = {
+          corner-roundness = 6;
+          initial-tool = "brush";
+          copy-command = lib.getExe' pkgs.wl-clipboard-rs "wl-copy";
+          output-filename = "${hmArgs.config.xdg.userDirs.desktop}/screenshot-%Y-%m-%d_%H:%M:%S.png";
+          actions-on-enter = [ "save-to-clipboard" ];
+          actions-on-escape = [ "exit" ];
+        };
+      };
+
+      wayland.windowManager.hyprland.settings.bind =
+        [
+          {
+            key = "w";
+            target = "active";
+          }
+          {
+            key = "o";
+            target = "output";
+          }
+          {
+            key = "r";
+            target = "area";
+          }
+        ]
+        |> map (
+          { key, target }:
+          [
+            "SUPER+SHIFT"
+            key
+            "exec"
+            (mkScript target |> lib.getExe)
+          ]
+          |> lib.concatStringsSep ", "
+        );
     };
 }
