fd104dae1660f94966131cd0126577fd63dd4b65
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-09-19 09:05:39 +0700

    bump inputs

--- modules/launcher.nix
+++ modules/launcher.nix
@@ -1,44 +1,41 @@
 { lib, ... }:
 {
-  flake.modules.homeManager.gui =
-    hmArgs@{ pkgs, ... }:
-    {
-      programs.rofi = {
-        enable = true;
-        package = pkgs.rofi-wayland;
-        terminal = hmArgs.config.terminal.path;
-        extraConfig = {
-          show-icons = true;
-          modes = [
-            "run"
-            "drun"
-            "window"
-          ];
-          drun-match-fields = [
-            "name"
-            "generic"
-            "exec"
-          ];
-          drun-display-format = "{icon} {name}";
-          display-drun = "   Apps ";
-          display-run = "   Run ";
-          display-window = " 󰕰  Window";
-          display-Network = " 󰤨  Network";
-        };
-        theme = {
-          listview.columns = 1;
-          "*".width = 1200;
-        };
+  flake.modules.homeManager.gui = hmArgs: {
+    programs.rofi = {
+      enable = true;
+      terminal = hmArgs.config.terminal.path;
+      extraConfig = {
+        show-icons = true;
+        modes = [
+          "run"
+          "drun"
+          "window"
+        ];
+        drun-match-fields = [
+          "name"
+          "generic"
+          "exec"
+        ];
+        drun-display-format = "{icon} {name}";
+        display-drun = "   Apps ";
+        display-run = "   Run ";
+        display-window = " 󰕰  Window";
+        display-Network = " 󰤨  Network";
       };
-      wayland.windowManager =
-        let
-          rofi = lib.getExe hmArgs.config.programs.rofi.package;
-        in
-        {
-          hyprland.settings.bind = [
-            "SUPER, d, exec, ${rofi} -show drun"
-            "SUPER+SHIFT, d, exec, ${rofi} -show run"
-          ];
-        };
+      theme = {
+        listview.columns = 1;
+        "*".width = 1200;
+      };
     };
+    wayland.windowManager =
+      let
+        rofi = lib.getExe hmArgs.config.programs.rofi.package;
+      in
+      {
+        hyprland.settings.bind = [
+          "SUPER, d, exec, ${rofi} -show drun"
+          "SUPER+SHIFT, d, exec, ${rofi} -show run"
+        ];
+      };
+  };
 }
