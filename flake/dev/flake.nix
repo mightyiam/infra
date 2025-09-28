mightyiam/infra

commit a5c1532ab8bf9566331f83be91517e02132f3d99

flake: partition dev inputs (#1289)
Link: https://github.com/nix-community/stylix/pull/1289

Reviewed-by: Daniel Thwaites <danth@danth.me>
Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>
Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Tested-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

flake/dev/flake.nix
@@ -1,0 +1,54 @@
+{
+  description = ''
+    Private inputs for development purposes. These are used by the top level
+    flake in the `dev` partition, but do not appear in consumers' lock files.
+  '';
+
+  inputs = {
+    # We need to define a nixpkgs input so that other inputs can follow it.
+    # It is prefixed with 'dev-' to avoid shadowing the public flake's
+    # 'nixpkgs' input, which could affect local testing with --override-input.
+    #
+    # TODO: Replace
+    #
+    #           dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    #
+    #       with
+    #
+    #           dev-nixpkgs = {
+    #             url = "path:../..";
+    #             inputs = {
+    #               "<PUBLIC_INPUT_1>".follows = "";
+    #               "<PUBLIC_INPUT_2>".follows = "";
+    #               "<PUBLIC_INPUT_3>".follows = "";
+    #             };
+    #           };
+    #
+    #       once 26.05 is released, giving non-flake end-users and Stylix
+    #       contributors two LTS releases to adopt Nix 2.26+ [1]. Note that
+    #       non-nixpkgs public flake inputs should be disabled.
+    #
+    #       [1]: https://github.com/NixOS/nix/blob/d4f67fd46dfe2bc950bdfa14273f87b8a4c32e47/doc/manual/source/release-notes/rl-2.26.md?plain=1#L3-L11
+    dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # keep-sorted start block=yes newline_separated=yes
+    flake-compat.url = "github:edolstra/flake-compat";
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        nixpkgs.follows = "dev-nixpkgs";
+      };
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "dev-nixpkgs";
+    };
+    # keep-sorted end
+  };
+
+  # This flake is only used for its inputs.
+  outputs = _: { };
+}

flake.nix
@@ -41,28 +41,12 @@
       flake = false;
     };
 
-    flake-compat.url = "github:edolstra/flake-compat";
-
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
     gnome-shell = {
       # TODO: Unlocking the input and pointing to official repository requires
       # updating the patch:
       # https://github.com/nix-community/stylix/pull/224#discussion_r1460339607.
       url = "github:GNOME/gnome-shell/48.2";
       flake = false;
-    };
-
-    # The 'home-manager' input is used to generate the documentation.
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nur = {

.github/workflows/update-flake.yml
@@ -41,22 +41,40 @@ jobs:
             email "$id+$name@users.noreply.github.com" \
             >>"$GITHUB_OUTPUT"
 
-      - uses: DeterminateSystems/update-flake-lock@v26
-        with:
-          token: ${{ steps.generate-token.outputs.token }}
-          git-committer-name: ${{ steps.user-info.outputs.name }}
-          git-committer-email: ${{ steps.user-info.outputs.email }}
-          git-author-name: ${{ steps.user-info.outputs.name }}
-          git-author-email: ${{ steps.user-info.outputs.email }}
-          branch: update_flake_lock_action_${{ matrix.branch }}
-          commit-msg: "flake: update all inputs"
-          pr-title: "${{ startsWith(matrix.branch, 'release') && format('[{0}] ', matrix.branch) || '' }}stylix: update all flake inputs"   # yamllint disable-line rule:line-length
-          pr-labels: "topic: dependencies"
-          pr-body: >
-            Automated changes by the
-            [update-flake-lock](https://github.com/DeterminateSystems/update-flake-lock)
-            GitHub Action.
+      - name: setup git
+        env:
+          name: ${{ steps.user-info.outputs.name }}
+          email: ${{ steps.user-info.outputs.email }}
+        run: |
+          git config --global user.name "$name"
+          git config --global user.email "$email"
 
-            ```
-            {{ env.GIT_COMMIT_MESSAGE }}
-            ```
+      - name: update lock files
+        run: |
+          nix flake update \
+            --commit-lock-file \
+            --option commit-lock-file-summary "flake: update root inputs"
+
+          nix flake update \
+            --commit-lock-file \
+            --flake ./flake/dev \
+            --option commit-lock-file-summary "flake: update dev inputs"
+
+      - name: create pull request
+        env:
+          GH_TOKEN: ${{ steps.generate-token.outputs.token }}
+          base_branch: ${{ matrix.branch }}
+          pr_branch: update_flake_lock_action_${{ matrix.branch }}
+          title: "${{ startsWith(matrix.branch, 'release') && format('[{0}] ', matrix.branch) || '' }}flake: update public and dev inputs"  # yamllint disable-line rule:line-length
+        run: |
+          git switch --create "$pr_branch"
+
+          git push origin "$pr_branch" \
+            --force \
+            --set-upstream
+
+          gh pr create \
+            --base "$base_branch" \
+            --title "$title" \
+            --label "topic: dependencies" ||
+            echo "Failed to create PR"

default.nix
@@ -1,7 +1,7 @@
 (import (
   let
     inherit (lock.nodes.flake-compat.locked) narHash rev;
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    lock = builtins.fromJSON (builtins.readFile ./flake/dev/flake.lock);
   in
   fetchTarball {
     url = "https://github.com/edolstra/flake-compat/archive/${rev}.tar.gz";

flake/default.nix
@@ -1,86 +1,21 @@
+{ inputs, lib, ... }:
 {
   imports = [
+    inputs.flake-parts.flakeModules.partitions
     ./deprecation
-    ./dev-shell.nix
     ./modules.nix
     ./packages.nix
-    ./pre-commit.nix
+    ./propagated-packages.nix
   ];
 
-  perSystem =
-    { pkgs, ... }:
-    {
-      # TODO: consider using https://flake.parts/options/treefmt-nix.html once
-      # dev flake is partitioned
-      formatter = pkgs.treefmt.withConfig {
-        runtimeInputs = with pkgs; [
-          # keep-sorted start
-          biome
-          keep-sorted
-          nixfmt-rfc-style
-          ruff
-          stylish-haskell
-          # keep-sorted end
-        ];
+  partitions.dev = {
+    module = ./dev;
+    extraInputsFlake = ./dev;
+  };
 
-        settings = {
-          on-unmatched = "info";
-          tree-root-file = "flake.nix";
-
-          formatter = {
-            # keep-sorted start block=yes
-            biome = {
-              command = "biome";
-              options = [
-                "format"
-                "--write"
-                "--no-errors-on-unmatched"
-                "--config-path"
-                (pkgs.writers.writeJSON "biome.json" {
-                  formatter = {
-                    indentStyle = "space";
-                    indentWidth = 2;
-                    lineWidth = 80;
-                  };
-                })
-              ];
-              includes = [
-                "*.css"
-                "*.js"
-                "*.json"
-              ];
-              excludes = [
-                # Contains custom syntax that biome can't handle
-                "modules/swaync/base.css"
-              ];
-            };
-            keep-sorted = {
-              command = "keep-sorted";
-              includes = [ "*" ];
-            };
-            nixfmt = {
-              command = "nixfmt";
-              options = [ "--width=80" ];
-              includes = [ "*.nix" ];
-            };
-            ruff = {
-              command = "ruff";
-              options = [
-                "--config"
-                (pkgs.writers.writeTOML "ruff.toml" {
-                  line-length = 80;
-                })
-                "format"
-              ];
-              includes = [ "*.py" ];
-            };
-            stylish-haskell = {
-              command = "stylish-haskell";
-              includes = [ "*.hx" ];
-            };
-            # keep-sorted end
-          };
-        };
-      };
-    };
+  partitionedAttrs = lib.genAttrs [
+    "checks"
+    "devShells"
+    "formatter"
+  ] (_: "dev");
 }

flake/dev/default.nix
@@ -1,0 +1,84 @@
+{
+  imports = [
+    ./dev-shell.nix
+    ./nixpkgs-and-dev-nixpkgs-version-consistency.nix
+    ./packages.nix
+    ./pre-commit.nix
+  ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+      # TODO: consider using https://flake.parts/options/treefmt-nix.html
+      formatter = pkgs.treefmt.withConfig {
+        runtimeInputs = with pkgs; [
+          # keep-sorted start
+          biome
+          keep-sorted
+          nixfmt-rfc-style
+          ruff
+          stylish-haskell
+          # keep-sorted end
+        ];
+
+        settings = {
+          on-unmatched = "info";
+          tree-root-file = "flake.nix";
+
+          formatter = {
+            # keep-sorted start block=yes
+            biome = {
+              command = "biome";
+              options = [
+                "format"
+                "--write"
+                "--no-errors-on-unmatched"
+                "--config-path"
+                (pkgs.writers.writeJSON "biome.json" {
+                  formatter = {
+                    indentStyle = "space";
+                    indentWidth = 2;
+                    lineWidth = 80;
+                  };
+                })
+              ];
+              includes = [
+                "*.css"
+                "*.js"
+                "*.json"
+              ];
+              excludes = [
+                # Contains custom syntax that biome can't handle
+                "modules/swaync/base.css"
+              ];
+            };
+            keep-sorted = {
+              command = "keep-sorted";
+              includes = [ "*" ];
+            };
+            nixfmt = {
+              command = "nixfmt";
+              options = [ "--width=80" ];
+              includes = [ "*.nix" ];
+            };
+            ruff = {
+              command = "ruff";
+              options = [
+                "--config"
+                (pkgs.writers.writeTOML "ruff.toml" {
+                  line-length = 80;
+                })
+                "format"
+              ];
+              includes = [ "*.py" ];
+            };
+            stylish-haskell = {
+              command = "stylish-haskell";
+              includes = [ "*.hx" ];
+            };
+            # keep-sorted end
+          };
+        };
+      };
+    };
+}

flake/dev/nixpkgs-and-dev-nixpkgs-version-consistency.nix
@@ -1,0 +1,24 @@
+{ inputs, ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      checks.nixpkgs-and-dev-nixpkgs-version-consistency =
+        pkgs.runCommand "nixpkgs-and-dev-nixpkgs-version-consistency"
+          {
+            dev_nixpkgs = inputs.dev-nixpkgs.narHash;
+            nixpkgs = inputs.nixpkgs.narHash;
+          }
+          ''
+            if [ "$nixpkgs" != "$dev_nixpkgs" ]; then
+              printf \
+                'inconsistent nixpkgs (%s) and dev-nixpkgs (%s) versions\n' \
+                "$nixpkgs" \
+                "$dev_nixpkgs" \
+                >&2
+              exit 1
+            fi
+            mkdir "$out"
+          '';
+    };
+}

commit 255dfc89026ea394cc76e94da8eaa70051494ab7

flake/dev/flake: match nixpkgs and dev-nixpkgs input references
Match nixpkgs and dev-nixpkgs input references, aligning with commit
90f95c5d8408 ("stylix: point nixpkgs input to more conventional
nixos-unstable ref (#712)").

Fixes: a5c1532ab8bf ("flake: partition dev inputs (#1289)")

flake/dev/flake.nix
@@ -120,7 +120,7 @@
   #
   #       [1]: https://github.com/NixOS/nix/blob/d4f67fd46dfe2bc950bdfa14273f87b8a4c32e47/doc/manual/source/release-notes/rl-2.26.md?plain=1#L3-L11
   inputs = {
-    dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    dev-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     dev-flake-parts = {
       url = "github:hercules-ci/flake-parts";