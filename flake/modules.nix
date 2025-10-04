503d989626aa41174b3a51f18528547da1afe572
musjj <72612857+musjj@users.noreply.github.com>
2025-10-04 23:32:58 +0700

    flake: add default modules (#1905)
    Link: https://github.com/nix-community/stylix/pull/1905
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    Reviewed-by: awwpotato <awwpotato@voidq.com>

--- flake/modules.nix
+++ flake/modules.nix
@@ -1,70 +1,82 @@
 { inputs, self, ... }:
 {
   flake = {
-    nixosModules.stylix =
-      { pkgs, ... }@args:
-      {
-        imports = [
-          ../stylix/nixos
-          {
-            stylix = {
-              inherit inputs;
-              paletteGenerator =
-                self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
-              base16 = inputs.base16.lib args;
-              homeManagerIntegration.module = self.homeModules.stylix;
-            };
-          }
-        ];
-      };
+    nixosModules = {
+      default = self.nixosModules.stylix;
+      stylix =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            ../stylix/nixos
+            {
+              stylix = {
+                inherit inputs;
+                paletteGenerator =
+                  self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
+                base16 = inputs.base16.lib args;
+                homeManagerIntegration.module = self.homeModules.stylix;
+              };
+            }
+          ];
+        };
+    };
 
-    homeModules.stylix =
-      { pkgs, ... }@args:
-      {
-        imports = [
-          ../stylix/hm
-          {
-            stylix = {
-              inherit inputs;
-              paletteGenerator =
-                self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
-              base16 = inputs.base16.lib args;
-            };
-          }
-        ];
-      };
+    homeModules = {
+      default = self.homeModules.stylix;
+      stylix =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            ../stylix/hm
+            {
+              stylix = {
+                inherit inputs;
+                paletteGenerator =
+                  self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
+                base16 = inputs.base16.lib args;
+              };
+            }
+          ];
+        };
+    };
 
-    darwinModules.stylix =
-      { pkgs, ... }@args:
-      {
-        imports = [
-          ../stylix/darwin
-          {
-            stylix = {
-              inherit inputs;
-              paletteGenerator =
-                self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
-              base16 = inputs.base16.lib args;
-              homeManagerIntegration.module = self.homeModules.stylix;
-            };
-          }
-        ];
-      };
+    darwinModules = {
+      default = self.darwinModules.stylix;
+      stylix =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            ../stylix/darwin
+            {
+              stylix = {
+                inherit inputs;
+                paletteGenerator =
+                  self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
+                base16 = inputs.base16.lib args;
+                homeManagerIntegration.module = self.homeModules.stylix;
+              };
+            }
+          ];
+        };
+    };
 
-    nixOnDroidModules.stylix =
-      { pkgs, ... }@args:
-      {
-        imports = [
-          ../stylix/droid
-          {
-            stylix = {
-              paletteGenerator =
-                self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
-              base16 = inputs.base16.lib args;
-              homeManagerIntegration.module = self.homeModules.stylix;
-            };
-          }
-        ];
-      };
+    nixOnDroidModules = {
+      default = self.nixOnDroidModules.stylix;
+      stylix =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            ../stylix/droid
+            {
+              stylix = {
+                paletteGenerator =
+                  self.packages.${pkgs.stdenv.hostPlatform.system}.palette-generator;
+                base16 = inputs.base16.lib args;
+                homeManagerIntegration.module = self.homeModules.stylix;
+              };
+            }
+          ];
+        };
+    };
   };
 }
