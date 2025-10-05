a0d105d2eb8484322734c381eae097b276ceaa55
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-10-05 22:11:53 +0700

    tiny refactor

--- modules/shells/completion.nix
+++ modules/shells/completion.nix
@@ -1,9 +1,11 @@
 { lib, ... }:
 {
-  flake.modules.homeManager.base.programs.zsh = {
-    enableCompletion = true;
-    initContent = lib.mkOrder 550 ''
-      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-    '';
+  flake.modules.homeManager.base = {
+    programs.zsh = {
+      enableCompletion = true;
+      initContent = lib.mkOrder 550 ''
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+      '';
+    };
   };
 }
