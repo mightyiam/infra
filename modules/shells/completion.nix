19f74f501d7c51db387ca4c80fe37c252fcaede2
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-10-05 22:18:35 +0700

    carapace

--- modules/shells/completion.nix
+++ modules/shells/completion.nix
@@ -1,11 +1,14 @@
 { lib, ... }:
 {
   flake.modules.homeManager.base = {
-    programs.zsh = {
-      enableCompletion = true;
-      initContent = lib.mkOrder 550 ''
-        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-      '';
+    programs = {
+      zsh = {
+        enableCompletion = true;
+        initContent = lib.mkOrder 550 ''
+          zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        '';
+      };
+      carapace.enable = true;
     };
   };
 }
