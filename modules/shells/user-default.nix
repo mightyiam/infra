48e45ec9452902a17133c9ded042042c573bbfdf
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-10-05 17:06:18 +0700

    default shell nushell

--- modules/shells/user-default.nix
+++ modules/shells/user-default.nix
@@ -1,11 +1,13 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   flake.modules = {
     nixos.base =
-      { pkgs, ... }:
+      nixosArgs@{ pkgs, ... }:
       {
         programs.zsh.enable = true;
         users.defaultUserShell = pkgs.zsh;
+        users.users.${config.flake.meta.owner.username}.shell =
+          nixosArgs.config.home-manager.users.${config.flake.meta.owner.username}.programs.nushell.package;
       };
 
     nixOnDroid.base =
