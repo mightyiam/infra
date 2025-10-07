df203d754aaede9b11faab3d813ae4e51dd2fc31
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-10-07 21:53:41 +0700

    ftn-advertise no hackernews

--- modules/full-time-nix.nix
+++ modules/full-time-nix.nix
@@ -12,7 +12,6 @@
             "https://www.reddit.com/r/NixOS/"
             "https://x.com/i/communities/1579185771939958785"
             "https://www.linkedin.com/"
-            "https://news.ycombinator.com/submit"
             "https://discourse.nixos.org/c/links/12"
             "https://bsky.app/"
             "https://discord.com/channels/568306982717751326/785982126243315762"
