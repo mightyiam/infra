d07c2bb36b5dfb7136f5e379664c7ee034ece437
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-10-10 22:25:30 +0700

    stellarium

--- modules/wife/games.nix
+++ modules/wife/games.nix
@@ -8,7 +8,7 @@
         # pingus failed to build
         sgt-puzzles
         # possibly https://github.com/NixOS/nixpkgs/issues/399790
-        #stellarium
+        stellarium
       ];
     };
 }
