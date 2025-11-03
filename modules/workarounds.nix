baffce8fd4e852789c67fc487c113ff3742d6f19
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-11-03 18:21:08 +0700

    bump inputs

--- modules/workarounds.nix
+++ modules/workarounds.nix
@@ -1,12 +1,1 @@
-# https://github.com/nix-community/stylix/issues/1946
-let
-  polyModule = {
-    stylix.targets.qt.enable = false;
-  };
-in
-{
-  flake.modules = {
-    homeManager.base = polyModule;
-    nixos.pc = polyModule;
-  };
-}
+{ }
