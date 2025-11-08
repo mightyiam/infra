14eab478ffb496667fb567f8cb6fe3924047ee64
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-11-08 11:15:14 +0700

    tiny style change

--- modules/nixvim/treesitter.nix
+++ modules/nixvim/treesitter.nix
@@ -1,15 +1,17 @@
 {
-  flake.modules.nixvim.base.plugins.treesitter = {
-    enable = true;
-    settings = {
-      highlight.enable = true;
-      indent.enable = true;
-      incremental_selection = {
-        enable = true;
-        keymaps = {
-          init_selection = "<CR>";
-          node_incremental = "<CR>";
-          node_decremental = "<BS>";
+  flake.modules.nixvim.base = {
+    plugins.treesitter = {
+      enable = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = true;
+        incremental_selection = {
+          enable = true;
+          keymaps = {
+            init_selection = "<CR>";
+            node_incremental = "<CR>";
+            node_decremental = "<BS>";
+          };
         };
       };
     };
