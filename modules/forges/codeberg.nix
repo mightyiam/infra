ae815c0551d1aad253cd395424fad564c1de0969
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-11-19 18:32:52 +0700

    small style change

--- modules/forges/codeberg.nix
+++ modules/forges/codeberg.nix
@@ -1,7 +1,7 @@
 {
   # https://codeberg.org/Codeberg/org/src/commit/172350cbd945af6e5cb062a99444892161c2e5db/Imprint.md
-  flake.modules.nixos.base.programs.ssh.knownHosts = {
-    "codeberg.org".publicKey =
+  flake.modules.nixos.base = {
+    programs.ssh.knownHosts."codeberg.org".publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB";
   };
 }
