78173ead48102602e35311e6cc74ea990d4432eb
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-11-19 16:12:23 +0700

    telegram-desktop mimeapps

--- modules/messaging-apps/telegram.nix
+++ modules/messaging-apps/telegram.nix
@@ -1,7 +1,15 @@
+{ lib, ... }:
 {
   flake.modules.homeManager.gui =
     { pkgs, ... }:
     {
       home.packages = [ pkgs.telegram-desktop ];
+      xdg.mimeApps.defaultApplications =
+        [
+          "x-scheme-handler/tg"
+          "x-scheme-handler/tonsite"
+        ]
+        |> map (type: lib.nameValuePair type [ "org.telegram.desktop.desktop" ])
+        |> lib.listToAttrs;
     };
 }
