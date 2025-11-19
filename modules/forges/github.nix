6ff7ecdb4264be5953726ef6b12e81d637f008df
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-11-19 18:18:05 +0700

    github ssh known host public key

--- modules/forges/github.nix
+++ modules/forges/github.nix
@@ -5,28 +5,36 @@
       username = "mightyiam";
     };
 
-    modules.homeManager = {
-      base =
-        { pkgs, ... }:
-        {
-          programs.gh = {
-            package = pkgs.gh.overrideAttrs (oldAttrs: {
-              buildInputs = oldAttrs.buildInputs or [ ] ++ [ pkgs.makeWrapper ];
-              postInstall = oldAttrs.postInstall or "" + ''
-                wrapProgram $out/bin/gh --unset GITHUB_TOKEN
-              '';
-            });
-            enable = true;
-            settings.git_protocol = "ssh";
-          };
+    modules = {
+      nixos.base = {
+        # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+        programs.ssh.knownHosts."github.com".publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
 
-          home.packages = with pkgs; [ gh-dash ];
-        };
-      gui =
-        { pkgs, ... }:
-        {
-          home.packages = with pkgs; [ gh-markdown-preview ];
-        };
+      homeManager = {
+        base =
+          { pkgs, ... }:
+          {
+            programs.gh = {
+              package = pkgs.gh.overrideAttrs (oldAttrs: {
+                buildInputs = oldAttrs.buildInputs or [ ] ++ [ pkgs.makeWrapper ];
+                postInstall = oldAttrs.postInstall or "" + ''
+                  wrapProgram $out/bin/gh --unset GITHUB_TOKEN
+                '';
+              });
+              enable = true;
+              settings.git_protocol = "ssh";
+            };
+
+            home.packages = with pkgs; [ gh-dash ];
+          };
+        gui =
+          { pkgs, ... }:
+          {
+            home.packages = with pkgs; [ gh-markdown-preview ];
+          };
+      };
     };
   };
 }
