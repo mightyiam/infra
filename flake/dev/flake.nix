mightyiam/infra

commit 84e7ea0aa447fbc11293e76977302ab1ee0fab38

{nixvim,nvf}: add testbeds (#1579)
Link: https://github.com/nix-community/stylix/pull/1579

Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Co-authored-by: awwpotato <awwpotato@voidq.com>

flake/dev/flake.nix
@@ -4,32 +4,129 @@
     flake in the `dev` partition, but do not appear in consumers' lock files.
   '';
 
+  # We need to define the dev-flake-parts, dev-nixpkgs, and dev-systems inputs
+  # so that other inputs can follow it. They are prefixed with 'dev-' to avoid
+  # shadowing public flake inputs, which could affect local testing with
+  # --override-input.
+  #
+  # TODO: Remove the /flake/dev/public-and-dev-version-consistency.nix checks
+  #       and replace
+  #
+  #           inputs = {
+  #             dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  #
+  #             dev-flake-parts = {
+  #               url = "github:hercules-ci/flake-parts";
+  #               inputs.nixpkgs-lib.follows = "dev-nixpkgs";
+  #             };
+  #
+  #             dev-systems.url = "github:nix-systems/default";
+  #
+  #             # keep-sorted start block=yes newline_separated=yes
+  #             flake-compat.url = "github:edolstra/flake-compat";
+  #
+  #             git-hooks = {
+  #               url = "github:cachix/git-hooks.nix";
+  #               inputs = {
+  #                 flake-compat.follows = "flake-compat";
+  #                 nixpkgs.follows = "dev-nixpkgs";
+  #               };
+  #             };
+  #
+  #             home-manager = {
+  #               url = "github:nix-community/home-manager";
+  #               inputs.nixpkgs.follows = "dev-nixpkgs";
+  #             };
+  #
+  #             nixvim = {
+  #               url = "github:nix-community/nixvim";
+  #               inputs = {
+  #                 flake-parts.follows = "dev-flake-parts";
+  #                 nixpkgs.follows = "dev-nixpkgs";
+  #                 systems.follows = "dev-systems";
+  #               };
+  #             };
+  #
+  #             nvf = {
+  #               url = "github:NotAShelf/nvf";
+  #               inputs = {
+  #                 nixpkgs.follows = "dev-nixpkgs";
+  #                 systems.follows = "dev-systems";
+  #               };
+  #             };
+  #             # keep-sorted end
+  #           };
+  #
+  #       with
+  #
+  #           inputs = {
+  #             stylix = {
+  #               url = "path:../..";
+  #               inputs = {
+  #                 base16-fish.follows = "";
+  #                 base16-helix.follows = "";
+  #                 base16-vim.follows = "";
+  #                 base16.follows = "";
+  #                 firefox-gnome-theme.follows = "";
+  #                 gnome-shell.follows = "";
+  #                 nur.follows = "";
+  #                 tinted-foot.follows = "";
+  #                 tinted-kitty.follows = "";
+  #                 tinted-schemes.follows = "";
+  #                 tinted-tmux.follows = "";
+  #                 tinted-zed.follows = "";
+  #               };
+  #             };
+  #
+  #             # keep-sorted start block=yes newline_separated=yes
+  #             flake-compat.url = "github:edolstra/flake-compat";
+  #
+  #             git-hooks = {
+  #               url = "github:cachix/git-hooks.nix";
+  #               inputs = {
+  #                 flake-compat.follows = "flake-compat";
+  #                 nixpkgs.follows = "stylix/nixpkgs";
+  #               };
+  #             };
+  #
+  #             home-manager = {
+  #               url = "github:nix-community/home-manager";
+  #               inputs.nixpkgs.follows = "stylix/nixpkgs";
+  #             };
+  #
+  #             nixvim = {
+  #               url = "github:nix-community/nixvim";
+  #               inputs = {
+  #                 flake-parts.follows = "stylix/flake-parts";
+  #                 nixpkgs.follows = "stylix/nixpkgs";
+  #                 systems.follows = "stylix/systems";
+  #               };
+  #             };
+  #
+  #             nvf = {
+  #               url = "github:NotAShelf/nvf";
+  #               inputs = {
+  #                 nixpkgs.follows = "stylix/nixpkgs";
+  #                 systems.follows = "stylix/systems";
+  #               };
+  #             };
+  #             # keep-sorted end
+  #           };
+  #
+  #       once 26.05 is released, giving non-flake end-users and Stylix
+  #       contributors two LTS releases to adopt Nix 2.26+ [1]. Note that
+  #       non-nixpkgs public flake inputs should be disabled.
+  #
+  #       [1]: https://github.com/NixOS/nix/blob/d4f67fd46dfe2bc950bdfa14273f87b8a4c32e47/doc/manual/source/release-notes/rl-2.26.md?plain=1#L3-L11
   inputs = {
-    # We need to define a nixpkgs input so that other inputs can follow it.
-    # It is prefixed with 'dev-' to avoid shadowing the public flake's
-    # 'nixpkgs' input, which could affect local testing with --override-input.
-    #
-    # TODO: Replace
-    #
-    #           dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    #
-    #       with
-    #
-    #           dev-nixpkgs = {
-    #             url = "path:../..";
-    #             inputs = {
-    #               "<PUBLIC_INPUT_1>".follows = "";
-    #               "<PUBLIC_INPUT_2>".follows = "";
-    #               "<PUBLIC_INPUT_3>".follows = "";
-    #             };
-    #           };
-    #
-    #       once 26.05 is released, giving non-flake end-users and Stylix
-    #       contributors two LTS releases to adopt Nix 2.26+ [1]. Note that
-    #       non-nixpkgs public flake inputs should be disabled.
-    #
-    #       [1]: https://github.com/NixOS/nix/blob/d4f67fd46dfe2bc950bdfa14273f87b8a4c32e47/doc/manual/source/release-notes/rl-2.26.md?plain=1#L3-L11
     dev-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    dev-flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "dev-nixpkgs";
+    };
+
+    dev-systems.url = "github:nix-systems/default";
 
     # keep-sorted start block=yes newline_separated=yes
     flake-compat.url = "github:edolstra/flake-compat";
@@ -45,6 +142,23 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "dev-nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs = {
+        flake-parts.follows = "dev-flake-parts";
+        nixpkgs.follows = "dev-nixpkgs";
+        systems.follows = "dev-systems";
+      };
+    };
+
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs = {
+        nixpkgs.follows = "dev-nixpkgs";
+        systems.follows = "dev-systems";
+      };
     };
 
     treefmt-nix = {

flake/dev/default.nix
@@ -1,9 +1,9 @@
 {
   imports = [
     ./dev-shell.nix
-    ./nixpkgs-and-dev-nixpkgs-version-consistency.nix
     ./packages.nix
     ./pre-commit.nix
     ./treefmt.nix
+    ./public-and-dev-version-consistency.nix
   ];
 }

flake/dev/nixpkgs-and-dev-nixpkgs-version-consistency.nix
@@ -1,24 +1,0 @@
-{ inputs, ... }:
-{
-  perSystem =
-    { pkgs, ... }:
-    {
-      checks.nixpkgs-and-dev-nixpkgs-version-consistency =
-        pkgs.runCommand "nixpkgs-and-dev-nixpkgs-version-consistency"
-          {
-            dev_nixpkgs = inputs.dev-nixpkgs.narHash;
-            nixpkgs = inputs.nixpkgs.narHash;
-          }
-          ''
-            if [ "$nixpkgs" != "$dev_nixpkgs" ]; then
-              printf \
-                'inconsistent nixpkgs (%s) and dev-nixpkgs (%s) versions\n' \
-                "$nixpkgs" \
-                "$dev_nixpkgs" \
-                >&2
-              exit 1
-            fi
-            mkdir "$out"
-          '';
-    };
-}

flake/dev/public-and-dev-version-consistency.nix
@@ -1,0 +1,41 @@
+{ inputs, ... }:
+{
+  perSystem =
+    { lib, pkgs, ... }:
+    {
+      checks = lib.mkMerge (
+        map
+          (
+            input:
+            let
+              name = "${input}-and-dev-${input}-version-consistency";
+            in
+            {
+              ${name} =
+                let
+                  dev = inputs."dev-${input}".narHash;
+                  public = inputs.${input}.narHash;
+                in
+                pkgs.runCommand name { } ''
+                  if [ "${public}" != "${dev}" ]; then
+                    printf \
+                      'inconsistent ${input} (%s) and dev-${input} (%s) versions\n' \
+                      "${public}" \
+                      "${dev}" \
+                      >&2
+
+                    exit 1
+                  fi
+
+                  mkdir "$out"
+                '';
+            }
+          )
+          [
+            "flake-parts"
+            "nixpkgs"
+            "systems"
+          ]
+      );
+    };
+}

modules/neovim/testbeds/nixvim.nix
@@ -1,0 +1,18 @@
+{ lib, pkgs, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = "nvim example.md";
+    useTerminal = true;
+  };
+
+  programs.nixvim.enable = true;
+
+  home-manager.sharedModules = lib.singleton {
+    home.file."example.md" = {
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/sharkdp/bat/refs/heads/master/tests/syntax-tests/source/Markdown/example.md";
+        hash = "sha256-VYYwgRFY1c2DPY7yGM8oF3zG4rtEpBWyqfPwmGZIkcA=";
+      };
+    };
+  };
+}

modules/neovim/testbeds/nvf.nix
@@ -1,0 +1,18 @@
+{ lib, pkgs, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = "nvim example.md";
+    useTerminal = true;
+  };
+
+  programs.nvf.enable = true;
+
+  home-manager.sharedModules = lib.singleton {
+    home.file."example.md" = {
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/sharkdp/bat/refs/heads/master/tests/syntax-tests/source/Markdown/example.md";
+        hash = "sha256-VYYwgRFY1c2DPY7yGM8oF3zG4rtEpBWyqfPwmGZIkcA=";
+      };
+    };
+  };
+}

stylix/testbed/default.nix
@@ -20,6 +20,10 @@ let
           inputs.self.nixosModules.stylix
           inputs.home-manager.nixosModules.home-manager
           testbed
+
+          # modules for external targets
+          inputs.nvf.nixosModules.default
+          inputs.nixvim.nixosModules.nixvim
         ];
       };
     in

commit 89e14e6e6b2d8b5894b3800af29e247ef0d446ec

flake/dev/flake: override nvf's flake-parts input
Fixes: 84e7ea0aa447 ("{nixvim,nvf}: add testbeds (#1579)")

flake/dev/flake.nix
@@ -160,6 +160,7 @@
         nixpkgs.follows = "dev-nixpkgs";
         systems.follows = "dev-systems";
         flake-compat.follows = "";
+        flake-parts.follows = "dev-flake-parts";
       };
     };
 