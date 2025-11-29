9cd0ea39b50a75000a4d41923c620ffff6c7a7ae
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-11-29 14:14:51 +0700

    single pkgs instance

---
+++ modules/nixpkgs/instance.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  config,
+  inputs,
+  withSystem,
+  ...
+}:
+{
+  options.nixpkgs = {
+    config = {
+      allowUnfreePredicate = lib.mkOption {
+        type = lib.types.functionTo lib.types.bool;
+        default = _: false;
+      };
+    };
+  };
+
+  config = {
+    perSystem =
+      { system, ... }:
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          inherit (config.nixpkgs) config;
+        };
+      };
+
+    flake.modules.nixos.base = nixosArgs: {
+      nixpkgs = {
+        pkgs = withSystem nixosArgs.config.facter.report.system (psArgs: psArgs.pkgs);
+        hostPlatform = nixosArgs.config.facter.report.system;
+      };
+    };
+  };
+}
