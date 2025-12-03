7420f203cac43514d6c9611b13c61fc169e7e37a
0xda157 <da157@voidq.com>
2025-12-03 13:46:54 -0800

    flake: add remaining drvs to ci.buildbot
    Closes: https://github.com/nix-community/stylix/issues/2056

--- flake/dev/public-and-dev-version-consistency.nix
+++ flake/dev/public-and-dev-version-consistency.nix
@@ -1,41 +1,44 @@
 { inputs, ... }:
 {
   perSystem =
-    { lib, pkgs, ... }:
     {
-      checks = lib.mkMerge (
-        map
-          (
-            input:
-            let
-              name = "${input}-and-dev-${input}-version-consistency";
-            in
-            {
-              ${name} =
-                let
-                  dev = inputs."dev-${input}".narHash;
-                  public = inputs.${input}.narHash;
-                in
-                pkgs.runCommand name { } ''
-                  if [ "${public}" != "${dev}" ]; then
-                    printf \
-                      'inconsistent ${input} (%s) and dev-${input} (%s) versions\n' \
-                      "${public}" \
-                      "${dev}" \
-                      >&2
+      lib,
+      pkgs,
+      system,
+      ...
+    }:
+    let
+      checks = lib.genAttrs' [ "flake-parts" "nixpkgs" "systems" ] (
+        input:
+        let
+          name = "${input}-and-dev-${input}-version-consistency";
+        in
+        lib.nameValuePair name (
+          let
+            dev = inputs."dev-${input}".narHash;
+            public = inputs.${input}.narHash;
+          in
+          pkgs.runCommand name { } ''
+            if [ "${public}" != "${dev}" ]; then
+              printf \
+                'inconsistent ${input} (%s) and dev-${input} (%s) versions\n' \
+                "${public}" \
+                "${dev}" \
+                >&2
 
-                    exit 1
-                  fi
+              exit 1
+            fi
 
-                  mkdir "$out"
-                '';
-            }
-          )
-          [
-            "flake-parts"
-            "nixpkgs"
-            "systems"
-          ]
+            mkdir "$out"
+          ''
+        )
       );
+    in
+    {
+      inherit checks;
+
+      ci.buildbot = lib.mkIf (system == "x86_64-linux") {
+        public-and-dev-version-consistency = pkgs.linkFarm "public-and-dev-version-consistency" checks;
+      };
     };
 }
