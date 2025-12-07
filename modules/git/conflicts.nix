87250d90b930674a68cdef0b12b1f6e52e588d04
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-12-07 10:10:05 +0700

    mergiraf

--- modules/git/conflicts.nix
+++ modules/git/conflicts.nix
@@ -1,8 +1,13 @@
 {
-  flake.modules.homeManager.base.programs.git = {
-    settings = {
-      merge.conflictstyle = "zdiff3";
-      rerere.enabled = true;
+  flake.modules.homeManager.base = {
+    programs = {
+      git = {
+        settings = {
+          merge.conflictstyle = "zdiff3";
+          rerere.enabled = true;
+        };
+      };
+      mergiraf.enable = true;
     };
   };
 }
