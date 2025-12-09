6a1a69b7b2ebf05a8161099e1bc812035835cf2b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-12-09 20:57:13 +0700

    nothing-but-nix nix-permission-edict

--- modules/ci.nix
+++ modules/ci.nix
@@ -32,6 +32,7 @@ let
       uses = "wimpysworld/nothing-but-nix@main";
       "with" = {
         hatchet-protocol = "holster";
+        nix-permission-edict = true;
       };
     };
     checkout = {
