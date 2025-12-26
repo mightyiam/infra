c239144485486a31face9f643a0814b63a8dc5a9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-12-26 22:17:52 +0700

    git-instafix

--- modules/git/rebasing.nix
+++ modules/git/rebasing.nix
@@ -1,9 +1,10 @@
 {
-  flake.modules.homeManager.base = {
-    home.packages = [
-      # https://github.com/quodlibetor/git-instafix/issues/39
-      # pkgs.git-instafix
-    ];
-    programs.git.settings.rebase.instructionFormat = "%d %s";
-  };
+  flake.modules.homeManager.base =
+    { pkgs, ... }:
+    {
+      home.packages = [
+        pkgs.git-instafix
+      ];
+      programs.git.settings.rebase.instructionFormat = "%d %s";
+    };
 }
