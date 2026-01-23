749285c90e3e35ebe0952c86838f3089abbc7939
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2026-01-23 22:15:31 +0100

    firefox: add reader mode support and improve firefoxGnomeTheme (#2126)
    Link: https://github.com/nix-community/stylix/pull/2126
    
    Reviewed-by: Flameopathic <flameopathic@gmail.com>
    Reviewed-by: 0xda157 <da157@voidq.com>
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/firefox/each-config.nix
+++ modules/firefox/each-config.nix
@@ -57,6 +57,21 @@ mkTarget {
       }
     )
     (
+      { cfg, colors }:
+      {
+        programs.${name}.profiles = lib.genAttrs cfg.profileNames (_: {
+          settings = {
+            "reader.color_scheme" = "custom";
+            "reader.custom_colors.background" = colors.withHashtag.base00;
+            "reader.custom_colors.foreground" = colors.withHashtag.base05;
+            "reader.custom_colors.selection-highlight" = colors.withHashtag.base04;
+            "reader.custom_colors.unvisited-links" = colors.withHashtag.base0D;
+            "reader.custom_colors.visited-links" = colors.withHashtag.base0E;
+          };
+        });
+      }
+    )
+    (
       {
         cfg,
         colors,
