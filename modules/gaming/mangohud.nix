0f27caf0904b324d2f526ffe44185901579bd1e3
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-01-26 07:22:58 +0700

    mangohud settings

--- modules/gaming/mangohud.nix
+++ modules/gaming/mangohud.nix
@@ -2,6 +2,12 @@
   flake.modules.homeManager.gui = {
     programs.mangohud = {
       enable = true;
+      settings = {
+        gamemode = true;
+        gpu_fan = true;
+        show_fps_limit = true;
+        fps = true;
+      };
     };
   };
 }
