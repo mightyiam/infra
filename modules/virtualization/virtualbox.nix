76787f32a31474c9f97561918dfbcc63df14a21d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-02-01 22:37:42 +0700

    virtualbox pc

--- modules/virtualization/virtualbox.nix
+++ modules/virtualization/virtualbox.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  flake.modules.nixos.workstation = {
+  flake.modules.nixos.pc = {
     # https://github.com/NixOS/nixpkgs/issues/449343
     # virtualisation.virtualbox.host.enable = true;
     users.extraGroups.vboxusers.members = [ config.flake.meta.owner.username ];
