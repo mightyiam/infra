f254396c2d2fb4e26edcf0f25f054a45d45ccde7
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-02-10 21:30:42 +0700

    typst-preview

--- modules/languages/typst.nix
+++ modules/languages/typst.nix
@@ -1,5 +1,8 @@
 {
   flake.modules.nixvim.base = {
-    plugins.lsp.servers.tinymist.enable = true;
+    plugins = {
+      lsp.servers.tinymist.enable = true;
+      typst-preview.enable = true;
+    };
   };
 }
