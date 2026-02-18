801843d10e9e22d7a00f660d069e2de70aa2980a
dastarruer <ayushpramanik399@gmail.com>
2026-02-18 11:34:04 -0400

    gitui: add testbed (#2206)
    Closes: https://github.com/nix-community/stylix/issues/1157
    Link: https://github.com/nix-community/stylix/pull/2206
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

---
+++ modules/gitui/testbeds/gitui.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = ''
+      ${lib.getExe pkgs.git} init
+      ${lib.getExe pkgs.gitui}
+    '';
+    useTerminal = true;
+  };
+
+  home-manager.sharedModules = lib.singleton {
+    programs = {
+      git.enable = true;
+      gitui.enable = true;
+    };
+  };
+}
