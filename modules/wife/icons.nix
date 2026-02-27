6c1444e362fd16ad8231614311279369ba6d3c1b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-02-27 14:47:14 +0700

    bump inputs

--- modules/wife/icons.nix
+++ modules/wife/icons.nix
@@ -10,23 +10,8 @@
           enable = true;
           dark = "Reversal-${variant}-dark";
           light = "Reversal-${variant}";
-          # https://github.com/NixOS/nixpkgs/issues/481518
-          package = pkgs.reversal-icon-theme.overrideAttrs {
-            installPhase = ''
-              runHook preInstall
-
-              mkdir -p $out/share/icons
-
-              name= ./install.sh \
-                -t ${variant} \
-                -d $out/share/icons
-
-              rm $out/share/icons/*/{AUTHORS,COPYING}
-
-              jdupes --quiet --link-soft --recurse $out/share
-
-              runHook postInstall
-            '';
+          package = pkgs.reversal-icon-theme.override {
+            colorVariants = [ variant ];
           };
         };
       };
