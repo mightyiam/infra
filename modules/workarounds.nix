8597551a09b9e072e35a874afd040b3a91dac59a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-03-08 16:02:32 +0700

    no stylix gtksourceview
    to avoid revuilding inkscape

--- modules/workarounds.nix
+++ modules/workarounds.nix
@@ -1,4 +1,14 @@
 {
+  flake.modules = {
+    # https://github.com/nix-community/stylix/discussions/2232
+    nixos.pc = {
+      stylix.targets.gtksourceview.enable = false;
+    };
+    homeManager.gui = {
+      stylix.targets.gtksourceview.enable = false;
+    };
+  };
+
   nixpkgs.overlays = [
     # https://github.com/NixOS/nixpkgs/pull/494547
     (final: prev: {
