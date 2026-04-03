6d0502ef7447090abf8b00362b5cda8ac64595b4
Karun Sandhu <karun@sndh.dev>
2026-04-03 22:15:34 +0200

    foot: set colors manually instead of relying on tinted-theming (#2235)
    Link: https://github.com/nix-community/stylix/pull/2235
    Closes: https://github.com/nix-community/stylix/issues/2234
    
    Tested-by: 0xda157 <da157@voidq.com>
    Reviewed-by: 0xda157 <da157@voidq.com>

--- modules/foot/hm.nix
+++ modules/foot/hm.nix
@@ -1,4 +1,4 @@
-{ lib, mkTarget, ... }:
+{ mkTarget, ... }:
 mkTarget {
   config = [
     (
@@ -11,19 +11,45 @@ mkTarget {
       }
     )
     (
-      { opacity }:
       {
-        programs.foot.settings.colors.alpha = opacity.terminal;
-      }
-    )
-    (
-      { colors, inputs }:
+        polarity,
+        opacity,
+        colors,
+      }:
+      let
+        colorTheme = if polarity == "either" then "light" else polarity;
+      in
       {
-        programs.foot.settings.main.include = lib.singleton (
-          toString (colors {
-            templateRepo = inputs.tinted-foot;
-          })
-        );
+        programs.foot.settings = {
+          main.initial-color-theme = colorTheme;
+          "colors-${colorTheme}" = {
+            alpha = opacity.terminal;
+            foreground = colors.base05;
+            background = colors.base00;
+            regular0 = colors.base00;
+            regular1 = colors.base08;
+            regular2 = colors.base0B;
+            regular3 = colors.base0A;
+            regular4 = colors.base0D;
+            regular5 = colors.base0E;
+            regular6 = colors.base0C;
+            regular7 = colors.base05;
+            bright0 = colors.base03;
+            bright1 = colors.base08;
+            bright2 = colors.base0B;
+            bright3 = colors.base0A;
+            bright4 = colors.base0D;
+            bright5 = colors.base0E;
+            bright6 = colors.base0C;
+            bright7 = colors.base07;
+            "16" = colors.base09;
+            "17" = colors.base0F;
+            "18" = colors.base01;
+            "19" = colors.base02;
+            "20" = colors.base04;
+            "21" = colors.base06;
+          };
+        };
       }
     )
   ];
