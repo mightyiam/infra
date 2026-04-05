b20401be46a4afa88f8a9bf9e755d149f25915a3
MD-Gonzalez <matias.daniel.gonzalez@proton.me>
2026-04-05 19:29:53 -0300

    hyprpaper: add monitor option (#2247)
    Link: https://github.com/nix-community/stylix/pull/2247
    
    Co-authored-by: Nuuvv <nuuvvtwitch@gmail.com>
    Co-authored-by: 0xda157 <da157@voidq.com>
    
    Reviewed-by: 0xda157 <da157@voidq.com>

--- modules/hyprpaper/hm.nix
+++ modules/hyprpaper/hm.nix
@@ -1,11 +1,20 @@
 { lib, mkTarget, ... }:
 mkTarget {
+  options = {
+    monitor = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      example = "DP-1";
+      description = ''Monitor(s) to apply the wallpaper to (`""` matches all monitors)'';
+    };
+  };
+
   config =
-    { image }:
+    { image, cfg }:
     {
       services.hyprpaper.settings = {
         wallpaper = lib.singleton {
-          monitor = "";
+          inherit (cfg) monitor;
           path = image;
         };
         splash = false;
