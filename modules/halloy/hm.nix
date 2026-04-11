d3e447786b74d62c75f665e17cb3e681c66e90c7
dastarruer <ayushpramanik399@gmail.com>
2026-04-11 15:45:57 -0400

    halloy: add font configuration (#2270)
    Link: https://github.com/nix-community/stylix/pull/2270
    Closes: https://github.com/nix-community/stylix/issues/2261
    
    Tested-by: 0xda157 <da157@voidq.com>
    Reviewed-by: 0xda157 <da157@voidq.com>

--- modules/halloy/hm.nix
+++ modules/halloy/hm.nix
@@ -1,60 +1,72 @@
 { mkTarget, ... }:
 mkTarget {
-  config =
-    { colors }:
-    {
-      programs.halloy = {
-        settings.theme = "stylix";
-        themes.stylix = with colors.withHashtag; {
-          general = {
-            background = base00;
-            border = base07;
-            horizontal_rule = base02;
-            unread_indicator = base0A;
-          };
-          text = {
-            primary = base05;
-            secondary = base04;
-            tertiary = base0A;
-            success = base0B;
-            error = base08;
-          };
-          buffer = {
-            action = base0B;
-            background = base00;
-            background_text_input = base01;
-            background_title_bar = base01;
-            border = base03;
-            border_selected = base07;
-            code = base0E;
-            highlight = base01;
-            nickname = base0C;
-            selection = base02;
-            timestamp = base05;
-            topic = base04;
-            url = base0D;
-            server_messages = {
-              join = base0B;
-              part = base08;
-              quit = base08;
-              default = base0C;
-            };
-          };
-          buttons = {
-            primary = {
+  config = [
+    (
+      { colors }:
+      {
+        programs.halloy = {
+          settings.theme = "stylix";
+          themes.stylix = with colors.withHashtag; {
+            general = {
               background = base00;
-              background_hover = base02;
-              background_selected = base03;
-              background_selected_hover = base04;
+              border = base07;
+              horizontal_rule = base02;
+              unread_indicator = base0A;
             };
-            secondary = {
-              background = base01;
-              background_hover = base02;
-              background_selected = base03;
-              background_selected_hover = base04;
+            text = {
+              primary = base05;
+              secondary = base04;
+              tertiary = base0A;
+              success = base0B;
+              error = base08;
+            };
+            buffer = {
+              action = base0B;
+              background = base00;
+              background_text_input = base01;
+              background_title_bar = base01;
+              border = base03;
+              border_selected = base07;
+              code = base0E;
+              highlight = base01;
+              nickname = base0C;
+              selection = base02;
+              timestamp = base05;
+              topic = base04;
+              url = base0D;
+              server_messages = {
+                join = base0B;
+                part = base08;
+                quit = base08;
+                default = base0C;
+              };
+            };
+            buttons = {
+              primary = {
+                background = base00;
+                background_hover = base02;
+                background_selected = base03;
+                background_selected_hover = base04;
+              };
+              secondary = {
+                background = base01;
+                background_hover = base02;
+                background_selected = base03;
+                background_selected_hover = base04;
+              };
             };
           };
         };
-      };
-    };
+      }
+    )
+    (
+      { fonts }:
+      {
+        programs.halloy.settings.font = {
+          family = fonts.monospace.name;
+          size = fonts.sizes.applications;
+        };
+      }
+    )
+  ];
 }
