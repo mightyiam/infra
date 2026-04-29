41c35b305d0228b9fff1fa2b53d12fa4884d7b1e
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-04-29 21:15:04 +0700

    ungoogled-chromium

--- modules/web-browsers/chromium.nix
+++ modules/web-browsers/chromium.nix
@@ -1,10 +1,15 @@
 {
-  flake.modules.homeManager.gui.programs.chromium = {
-    enable = true;
-    extensions = [
-      # Bitwarden
-      # https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb
-      { id = "nngceckbapebfimnlniiiahkandclblb"; }
-    ];
-  };
+  flake.modules.homeManager.gui =
+    { pkgs, ... }:
+    {
+      programs.chromium = {
+        enable = true;
+        package = pkgs.ungoogled-chromium;
+        extensions = [
+          # Bitwarden
+          # https://chrome.google.com/webstore/detail/bitwarden-free-password-m/nngceckbapebfimnlniiiahkandclblb
+          { id = "nngceckbapebfimnlniiiahkandclblb"; }
+        ];
+      };
+    };
 }
