2c7ed6d14ee0cd5f11490318bf04189fe55db5c9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-19 15:01:15 +0700

    statix in home and nixvim

--- modules/languages/nix/linting.nix
+++ modules/languages/nix/linting.nix
@@ -6,4 +6,18 @@
       statix.enable = true;
     };
   };
+
+  flake.modules = {
+    nixvim.base = {
+      plugins.lsp.servers.statix = {
+        enable = true;
+        package = null;
+      };
+    };
+    homeManager.base =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.statix ];
+      };
+  };
 }
