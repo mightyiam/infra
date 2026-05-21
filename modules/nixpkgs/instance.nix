59db5be84befafb2bbbdd876ee48034f2accf4cd
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-21 23:02:53 +0700

    nixpkgs.factory

--- modules/nixpkgs/instance.nix
+++ modules/nixpkgs/instance.nix
@@ -20,15 +20,22 @@
       type = lib.types.listOf lib.types.unspecified;
       default = [ ];
     };
+    factory = lib.mkOption {
+      type = lib.types.functionTo lib.types.pkgs;
+      readOnly = true;
+      default =
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          inherit (config.nixpkgs) config overlays;
+        };
+    };
   };
 
   config = {
     flake.modules.nixos.base = nixosArgs: {
       nixpkgs = {
-        pkgs = import inputs.nixpkgs {
-          inherit (nixosArgs.config.hardware.facter.report) system;
-          inherit (config.nixpkgs) config overlays;
-        };
+        pkgs = config.nixpkgs.factory nixosArgs.config.hardware.facter.report.system;
         hostPlatform = nixosArgs.config.hardware.facter.report.system;
       };
     };
