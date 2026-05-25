31d1c9149f9a3b9e92ff8c9b7d95b782ad92a77c
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-25 13:54:45 +0700

    environment.defaultPackages = []

--- modules/workarounds.nix
+++ modules/workarounds.nix
@@ -1,8 +1,14 @@
 {
   flake.modules = {
-    # https://github.com/nix-community/stylix/discussions/2232
-    nixos.pc = {
-      stylix.targets.gtksourceview.enable = false;
+    nixos = {
+      base = {
+        # https://github.com/NixOS/nixpkgs/issues/263289
+        environment.defaultPackages = [ ];
+      };
+      pc = {
+        # https://github.com/nix-community/stylix/discussions/2232
+        stylix.targets.gtksourceview.enable = false;
+      };
     };
     homeManager.gui = {
       stylix.targets.gtksourceview.enable = false;
