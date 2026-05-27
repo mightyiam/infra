e5f7424ff42797ff650bdf49ac619134133ca71d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-28 06:33:55 +0700

    yaml-language-server in home.packages

--- modules/languages/yaml.nix
+++ modules/languages/yaml.nix
@@ -1,11 +1,19 @@
 {
   perSystem.treefmt.programs.yamlfmt.enable = true;
 
-  flake.modules.nixvim.base = {
-    plugins.lsp.servers.yamlls = {
-      enable = true;
-      package = null;
+  flake.modules = {
+    nixvim.base = {
+      plugins.lsp.servers.yamlls = {
+        enable = true;
+        package = null;
+      };
     };
+
+    homeManager.base =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.yaml-language-server ];
+      };
   };
 
 }
