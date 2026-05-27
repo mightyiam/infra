1e717d93aa2f74c45686ce08a844898b87198cdf
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-27 20:23:04 +0700

    tiny style change

--- modules/xdg/portal.nix
+++ modules/xdg/portal.nix
@@ -1,6 +1,8 @@
 {
-  flake.modules.homeManager.gui.xdg.portal = {
-    enable = true;
-    config.common.default = "*";
+  flake.modules.homeManager.gui = {
+    xdg.portal = {
+      enable = true;
+      config.common.default = "*";
+    };
   };
 }
