95aaff3e429773aa25ad80be70e3ef91d32ea3dc
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-05-30 21:33:19 +0700

    better mini-files open keymap

--- modules/file-management/in-editor.nix
+++ modules/file-management/in-editor.nix
@@ -5,7 +5,16 @@
     keymaps = [
       {
         key = "<leader>.";
-        action = nixvim.mkRaw "MiniFiles.open";
+        action = nixvim.mkRaw ''
+          function()
+            if vim.bo.filetype == "minifiles" then
+              MiniFiles.close()
+              return
+            end
+
+            MiniFiles.open(vim.api.nvim_buf_get_name(0))
+          end
+        '';
         options.desc = "file manager";
       }
     ];
