6b77b408f31403ddad968bba72d8a08bbd019cda
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-10 16:53:24 +0700

    home-manager neovide

--- modules/mightyiam/neovide.nix
+++ modules/mightyiam/neovide.nix
@@ -1,19 +1,22 @@
 {lib, ...}: {
-  home.gui = hmArgs @ {pkgs, ...}: {
-    home.packages = [pkgs.neovide hmArgs.config.fonts.bitmapEmojiFont.package];
+  home.gui = hmArgs: {
+    home.packages = [hmArgs.config.fonts.bitmapEmojiFont.package];
+    gui.editor.command = lib.getExe hmArgs.config.programs.neovide.package;
+    home.sessionVariables.VISUAL = lib.getExe hmArgs.config.programs.neovide.package;
 
-    gui.editor.command = lib.getExe pkgs.neovide;
-    home.sessionVariables.VISUAL = lib.getExe pkgs.neovide;
-
-    xdg.configFile."neovide/config.toml".source = pkgs.writers.writeTOML "neovide/config.toml" {
-      box-drawing.mode = "native";
-      cursor_animation_length = 8.0e-2;
-      cursor_vfx_mode = "railgun";
-      cursor_vfx_particle_density = 20;
-      fork = true;
-      font = {
-        normal = ["monospace" hmArgs.config.fonts.bitmapEmojiFont.name];
-        size = hmArgs.config.stylix.fonts.sizes.applications;
+    programs.neovide = {
+      enable = true;
+      settings = {
+        box-drawing.mode = "native";
+        cursor_animation_length = 8.0e-2;
+        cursor_vfx_mode = "railgun";
+        cursor_vfx_particle_density = 20;
+        fork = true;
+        font.normal = [
+          # override stylix not being helpful
+          "monospace"
+          hmArgs.config.fonts.bitmapEmojiFont.name
+        ];
       };
     };
   };
