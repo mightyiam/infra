284f5382436bb5d12402b353c67f8e6377aec3d8
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-11 19:14:48 +0700

    fonts are not a nixos concern

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,5 +1,5 @@
-{lib, ...}: let
-  polyModule = polyArgs @ {pkgs, ...}: {
+{lib, ...}: {
+  homeManager.modules.gui = hmArgs @ {pkgs, ...}: {
     options.fonts.bitmapEmojiFont = {
       package = lib.mkOption {
         type = lib.types.package;
@@ -10,25 +10,19 @@
         default = "Twitter Color Emoji";
       };
     };
-    config.fonts.fontconfig = {
-      enable = true;
-      defaultFonts.monospace = [polyArgs.config.fonts.bitmapEmojiFont.name];
-    };
-  };
-in {
-  nixos.modules.pc = nixosArgs @ {pkgs, ...}: {
-    imports = [polyModule];
-    fonts.packages = [
-      nixosArgs.config.fonts.bitmapEmojiFont.package
-      pkgs.google-fonts
-      pkgs.gucharmap
-      pkgs.tlwg
-      pkgs.uni
-    ];
-  };
 
-  homeManager.modules.gui = hmArgs: {
-    imports = [polyModule];
-    home.packages = [hmArgs.config.fonts.bitmapEmojiFont.package];
+    config = {
+      fonts.fontconfig = {
+        enable = true;
+        defaultFonts.monospace = [hmArgs.config.fonts.bitmapEmojiFont.name];
+      };
+      home.packages = [
+        hmArgs.config.fonts.bitmapEmojiFont.package
+        pkgs.google-fonts
+        pkgs.gucharmap
+        pkgs.tlwg
+        pkgs.uni
+      ];
+    };
   };
 }
