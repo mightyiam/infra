6b4fcebe5498f9ac9dd046652dd1eaa46b308d64
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-12 08:44:43 +0700

    rm bitmap emoji font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,28 +1,11 @@
-{lib, ...}: {
-  homeManager.modules.gui = hmArgs @ {pkgs, ...}: {
-    options.fonts.bitmapEmojiFont = {
-      package = lib.mkOption {
-        type = lib.types.package;
-        default = pkgs.twitter-color-emoji;
-      };
-      name = lib.mkOption {
-        type = lib.types.str;
-        default = "Twitter Color Emoji";
-      };
-    };
-
-    config = {
-      fonts.fontconfig = {
-        enable = true;
-        defaultFonts.monospace = [hmArgs.config.fonts.bitmapEmojiFont.name];
-      };
-      home.packages = [
-        hmArgs.config.fonts.bitmapEmojiFont.package
-        pkgs.google-fonts
-        pkgs.gucharmap
-        pkgs.tlwg
-        pkgs.uni
-      ];
-    };
+{
+  homeManager.modules.gui = {pkgs, ...}: {
+    fonts.fontconfig.enable = true;
+    home.packages = [
+      pkgs.google-fonts
+      pkgs.gucharmap
+      pkgs.tlwg
+      pkgs.uni
+    ];
   };
 }
--- modules/mightyiam/neovide.nix
+++ modules/mightyiam/neovide.nix
@@ -1,6 +1,5 @@
 {lib, ...}: {
   home.gui = hmArgs: {
-    home.packages = [hmArgs.config.fonts.bitmapEmojiFont.package];
     gui.editor.command = lib.getExe hmArgs.config.programs.neovide.package;
 
     programs.neovide = {
@@ -14,7 +13,6 @@
         font.normal = [
           # override stylix not being helpful
           "monospace"
-          hmArgs.config.fonts.bitmapEmojiFont.name
         ];
       };
     };
