bdd118a2b7b50d9794a5e694cb9df5d6db1f65bb
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-13 21:00:54 +0700

    mkOrder imv

--- modules/mightyiam/graphical-imagery.nix
+++ modules/mightyiam/graphical-imagery.nix
@@ -1,10 +1,10 @@
-{
+{lib, ...}: {
   home.gui = {pkgs, ...}: {
     home.packages = with pkgs; [
       imv
       wl-color-picker
     ];
 
-    xdg.mimeApps.defaultApplicationPackages = [pkgs.imv];
+    xdg.mimeApps.defaultApplicationPackages = lib.mkOrder 50 [pkgs.imv];
   };
 }
