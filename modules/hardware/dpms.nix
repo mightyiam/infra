83e15fc29842c0ef79489905c7ffb3b8980202cb
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-18 11:33:30 +0700

    dpms-all via overlay

--- modules/hardware/dpms.nix
+++ modules/hardware/dpms.nix
@@ -1,30 +1,35 @@
-{withSystem, ...}: {
-  perSystem = {pkgs, ...}: {
-    # TODO via overlay
-    packages.dpms-all = pkgs.writeShellApplication {
-      name = "dpms-all";
-      runtimeInputs = with pkgs; [hyprland];
-      text = ''
-        if [ $# -ne 1 ]; then
-            echo "Usage: $0 [on|off]" >&2
-            exit 1
-        fi
-        if [ "$1" != "on" ] && [ "$1" != "off" ]; then
-            echo "Usage: $0 [on|off]" >&2
-            exit 1
-        fi
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      dpms-all = prev.writeShellApplication {
+        name = "dpms-all";
+        runtimeInputs = [prev.hyprland];
+        text = ''
+          if [ $# -ne 1 ]; then
+              echo "Usage: $0 [on|off]" >&2
+              exit 1
+          fi
+          if [ "$1" != "on" ] && [ "$1" != "off" ]; then
+              echo "Usage: $0 [on|off]" >&2
+              exit 1
+          fi
 
-        case "$XDG_CURRENT_DESKTOP" in
-            "Hyprland")
-                hyprctl dispatch dpms "$1"
-                ;;
-            *)
-                echo "Unsupported XDG_CURRENT_DESKTOP" >&2
-                exit 1
-                ;;
-        esac
-      '';
-    };
+          case "$XDG_CURRENT_DESKTOP" in
+              "Hyprland")
+                  hyprctl dispatch dpms "$1"
+                  ;;
+              *)
+                  echo "Unsupported XDG_CURRENT_DESKTOP" >&2
+                  exit 1
+                  ;;
+          esac
+        '';
+      };
+    })
+  ];
+
+  perSystem = {pkgs, ...}: {
+    packages = {inherit (pkgs) dpms-all;};
   };
 
   nixos.modules.base = {
@@ -32,8 +37,6 @@
   };
 
   home.gui = {pkgs, ...}: {
-    home.packages = [
-      (withSystem pkgs.stdenv.hostPlatform.system (psArgs: psArgs.config.packages.dpms-all))
-    ];
+    home.packages = [pkgs.dpms-all];
   };
 }
