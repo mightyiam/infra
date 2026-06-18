47fa3347f2c31aecdefa56fb97a494791ba10aba
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-18 16:06:58 +0700

    workaround typst-preview-nvim not opening

--- modules/mightyiam/languages/typst.nix
+++ modules/mightyiam/languages/typst.nix
@@ -6,4 +6,17 @@
     };
     plugins.typst-preview.enable = true;
   };
+
+  # https://github.com/chomosuke/typst-preview.nvim/issues/136
+  nixpkgs.overlays = [
+    (final: prev: {
+      vimPlugins = prev.vimPlugins.extend (final': prev': {
+        typst-preview-nvim = prev'.typst-preview-nvim.overrideAttrs (old: {
+          postPatch = ''
+            substituteInPlace lua/typst-preview/servers/factory.lua --replace-fail "'--no-open'," "'--no-open',''\n    '--verbose',"
+          '';
+        });
+      });
+    })
+  ];
 }
