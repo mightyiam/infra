f4d81575179be8a2879bc6f84fdf89d1f5114fcf
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-19 23:46:22 +0700

    delta differ

--- modules/mightyiam/diff.nix
+++ modules/mightyiam/diff.nix
@@ -6,11 +6,15 @@
         enable = true;
         git.enable = true;
       };
+      delta.enable = true;
       git.settings.diff.algorithm = "histogram";
 
       lazygit.settings.git.pagers = [
         {
-          externalDiffCommand = "${lib.getExe hmArgs.config.programs.difftastic.package}";
+          externalDiffCommand = "${lib.getExe hmArgs.config.programs.difftastic.package} --display=inline";
+        }
+        {
+          pager = "${lib.getExe hmArgs.config.programs.delta.package} --paging=never";
         }
         {} # default
       ];
