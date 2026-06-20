727a9a262542132abb813e8f3ba15677a9ca458a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-20 21:17:23 +0700

    lazygit intent to add

--- modules/mightyiam/git/tui.nix
+++ modules/mightyiam/git/tui.nix
@@ -6,6 +6,15 @@
         git = {
           autoFetch = false;
         };
+        customCommands = [
+          {
+            key = "N";
+            context = "files";
+            command = "git add --intent-to-add {{.SelectedFile.Name}}";
+            description = "record intent to add";
+            loadingText = "recording intent to add";
+          }
+        ];
       };
     };
   };
