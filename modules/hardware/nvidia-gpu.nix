2dd9358372670f4308873763598c9ad82f2c4780
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-06-30 11:02:18 +0700

    nvidia unfree package list in toplevel config

--- modules/hardware/nvidia-gpu.nix
+++ modules/hardware/nvidia-gpu.nix
@@ -2,12 +2,12 @@
   nixos.modules.nvidia-gpu = {
     specialisation.nvidia-gpu.configuration = {
       services.xserver.videoDrivers = ["nvidia"];
-
-      nixpkgs.config.allowUnfreePackages = [
-        "nvidia-kernel-modules"
-        "nvidia-settings"
-        "nvidia-x11"
-      ];
     };
   };
+
+  nixpkgs.config.allowUnfreePackages = [
+    "nvidia-kernel-modules"
+    "nvidia-settings"
+    "nvidia-x11"
+  ];
 }
