84a6b496b27c4d292ec18d57cc464a65d572add9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-07-02 11:29:06 +0700

    rm empty keymaps list

--- modules/armilaria/find-and-replace.nix
+++ modules/armilaria/find-and-replace.nix
@@ -11,25 +11,5 @@
 
       spectre.enable = true;
     };
-
-    keymaps = [
-      # TODO
-      # ["<leader>"] = {
-      #   s = {
-      #     s = {
-      #       '<cmd>lua require("spectre").toggle()<CR>',
-      #       "Toggle Spectre",
-      #     },
-      #     w = {
-      #       '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
-      #       "Search current word",
-      #     },
-      #     p = {
-      #       '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
-      #       "Search current file",
-      #     },
-      #   },
-      # },
-    ];
   };
 }
