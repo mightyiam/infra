8ae06f1cdb9a0cab2e72b3c45c3c5aed86ece69d
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-07-04 12:09:17 +0700

    users group can do iwd

--- modules/networking/manager.nix
+++ modules/networking/manager.nix
@@ -11,6 +11,18 @@
       networkmanager.wifi.backend = "iwd";
     };
 
+    services.dbus.packages = [
+      (pkgs.writeTextDir "share/dbus-1/system.d/99-iwd-group.conf" ''
+        <!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"
+         "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+        <busconfig>
+          <policy group="users">
+            <allow send_destination="net.connman.iwd"/>
+          </policy>
+        </busconfig>
+      '')
+    ];
+
     environment.systemPackages = [pkgs.impala];
   };
 }
