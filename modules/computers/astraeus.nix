86079758bdf0f13b4bcf13de09ed14b20424a244
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-07-05 16:48:08 +0700

    astraeus audio sink mapping HDMI

--- modules/computers/astraeus.nix
+++ modules/computers/astraeus.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   nixos.configurations.astraeus = {
     module = {
       networking.hostId = "5ddc3c42";
@@ -6,10 +10,16 @@
       services.openssh.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYvXn67X8v5UbC1STan+yUVC7xoCPuYn0V/m8qgt4hy root@astraeus";
       boot.partlabels = ["boot0"];
 
-      home-manager.users.mightyiam.audio.deviceNameMaps = {
-        "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink" = "SPEAKER";
-        "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Headphones__sink" = "JACK";
-      };
+      home-manager.users.mightyiam.audio.deviceNameMaps =
+        {
+          "Speaker" = "SPEAKER";
+          "Headphones" = "JACK";
+          "HDMI1" = "HDMI";
+        }
+        |> lib.mapAttrs' (substr: value: {
+          name = "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__${substr}__sink";
+          inherit value;
+        });
 
       imports = with config.nixos.modules; [
         efi
