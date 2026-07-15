9e7a526e90c9e5b7cdccb73a61a76b198828f6d4
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-07-15 14:46:09 +0700

    perSystem nixos/modules/misc/nixpkgs.nix
    Nix wth moment 29582: import the NixOS nixpkgs module into flake-parts perSystem

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,8 +1,4 @@
-{
-  lib,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   config = {
     flake-file.inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
 
@@ -11,31 +7,10 @@
       pkgs,
       ...
     }: {
-      options.nixpkgs = {
-        config = {
-          allowUnfreePredicate = lib.mkOption {
-            type = lib.types.functionTo lib.types.bool;
-            default = _: false;
-          };
-          allowUnfreePackages = lib.mkOption {
-            type = lib.types.listOf lib.types.singleLineStr;
-            default = [];
-          };
-        };
-        overlays = lib.mkOption {
-          type = lib.types.listOf lib.types.unspecified;
-          default = [];
-        };
-      };
-
-      config = {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          inherit (psArgs.config.nixpkgs) overlays config;
-        };
-
-        legacyPackages = pkgs;
-      };
+      imports = ["${inputs.nixpkgs}/nixos/modules/misc/nixpkgs.nix"];
+      nixpkgs.hostPlatform = {inherit system;};
+      _module.args = psArgs.config.nixpkgs.pkgs;
+      legacyPackages = pkgs;
     };
   };
 }
