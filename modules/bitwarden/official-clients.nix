5fcca6cbd1ffbe147164af6d62e303f58df0b930
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-07-28 21:46:45 +0700

    bump inputs

--- modules/bitwarden/official-clients.nix
+++ modules/bitwarden/official-clients.nix
@@ -4,9 +4,8 @@
       home.packages = [pkgs.bitwarden-cli];
     };
 
-    gui = {
-      # https://github.com/NixOS/nixpkgs/issues/526914
-      # home.packages = [ pkgs.bitwarden-desktop ];
+    gui = {pkgs, ...}: {
+      home.packages = [pkgs.bitwarden-desktop];
     };
   };
 }
