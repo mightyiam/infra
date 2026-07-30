13712863c0303c71eb3cabb03d2d1a18e463ef31
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-07-30 13:21:38 +0700

    break apart modules/mightyiam/basics.nix

--- modules/mightyiam/nixos.nix
+++ modules/mightyiam/nixos.nix
@@ -1,8 +1,4 @@
 {config, ...}: {
-  users.mightyiam = {
-    email = "mightyiampresence@gmail.com";
-  };
-
   nixos.modules = {
     inherit (config.users.mightyiam.nixos) base pc;
   };
---
+++ modules/mightyiam/social.nix
@@ -0,0 +1,5 @@
+{
+  users.mightyiam = {
+    email = "mightyiampresence@gmail.com";
+  };
+}
