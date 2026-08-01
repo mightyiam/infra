0bb3a9932a6df0e6562eba94f79c1ecb14c9041a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-01 16:43:39 +0700

    rm silly extraneous `_module.args =` in nixpkgs module

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -2,14 +2,13 @@
   config = {
     flake-file.inputs.nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
 
-    perSystem = psArgs @ {
+    perSystem = {
       system,
       pkgs,
       ...
     }: {
       imports = ["${inputs.nixpkgs}/nixos/modules/misc/nixpkgs.nix"];
       nixpkgs.hostPlatform = {inherit system;};
-      _module.args = psArgs.config.nixpkgs.pkgs;
       legacyPackages = pkgs;
     };
   };
