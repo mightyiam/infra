a7b7b275c797bf7ee29ae38da02b6572a5c45bbf
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-03 10:00:25 +0700

    use mkOrder on defaultApplicationPackages instead of home.packages

--- modules/libreoffice.nix
+++ modules/libreoffice.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   homeManager.modules.gui = {pkgs, ...}: {
-    home.packages = lib.mkOrder 30 [pkgs.libreoffice-fresh];
-    xdg.mimeApps.defaultApplicationPackages = [pkgs.libreoffice-fresh];
+    home.packages = [pkgs.libreoffice-fresh];
+    xdg.mimeApps.defaultApplicationPackages = lib.mkOrder 30 [pkgs.libreoffice-fresh];
   };
 }
