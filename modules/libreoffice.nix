e8c0f6b343e125d3b1602ccdb6fe8ce370521019
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-03 10:03:33 +0700

    tweak some defaultApplicationPackages order

--- modules/libreoffice.nix
+++ modules/libreoffice.nix
@@ -1,6 +1,6 @@
 {lib, ...}: {
   homeManager.modules.gui = {pkgs, ...}: {
     home.packages = [pkgs.libreoffice-fresh];
-    xdg.mimeApps.defaultApplicationPackages = lib.mkOrder 30 [pkgs.libreoffice-fresh];
+    xdg.mimeApps.defaultApplicationPackages = lib.mkOrder 100 [pkgs.libreoffice-fresh];
   };
 }
--- modules/mightyiam/document-viewer.nix
+++ modules/mightyiam/document-viewer.nix
@@ -1,6 +1,6 @@
-{
+{lib, ...}: {
   home.gui = hmArgs: {
     programs.zathura.enable = true;
-    xdg.mimeApps.defaultApplicationPackages = [hmArgs.config.programs.zathura.package];
+    xdg.mimeApps.defaultApplicationPackages = lib.mkOrder 15 [hmArgs.config.programs.zathura.package];
   };
 }
