1abc1dee1968547143dd9aba724ad94e05f02303
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-05 10:57:21 +0700

    replace init template refspec with clone-bare-with-refspec alias

--- modules/mightyiam/git/clone.nix
+++ modules/mightyiam/git/clone.nix
@@ -1,10 +1,9 @@
-{lib, ...}: {
-  home.base = {pkgs, ...}: {
-    programs.git.settings.init.templateDir =
-      pkgs.writeTextDir "config" ''
-        [remote "origin"]
-            fetch = +refs/heads/*:refs/remotes/origin/*
-      ''
-      |> lib.getAttr "outPath";
+{
+  home.base = {
+    programs.git.settings = {
+      alias = {
+        clone-bare-with-refspec = "clone --bare --config remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*";
+      };
+    };
   };
 }
