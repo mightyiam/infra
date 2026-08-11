57212007b8b0b24f26bd3b1d4385d6295f349206
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-11 19:16:21 +0700

    avoid `nixpkgs.lib`

--- outputs.nix
+++ outputs.nix
@@ -1,6 +1,8 @@
-inputs:
-inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-  debug = true;
-  imports = [((import inputs.import-tree).filterNot (inputs.nixpkgs.lib.hasSuffix ".pkg.nix") ./modules)];
-  _module.args.rootPath = ./.;
-}
+inputs: let
+  lib = import "${inputs.nixpkgs}/lib";
+in
+  inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+    debug = true;
+    imports = [((import inputs.import-tree).filterNot (lib.hasSuffix ".pkg.nix") ./modules)];
+    _module.args.rootPath = ./.;
+  }
