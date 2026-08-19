e5c0455326ba052aa66ef0e85b9b721294dfe392
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-19 10:25:57 +0700

    rofimoji skin tone

--- modules/mightyiam/hid/character-picker.nix
+++ modules/mightyiam/hid/character-picker.nix
@@ -2,6 +2,10 @@
   home.gui = {pkgs, ...}: {
     home.packages = [pkgs.rofimoji];
 
+    xdg.configFile."rofimoji.rc".text = lib.generators.toKeyValue {} {
+      skin-tone = "medium-light";
+    };
+
     wayland.windowManager.hyprland.settings.bind = [
       "SUPER, u, exec, ${lib.getExe pkgs.rofimoji}"
       "SUPER+Shift, u, exec, ${lib.getExe pkgs.rofimoji} --files all"
