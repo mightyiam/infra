c8e66f12477f201757b17fddbc62d91ca7b28af9
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-08-31 19:56:48 +0700

    all users vellum autostart

--- modules/mightyiam/annotation.nix
+++ modules/mightyiam/annotation.nix
@@ -1,4 +1,18 @@
 {lib, ...}: {
+  homeManager.modules.gui = {pkgs, ...}: {
+    home.packages = [pkgs.vellum];
+
+    xdg.autostart.entries = [
+      (pkgs.makeDesktopItem {
+          name = "vellum";
+          desktopName = "Vellum";
+          exec = "vellum";
+          terminal = false;
+        }
+        + "/share/applications/vellum.desktop")
+    ];
+  };
+
   home.gui = {pkgs, ...}: {
     wayland.windowManager.hyprland.settings = {
       exec-once = [(lib.getExe pkgs.vellum)];
