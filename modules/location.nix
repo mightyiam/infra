4393e46a3bc11fe02313f29166f5d562e039a1d0
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-09-24 08:15:22 +0700

    default geoclue2.enablelDemoAgent

--- modules/location.nix
+++ modules/location.nix
@@ -1,10 +1,5 @@
-{lib, ...}: {
+{
   nixos.modules.pc = {
-    services.geoclue2 = {
-      enable = true;
-
-      # https://github.com/NixOS/nixpkgs/issues/68489#issuecomment-1484030107
-      enableDemoAgent = lib.mkDefault true;
-    };
+    services.geoclue2.enable = true;
   };
 }
