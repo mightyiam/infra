65fa9339efcf2222fcf53ae5b18d69b09afd3fec
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2026-09-24 06:00:21 +0400

    geoclue with ip

--- modules/location.nix
+++ modules/location.nix
@@ -1,5 +1,12 @@
 {
   nixos.modules.pc = {
     services.geoclue2.enable = true;
+
+    # workaround for https://gitlab.freedesktop.org/geoclue/geoclue/-/work_items/162
+    environment.etc."geoclue/conf.d/00-ip.conf".text = ''
+      [ip]
+      enable=true
+      method=ichnaea
+    '';
   };
 }
